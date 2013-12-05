0d7c1e0b6e82ab823e2eaeb9ab66ca1b07ca3d28
Масленников Иван Евгеньевич <i.maslennikov@2gis.ru>
2013-12-05 06:28:45 +0000

    т.к. нет прав на прокидывание изменение в BLFlex из Main через teamproject BL, то для buldability выполнена синхронизация напрямую с Main продолжение
    git-tfs-id: [http://uk-tfs02:8080/tfs/DefaultCollection]$/ERM.Core/Main/Platform;C14178

---
+++ Source/EntryPoints/UI/Desktop/2Gis.Platform.UI.WPF.Shell/Presentation/Blendability/DesignTimePaths.cs
@@ -0,0 +1,1 @@
+namespace DoubleGis.Platform.UI.WPF.Shell.Presentation.Blendability { static public class DesignTimePaths { public static readonly string LocalPath = @"D:\TFS\Core\Main\CompositionRoots\Source\2Gis.Erm.UI.Desktop.WPF\bin\Debug";} } 
