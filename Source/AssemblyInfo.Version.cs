d6c95672dc1d91a809e90f274bd3ac22afe7782c
Масленников Иван Евгеньевич <i.maslennikov@2gis.ru>
2014-01-13 08:38:35 +0000

    заново создан компонент BL
    git-tfs-id: [http://uk-tfs02:8080/tfs/DefaultCollection]$/ERM.BL/Main/BL;C14898

---
+++ Source/AssemblyInfo.Version.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+using System.Resources;
+
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: AssemblyFileVersion("0.0.0.0")]
+[assembly: AssemblyInformationalVersion("0.0.0")]
+
+[assembly: SatelliteContractVersion("0.0.0.0")]
