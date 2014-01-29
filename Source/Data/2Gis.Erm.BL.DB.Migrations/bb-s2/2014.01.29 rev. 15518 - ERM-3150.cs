e4e83318283461a0fae34f6933093f399f7fc5b2
Баранихин Юрий Владимирович <y.baranihin@2Gis.ru>
2014-01-29 10:44:48 +0000

    актуализация
    git-tfs-id: [http://uk-tfs02:8080/tfs/DefaultCollection]$/ERM.BL/Main/BL;C15633

---
+++ Source/Data/2Gis.Erm.BL.DB.Migrations/bb-s2/2014.01.29 rev. 15518 - ERM-3150.cs
@@ -0,0 +1,15 @@
+﻿using DoubleGis.Erm.BL.DB.Migrations.Properties;
+using DoubleGis.Erm.Platform.Migration.Base;
+using DoubleGis.Erm.Platform.Migration.Core;
+
+namespace DoubleGis.Erm.BL.DB.Migrations.bb_s2
+{
+    [Migration(15518, "фикс бага ERM-3150", "y.baranihin")]
+    public class Migration15518 : TransactedMigration
+    {
+        protected override void ApplyOverride(IMigrationContext context)
+        {
+            context.Database.ExecuteNonQuery(Resources.Migration15518);
+        }
+    }
+}
