08b798c6aa56f0ebbef4bfe7276961a2566df928
Масленников Иван Евгеньевич <i.maslennikov@2gis.ru>
2014-02-04 05:41:57 +0000

    Выделение BLQuerying. Split1 RI
    git-tfs-id: [http://uk-tfs02:8080/tfs/DefaultCollection]$/ERM.Core/Main/Platform;C15886

--- Source/EntryPoints/UI/Desktop/2Gis.Platform.UI.WPF.Shell/Presentation/Blendability/DesignTimePaths.cs
+++ Source/EntryPoints/UI/Desktop/2Gis.Platform.UI.WPF.Shell/Presentation/Blendability/DesignTimePaths.cs
@@ -1,1 +1,1 @@
-namespace DoubleGis.Platform.UI.WPF.Shell.Presentation.Blendability { static public class DesignTimePaths { public static readonly string LocalPath = @"D:\TFS\Core\Main\CompositionRoots\Source\2Gis.Erm.UI.Desktop.WPF\bin\Debug";} } 
+namespace DoubleGis.Platform.UI.WPF.Shell.Presentation.Blendability { static public class DesignTimePaths { public static readonly string LocalPath = @"D:\TFS\ERM.Research\Dev\Features\Split1\CompositionRoots\Source\2Gis.Erm.UI.Desktop.WPF\bin\Debug";} } 
