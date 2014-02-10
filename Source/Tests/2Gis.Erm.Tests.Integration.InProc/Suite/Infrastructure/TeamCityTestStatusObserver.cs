d05629a0b893d36da33ab2e2b3ae28af6ddae06c
Лапеев Валерий Алексеевич <v.lapeev@2gis.ru>
2014-02-10 10:43:21 +0000

    Подправил взаимодейтсиве InProc-тестов с TeamCity
    git-tfs-id: [http://uk-tfs02:8080/tfs/DefaultCollection]$/ERM.Composition/Main/CompositionRoots;C16265

--- Source/Tests/2Gis.Erm.Tests.Integration.InProc/Suite/Infrastructure/TeamCityTestStatusObserver.cs
+++ Source/Tests/2Gis.Erm.Tests.Integration.InProc/Suite/Infrastructure/TeamCityTestStatusObserver.cs
@@ -9,27 +9,27 @@ namespace DoubleGis.Erm.Tests.Integration.InProc.Suite.Infrastructure
 
         public void Started(Type testType)
         {
-            Console.WriteLine(@"##teamcity[testStarted name='{0}']", ResolveTestId(testType));
+            SendToTeamCity(string.Format(@"testStarted name='{0}'", ResolveTestId(testType)));
         }
 
         public void Unresolved(Type testType, Exception exception)
         {
             string description = string.Format("{0}", exception.Message);
-            Console.WriteLine(@"##teamcity[testFailed name='{0}' message='Unresolved. Can't create test instance' details='{1}']", ResolveTestId(testType), description);
+            SendToTeamCity(string.Format(@"testFailed name='{0}' message='Unresolved. Can't create test instance' details='{1}'", ResolveTestId(testType), description));
             FinishTest(testType);
         }
 
         public void Unhandled(Type testType, Exception exception)
         {
-            string description = string.Format("{0}", exception.Message + exception.StackTrace).Replace(Environment.NewLine, string.Empty);
-            Console.WriteLine(@"##teamcity[testFailed name='{0}' message='Unhandled. Exception caught {1}' details='{2}']", ResolveTestId(testType), exception.GetType().Name, description);
+            string description = string.Format("{0}", exception.Message + exception.StackTrace);
+            SendToTeamCity(string.Format(@"testFailed name='{0}' message='Unhandled. Exception caught {1}' details='{2}'", ResolveTestId(testType), exception.GetType().Name, description));
             FinishTest(testType);
         }
 
         public void Asserted(Type testType, ITestResult testResult)
         {
             string description = string.Format("{0}", testResult.Asserted != null ? testResult.Asserted.Message : testResult.Report);
-            Console.WriteLine(@"##teamcity[testFailed type='comparisonFailure' name='{0}' message='Asserted. ' details='{1}' expected='Succeeded' actual='Failed']", ResolveTestId(testType), description);
+            SendToTeamCity(string.Format(@"testFailed type='comparisonFailure' name='{0}' message='Asserted. ' details='{1}' expected='Succeeded' actual='Failed'", ResolveTestId(testType), description));
             FinishTest(testType);
         }
 
@@ -41,17 +41,23 @@ namespace DoubleGis.Erm.Tests.Integration.InProc.Suite.Infrastructure
         public void Ignored(Type testType, ITestResult testResult)
         {
             string description = string.Format("{0}", testResult.Asserted != null ? testResult.Asserted.Message : testResult.Report);
-            Console.WriteLine(@"##teamcity[testIgnored name='{0}' details='{1}']", ResolveTestId(testType), description);
+            SendToTeamCity(string.Format(@"testIgnored name='{0}' details='{1}'", ResolveTestId(testType), description));
         }
 
-        private void FinishTest(Type testType)
+        private static void FinishTest(Type testType)
         {
-            Console.WriteLine(@"##teamcity[testFinished name='{0}']", ResolveTestId(testType));
+            SendToTeamCity(string.Format(@"testFinished name='{0}'", ResolveTestId(testType)));
         }
 
-        private string ResolveTestId(Type testType)
+        private static string ResolveTestId(Type testType)
         {
             return testType.Name;
+        }
+
+        // TeamCity не распознает сообщения с переносами строк
+        private static void SendToTeamCity(string message)
+        {
+            Console.WriteLine(string.Format("##teamcity[{0}]", message).Replace(Environment.NewLine, " "));
         }
     }
 }
