b6a9c3f0394ec6114f535801ac069e9a94e32e54
Тукаев Александр Юрьевич <a.tukaev@2Gis.ru>
2014-02-20 09:42:49 +0000

    [ERM-3360] fix
    git-tfs-id: [http://uk-tfs02:8080/tfs/DefaultCollection]$/Erm.Core/Prerelease/BLCore;C17021

--- Source/API/2Gis.Erm.BLCore.API.Operations/Generic/Modify/Old/EditOrderPositionRequest.cs
+++ Source/API/2Gis.Erm.BLCore.API.Operations/Generic/Modify/Old/EditOrderPositionRequest.cs
@@ -1,4 +1,6 @@
-﻿using DoubleGis.Erm.Platform.Model.Entities.DTOs;
+﻿using System.Linq;
+
+using DoubleGis.Erm.Platform.Model.Entities.DTOs;
 using DoubleGis.Erm.Platform.Model.Entities.Erm;
 
 namespace DoubleGis.Erm.BLCore.API.Operations.Generic.Modify.Old
@@ -11,6 +13,16 @@ namespace DoubleGis.Erm.BLCore.API.Operations.Generic.Modify.Old
         public EditOrderPositionRequest()
         {
             HasUpdatedAdvertisementMaterials = true;
+        }
+
+        // TODO {all, 20.02.2014}: После обновления UI позиции заказа имеет смысл реализацию свойства поменять на auto
+        public long? CategoryId
+        {
+            get
+            {
+                var categories = AdvertisementsLinks.Where(x => x.CategoryId != null).Select(x => x.CategoryId.Value).Distinct().ToArray();
+                return categories.Length == 1 ? (long?)categories[0] : null;
+            }
         }
     }
 }
