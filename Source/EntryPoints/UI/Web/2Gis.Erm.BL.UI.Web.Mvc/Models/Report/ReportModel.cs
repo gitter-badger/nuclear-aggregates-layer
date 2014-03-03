772b7bf05b5075678ee80d6ec28e3f77ec5517e2
Иванов Денис Игоревич <d.ivanov@2gis.ru>
2014-03-03 05:21:47 +0000

    [ERM-3500] ModelBinderProvider в DoubleGis.Erm.BL.UI.Web.Mvc
    git-tfs-id: [http://uk-tfs02:8080/tfs/DefaultCollection]$/ERM.BL/Prerelease/BL;C17582

--- Source/EntryPoints/UI/Web/2Gis.Erm.BL.UI.Web.Mvc/Models/Report/ReportModel.cs
+++ Source/EntryPoints/UI/Web/2Gis.Erm.BL.UI.Web.Mvc/Models/Report/ReportModel.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq.Expressions;
-using System.Web.Mvc;
 
 using DoubleGis.Erm.BLCore.UI.Web.Mvc.ViewModels;
 using DoubleGis.Erm.Platform.UI.Web.Mvc.ViewModels;
@@ -35,26 +34,6 @@ namespace DoubleGis.Erm.BL.UI.Web.Mvc.Models.Report
 
             /// <summary>Выражение, используемое для получения значения поля модели</summary>
             public Expression GetExpression { get; set; }
-        }
-    }
-
-    internal sealed class DelayedReportModel : ReportModel
-    {
-        private readonly IModelBinder _binder;
-        private readonly ControllerContext _controllerContext;
-        private readonly ModelBindingContext _bindingContext;
-
-        public DelayedReportModel(IModelBinder binder, ControllerContext controllerContext, ModelBindingContext bindingContext)
-        {
-            _binder = binder;
-            _controllerContext = controllerContext;
-            _bindingContext = bindingContext;
-        }
-
-        public ReportModel GetReportModel()
-        {
-            var model = _binder.BindModel(_controllerContext, _bindingContext);
-            return model is DelayedReportModel ? null : (ReportModel)model;
         }
     }
 }
---
+++ Source/EntryPoints/UI/Web/2Gis.Erm.BL.UI.Web.Mvc/Utils/ModelBinderProvider.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Linq;
+using System.Web.Mvc;
+
+using DoubleGis.Erm.BL.UI.Web.Mvc.Models.Report;
+
+using ReportModel = DoubleGis.Erm.BLCore.UI.Web.Mvc.Models.Report.ReportModel;
+
+namespace DoubleGis.Erm.BL.UI.Web.Mvc.Utils
+{
+    public class ModelBinderProvider : IModelBinderProvider
+    {
+        private readonly IModelBinder _modelBinder;
+
+        public ModelBinderProvider()
+        {
+            _modelBinder = new DefaultModelBinder();
+        }
+
+        IModelBinder IModelBinderProvider.GetBinder(Type modelType)
+        {
+            return _modelBinder;
+        }
+
+        private sealed class DefaultModelBinder : System.Web.Mvc.DefaultModelBinder
+        {
+            public override object BindModel(ControllerContext controllerContext, ModelBindingContext bindingContext)
+            {
+                if (bindingContext.ModelType == typeof(ReportModel))
+                {
+                    return BindReport(controllerContext, bindingContext);
+                }
+            }
+
+            private object BindReport(ControllerContext controllerContext, ModelBindingContext bindingContext)
+            {
+                var typeName = bindingContext.ValueProvider.GetValue("ReportType").AttemptedValue;
+                if (string.IsNullOrWhiteSpace(typeName))
+                {
+                    throw new ArgumentException("Bad report type");
+                }
+
+                var type = AppDomain.CurrentDomain.GetAssemblies()
+                                    .Select(assembly => assembly.GetType(typeName))
+                                    .SingleOrDefault(t => t != null);
+
+                if (type == null)
+                {
+                    // Сервер не знает о типе данных, который был использован для построения модели. Даём шанс позже вернуться к этому вопросу.
+                    return new DelayedReportModel(this, controllerContext, bindingContext);
+                }
+
+                if (type == null || !typeof(ReportModel).IsAssignableFrom(type))
+                {
+                    throw new ArgumentException("Bad report type");
+                }
+
+                var constructor = type.GetConstructor(new Type[0]);
+
+                if (constructor == null)
+                {
+                    throw new ArgumentException("Bad report type");
+                }
+
+                bindingContext.ModelMetadata = ModelMetadataProviders.Current.GetMetadataForType(() => constructor.Invoke(new object[0]), type);
+                var model = base.BindModel(controllerContext, bindingContext);
+                return model;
+            }
+        }
+    }
+}
