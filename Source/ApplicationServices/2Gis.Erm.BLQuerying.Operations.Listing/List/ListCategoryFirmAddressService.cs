f703054f3bc8a3c17f8ce33883ce6dab25c65a6f
Пашук Максим Владимирович <m.pashuk@2gis.ru>
2014-03-07 07:17:39 +0000

    ERM-3550
    git-tfs-id: [http://uk-tfs02:8080/tfs/DefaultCollection]$/ERM.Research/Prerelease/BLQuerying;C17873

--- Source/ApplicationServices/2Gis.Erm.BLQuerying.Operations.Listing/List/ListCategoryFirmAddressService.cs
+++ Source/ApplicationServices/2Gis.Erm.BLQuerying.Operations.Listing/List/ListCategoryFirmAddressService.cs
@@ -38,7 +38,7 @@ namespace DoubleGis.Erm.BLQuerying.Operations.Listing.List
             var defaultCategoryRate = string.Format(_userContext.Profile.UserLocaleInfo.UserCultureInfo, "{0:p0}", 1);
             long? categoryRateOrganizationUnitId = null;
 
-            if (querySettings.ParentEntityName == EntityName.Firm)
+            if (querySettings.ParentEntityName == EntityName.Firm && querySettings.ParentEntityId != null)
             {
                 categoryRateOrganizationUnitId = _finder.Find(Specs.Find.ById<Firm>(querySettings.ParentEntityId.Value))
                                                         .Select(firm => firm.OrganizationUnitId)
@@ -61,31 +61,36 @@ namespace DoubleGis.Erm.BLQuerying.Operations.Listing.List
                                                 x.FirmAddress.FirmId == querySettings.ParentEntityId.Value;
                 }
 
-                // Для фирмы агрегируем по рубрикам адресов
+                // Если родитель фирма, то делаем Distinct где оставляем проекции от Firm и Category
                 return query
                     .Where(x => !x.Category.IsDeleted && !x.FirmAddress.IsDeleted && !x.IsDeleted)
                     .Filter(_filterHelper, firmFilterExpression)
                     .DefaultFilter(_filterHelper, querySettings)
-                    .GroupBy(x => x.Category)
                     .Select(x => new ListCategoryFirmAddressDto
-                        {
-                            CategoryGroup = categoryRateOrganizationUnitId.HasValue
-                                ? x.Key.CategoryOrganizationUnits.Where(unit => unit.OrganizationUnitId == categoryRateOrganizationUnitId.Value)
-                                            .Select(unit => unit.CategoryGroup.CategoryGroupName).FirstOrDefault() ?? defaultCategoryRate
-                                : string.Empty,
-                            CategoryId = x.Key.Id,
-                            Name = x.Key.Name,
-                            ParentId = x.Key.ParentId,
-                            ParentName = x.Key.ParentCategory != null ? x.FirstOrDefault().Category.ParentCategory.Name : null,
-                            Level = x.Key.Level,
-                            SortingPosition = x.Min(y => y.SortingPosition),
-                            IsPrimary = x.Any(y => y.IsPrimary),
-                            IsActive = x.Any(y => y.IsActive)
-                        })
+                    {
+                        CategoryId = x.Category.Id,
+                        Name = x.Category.Name,
+                        ParentId = x.Category.ParentId,
+                        ParentName = x.Category.ParentCategory.Name,
+                        CategoryGroup = categoryRateOrganizationUnitId.HasValue
+                            ? x.Category.CategoryOrganizationUnits.Where(unit => unit.OrganizationUnitId == categoryRateOrganizationUnitId.Value)
+                                        .Select(unit => unit.CategoryGroup.CategoryGroupName).FirstOrDefault() ?? defaultCategoryRate
+                            : string.Empty,
+                        Level = x.Category.Level,
+                        FirmId = x.FirmAddress.FirmId,
+
+                        // fake
+                        FirmAddressId = 0,
+                        Id = 0,
+                        IsActive = true,
+                        IsPrimary = true,
+                        SortingPosition = 0,
+                    })
+                    .Distinct()
                     .QuerySettings(_filterHelper, querySettings, out count);
             }
 
-            if (querySettings.ParentEntityName == EntityName.FirmAddress)
+            if (querySettings.ParentEntityName == EntityName.FirmAddress && querySettings.ParentEntityId != null)
             {
                 categoryRateOrganizationUnitId = _finder.Find(Specs.Find.ById<FirmAddress>(querySettings.ParentEntityId.Value))
                                                         .Select(address => address.Firm.OrganizationUnitId)
@@ -102,13 +107,15 @@ namespace DoubleGis.Erm.BLQuerying.Operations.Listing.List
                             ? x.Category.CategoryOrganizationUnits.Where(unit => unit.OrganizationUnitId == categoryRateOrganizationUnitId.Value)
                                         .Select(unit => unit.CategoryGroup.CategoryGroupName).FirstOrDefault() ?? defaultCategoryRate
                             : string.Empty,
-                        CategoryId = x.Category.Id,
                         FirmAddressId = x.FirmAddressId,
                         SortingPosition = x.SortingPosition,
+
+                        CategoryId = x.Category.Id,
                         Name = x.Category.Name,
                         ParentId = x.Category.ParentId,
                         ParentName = x.Category.ParentCategory != null ? x.Category.ParentCategory.Name : null,
                         Level = x.Category.Level,
+
                         IsPrimary = x.IsPrimary,
                         IsActive = x.IsActive,
                         FirmId = x.FirmAddress.FirmId,
