77800455d4a1d946da314df63243bdfe2cd29e83
Тукаев Александр Юрьевич <a.tukaev@2Gis.ru>
2014-03-17 07:17:29 +0000

    [ERM-3566] fix
    git-tfs-id: [http://uk-tfs02:8080/tfs/DefaultCollection]$/ERM.Composition/Prerelease/CompositionRoots;C18112

--- Source/2Gis.Erm.TaskService/DI/Bootstrapper.cs
+++ Source/2Gis.Erm.TaskService/DI/Bootstrapper.cs
@@ -38,6 +38,7 @@ using DoubleGis.Erm.Platform.Common.CorporateQueue.RabbitMq;
 using DoubleGis.Erm.Platform.Common.PrintFormEngine;
 using DoubleGis.Erm.Platform.Core.Identities;
 using DoubleGis.Erm.Platform.Core.Operations.Logging;
+using DoubleGis.Erm.Platform.DAL.PersistenceServices;
 using DoubleGis.Erm.Platform.DI.Common.Config;
 using DoubleGis.Erm.Platform.DI.Common.Config.MassProcessing;
 using DoubleGis.Erm.Platform.DI.Config.MassProcessing;
@@ -58,7 +59,7 @@ namespace DoubleGis.Erm.TaskService.DI
 {
     internal static class Bootstrapper
     {
-        private static readonly Type[] EagerLoading = { typeof(TaskObtainer) }; // чтобы в домен загрузилась сборка 2Gis.Erm.BLFlex.Operations.Global
+        private static readonly Type[] EagerLoading = { typeof(TaskObtainer), typeof(ICleanupPersistenceService) }; // чтобы в домен загрузилась сборка 2Gis.Erm.BLFlex.Operations.Global
 
         public static IUnityContainer ConfigureUnity(ITaskServiceAppSettings settings)
         {
