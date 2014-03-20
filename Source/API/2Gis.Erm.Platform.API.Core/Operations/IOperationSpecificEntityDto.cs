8c25ae9c2267ca296e9ec6e1bb3d5cd8ed86754b
Тукаев Александр Юрьевич <a.tukaev@2Gis.ru>
2014-03-20 09:35:08 +0000

    Core/ES RI
    git-tfs-id: [http://uk-tfs02:8080/tfs/DefaultCollection]$/ERM.Core/Main/Platform;C18347

--- Source/API/2Gis.Erm.Platform.API.Core/Operations/IOperationSpecificEntityDto.cs
+++ Source/API/2Gis.Erm.Platform.API.Core/Operations/IOperationSpecificEntityDto.cs
@@ -1,19 +1,9 @@
-﻿using DoubleGis.Erm.Platform.Model.Entities.Interfaces;
-
-namespace DoubleGis.Erm.Platform.API.Core.Operations
+﻿namespace DoubleGis.Erm.Platform.API.Core.Operations
 {
     /// <summary>
     /// Маркерный интерфейс для DTO специфичной для конкретной операции
     /// </summary>
     public interface IOperationSpecificEntityDto
-    {
-    }
-
-    /// <summary>
-    /// Маркерный интерфейс для DTO специфичной для конкретного типа сущности для конкретной операции
-    /// </summary>
-    public interface IOperationSpecificEntityDto<TEntity> : IOperationSpecificEntityDto
-        where TEntity : IEntityKey
     {
     }
 }
