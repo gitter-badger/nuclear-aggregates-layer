baeb2bf96cb592b75c9003dc8c42516bb21b33cb
Тукаев Александр Юрьевич <a.tukaev@2Gis.ru>
2014-03-21 10:53:32 +0000

    [ERM-3668]
    git-tfs-id: [http://uk-tfs02:8080/tfs/DefaultCollection]$/Erm.Core/Prerelease/BLCore;C18474

--- Source/ApplicationServices/2Gis.Erm.BLCore.Aggregates/Firms/ReadModel/IFirmReadModel.cs
+++ Source/ApplicationServices/2Gis.Erm.BLCore.Aggregates/Firms/ReadModel/IFirmReadModel.cs
@@ -10,5 +10,6 @@ namespace DoubleGis.Erm.BLCore.Aggregates.Firms.ReadModel
     public interface IFirmReadModel : IAggregateReadModel<Firm>
     {
         IReadOnlyDictionary<Guid, FirmWithAddressesAndProjectDto> GetFirmInfosByCrmIds(IEnumerable<Guid> crmIds);
+        bool HasFirmClient(long firmId);
     }
 }
