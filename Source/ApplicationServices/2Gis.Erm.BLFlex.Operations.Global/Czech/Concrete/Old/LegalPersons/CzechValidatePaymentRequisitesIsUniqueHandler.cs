b037c0619cabc3800f34b95914b8e975f035854a
Лапеев Валерий Алексеевич <v.lapeev@2gis.ru>
2014-03-26 09:11:57 +0000

    [ERM-3623] Erm.Core/Main -> Erm.BL/Main FI + шелв в BLFlex от Саши
    git-tfs-id: [http://uk-tfs02:8080/tfs/DefaultCollection]$/ERM.BL/Main/BLFlex;C18701

--- Source/ApplicationServices/2Gis.Erm.BLFlex.Operations.Global/Czech/Concrete/Old/LegalPersons/CzechValidatePaymentRequisitesIsUniqueHandler.cs
+++ Source/ApplicationServices/2Gis.Erm.BLFlex.Operations.Global/Czech/Concrete/Old/LegalPersons/CzechValidatePaymentRequisitesIsUniqueHandler.cs
@@ -1,7 +1,7 @@
 using DoubleGis.Erm.BLCore.Aggregates.LegalPersons;
 using DoubleGis.Erm.BLCore.API.Operations.Concrete.Old.LegalPersons;
 using DoubleGis.Erm.BLCore.Common.Infrastructure.Handlers;
-using DoubleGis.Erm.BLCore.Resources.Server.Properties;
+using DoubleGis.Erm.BLFlex.Resources.Server.Properties;
 using DoubleGis.Erm.Platform.API.Core.Exceptions;
 using DoubleGis.Erm.Platform.API.Core.Operations.RequestResponse;
 using DoubleGis.Erm.Platform.Model.Entities.Enums;
