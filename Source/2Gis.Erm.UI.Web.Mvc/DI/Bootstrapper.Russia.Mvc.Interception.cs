a8e3a5e22c33b48a68406e74c41d30c75e3a6e01
Лапеев Валерий Алексеевич <v.lapeev@2gis.ru>
2014-04-03 08:56:37 +0000

    [ERM-3723] Настроил interception для логирования смены реквизитов юрлица
    git-tfs-id: [http://uk-tfs02:8080/tfs/DefaultCollection]$/ERM.Composition/Prerelease/CompositionRoots;C18990

---
+++ Source/2Gis.Erm.UI.Web.Mvc/DI/Bootstrapper.Russia.Mvc.Interception.cs
@@ -0,0 +1,16 @@
+﻿using DoubleGis.Erm.BLFlex.UI.Web.Mvc.Global.Areas.Russia.Controllers;
+
+using Microsoft.Practices.Unity.InterceptionExtension;
+
+namespace DoubleGis.Erm.UI.Web.Mvc.DI
+{
+    internal static partial class BootstrapperMvc
+    {
+        private static Interception ConfigureRussiaSpecific(Interception interception)
+        {
+            interception.SetInterceptorFor<LegalPersonController>(new VirtualMethodInterceptor());
+
+            return interception;
+        }
+    }
+}
