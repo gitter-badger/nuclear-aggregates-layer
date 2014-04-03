206a46ccc5398e142b28b07b62eacac801673a48
Тукаев Александр Юрьевич <a.tukaev@2Gis.ru>
2014-04-03 07:15:47 +0000

    [ERM-3723] убрал проверку признака IsInSyncWith1C при логировании изменений по юр. лицу
    git-tfs-id: [http://uk-tfs02:8080/tfs/DefaultCollection]$/Erm.Core/Prerelease/Platform;C18979

--- Source/2Gis.Erm.Platform.Core/ActionLogging/ActionsLogger.cs
+++ Source/2Gis.Erm.Platform.Core/ActionLogging/ActionsLogger.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections.Generic;
-using System.Linq;
 
 using DoubleGis.Erm.Platform.API.Core.ActionLogging;
 using DoubleGis.Erm.Platform.API.Core.Identities;
@@ -14,19 +13,16 @@ namespace DoubleGis.Erm.Platform.Core.ActionLogging
     {
         private readonly IRepository<ActionsHistory> _actionsHistoryRepository;
         private readonly IRepository<ActionsHistoryDetail> _actionsHistoryDetailRepository;
-        private readonly IActionLoggingValidatorFactory _actionLoggingValidatorFactory;
         private readonly IIdentityProvider _identityProvider;
         private readonly IOperationScopeFactory _scopeFactory;
 
         public ActionsLogger(IRepository<ActionsHistory> actionsHistoryRepository,
                              IRepository<ActionsHistoryDetail> actionsHistoryDetailRepository,
-                             IActionLoggingValidatorFactory actionLoggingValidatorFactory,
                              IIdentityProvider identityProvider,
                              IOperationScopeFactory scopeFactory)
         {
             _actionsHistoryRepository = actionsHistoryRepository;
             _actionsHistoryDetailRepository = actionsHistoryDetailRepository;
-            _actionLoggingValidatorFactory = actionLoggingValidatorFactory;
             _identityProvider = identityProvider;
             _scopeFactory = scopeFactory;
         }
@@ -37,7 +33,7 @@ namespace DoubleGis.Erm.Platform.Core.ActionLogging
             {
                 LogChanges(scope, changeDescriptors);
                 scope.Complete();
-        }
+            }
         }
 
         public void LogChanges(ChangesDescriptor changeDescriptor)
@@ -53,16 +49,11 @@ namespace DoubleGis.Erm.Platform.Core.ActionLogging
         {
             foreach (var changesDescriptor in changeDescriptors)
             {
-                if (!CanLog(changesDescriptor))
-            {
-                    continue;
-            }
-
                 var actionsHistoryItem = new ActionsHistory
                     {
                         ActionType = (int)ActionType.Edit, // edit only for now
-                    EntityType = (int)changesDescriptor.EntityType,
-                    EntityId = changesDescriptor.EntityId
+                        EntityType = (int)changesDescriptor.EntityType,
+                        EntityId = changesDescriptor.EntityId
                     };
 
                 _identityProvider.SetFor(actionsHistoryItem);
@@ -75,25 +66,18 @@ namespace DoubleGis.Erm.Platform.Core.ActionLogging
                         {
                             ActionsHistoryId = actionsHistoryItem.Id,
                             PropertyName = difference.Key,
-                        OriginalValue = difference.Value.OriginalValue != null ? difference.Value.OriginalValue.ToString() : null,
-                        ModifiedValue = difference.Value.ModifiedValue != null ? difference.Value.ModifiedValue.ToString() : null,
+                            OriginalValue = difference.Value.OriginalValue != null ? difference.Value.OriginalValue.ToString() : null,
+                            ModifiedValue = difference.Value.ModifiedValue != null ? difference.Value.ModifiedValue.ToString() : null,
                         };
 
                     _identityProvider.SetFor(actionsHistoryDetailItem);
                     _actionsHistoryDetailRepository.Add(actionsHistoryDetailItem);
                     scope.Added<ActionsHistoryDetail>(actionsHistoryDetailItem.Id);
                 }
-                }
-
-            _actionsHistoryRepository.Save();
-                _actionsHistoryDetailRepository.Save();
             }
 
-        private bool CanLog(ChangesDescriptor changesDescriptor)
-        {
-            // FIXME {i.maslennikov, 24.09.2013}: в процессе рефакторинга - попробовать перенести в точки вызова/конфигурирования логирования изменений
-            var validators = _actionLoggingValidatorFactory.GetValidators(changesDescriptor.EntityType);
-            return validators.All(x => x.Validate(changesDescriptor.EntityId));
+            _actionsHistoryRepository.Save();
+            _actionsHistoryDetailRepository.Save();
         }
     }
 }
