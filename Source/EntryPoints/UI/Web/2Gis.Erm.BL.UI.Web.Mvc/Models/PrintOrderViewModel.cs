97434e6f87a4c8f023a457120b33d7d6f796adbd
Лапеев Валерий Алексеевич <v.lapeev@2gis.ru>
2014-04-11 10:57:31 +0000

    [ERM-3694] Erm.BL/Dev/OrderProfileWarning -> Erm.BL/Main RI
    git-tfs-id: [http://uk-tfs02:8080/tfs/DefaultCollection]$/ERM.BL/Main/BL;C19409

--- Source/EntryPoints/UI/Web/2Gis.Erm.BL.UI.Web.Mvc/Models/PrintOrderViewModel.cs
+++ Source/EntryPoints/UI/Web/2Gis.Erm.BL.UI.Web.Mvc/Models/PrintOrderViewModel.cs
@@ -4,12 +4,15 @@ using DoubleGis.Erm.Platform.UI.Web.Mvc.ViewModels;
 
 namespace DoubleGis.Erm.BL.UI.Web.Mvc.Models
 {
+    // TODO {all, 09.04.2014}: возможно, стоит унифицировать форму выбора профиля юрлица при печати заказа и счета
     public sealed class PrintOrderViewModel : ViewModel
     {
         [RequiredLocalized]
         public LookupField LegalPersonProfile { get; set; }
 
         public PrintOrderType PrintOrderType { get; set; }
+
+        public long DefaultLegalPersonProfileId { get; set; }
 
         public long LegalPersonId { get; set; }
 
