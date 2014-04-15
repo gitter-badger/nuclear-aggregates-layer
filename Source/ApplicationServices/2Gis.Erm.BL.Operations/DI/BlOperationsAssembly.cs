ee03d40ab8fd6c8fa1bf500d5c7fc7b266be6276
Речкалов Андрей Михайлович <a.rechkalov@2Gis.ru>
2014-04-15 04:35:13 +0000

    [NA] Erm.BL\EagerLoading -> Erm.BL\Main RI
    git-tfs-id: [http://uk-tfs02:8080/tfs/DefaultCollection]$/ERM.BL/Main/BL;C19538

---
+++ Source/ApplicationServices/2Gis.Erm.BL.Operations/DI/BlOperationsAssembly.cs
@@ -0,0 +1,12 @@
+﻿using DoubleGis.Erm.BLCore.API.Operations.DI;
+using DoubleGis.Erm.BLCore.Common.Infrastructure.Handlers;
+using DoubleGis.Erm.Platform.Model.Zones;
+
+namespace DoubleGis.Erm.BL.Operations.DI
+{
+    public class BlOperationsAssembly : IZoneAssembly<OperationsZone>,
+                                        IZoneAnchor<OperationsZone>,
+                                        IContainsType<IRequestHandler>
+    {
+    }
+}
