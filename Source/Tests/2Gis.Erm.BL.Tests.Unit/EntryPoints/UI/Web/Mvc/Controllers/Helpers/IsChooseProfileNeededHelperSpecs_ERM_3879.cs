149bb98cb0a0333b48110b1c5bfb8ed88224d3fb
Речкалов Андрей Михайлович <a.rechkalov@2Gis.ru>
2014-04-16 04:23:17 +0000

    [ERM-3879], рефакторинг теста по багу - суть прежняя, читаемость выше
    git-tfs-id: [http://uk-tfs02:8080/tfs/DefaultCollection]$/ERM.BL/Prerelease/BL;C19610

--- Source/Tests/2Gis.Erm.BL.Tests.Unit/EntryPoints/UI/Web/Mvc/Controllers/Helpers/IsChooseProfileNeededHelperSpecs_ERM_3879.cs
+++ Source/Tests/2Gis.Erm.BL.Tests.Unit/EntryPoints/UI/Web/Mvc/Controllers/Helpers/IsChooseProfileNeededHelperSpecs_ERM_3879.cs
@@ -19,20 +19,22 @@ namespace DoubleGis.Erm.BL.Tests.Unit.EntryPoints.UI.Web.Mvc.Controllers.Helpers
 {
     class IsChooseProfileNeededHelperSpecs_ERM_3879
     {
-
         static class Create
         {
             const long OrderId = 1;
             const long LegalPersonId = 2;
+            const long LegalPersonProfileId1 = 3;
+            const long LegalPersonProfileId2 = 3;
+            const long InvalidLegalPersonProfileId = 666;
 
             public static Order Order(bool isProfileSpecified, int profileCount)
             {
-                var profiles = new long[] { 3, 4, 5 }
+                var profiles = new [] { LegalPersonProfileId1, LegalPersonProfileId2 }
                     .Take(profileCount)
                     .Select(LegalPersonProfile)
                     .ToArray();
 
-                var profileId = profiles.Any() ? profiles.First().Id : 666;
+                var profileId = profiles.Any() ? profiles.First().Id : InvalidLegalPersonProfileId;
 
                 return new Order
                     {
@@ -56,7 +58,6 @@ namespace DoubleGis.Erm.BL.Tests.Unit.EntryPoints.UI.Web.Mvc.Controllers.Helpers
             {
                 var mock = Mock.Of<IOrderReadModel>();
                 Mock.Get(mock).Setup(model => model.GetOrder(Moq.It.IsAny<long>())).Returns(order);
-                var x = mock.GetOrder(123);
                 return mock;
             }
 
@@ -72,212 +73,144 @@ namespace DoubleGis.Erm.BL.Tests.Unit.EntryPoints.UI.Web.Mvc.Controllers.Helpers
             }
         }
 
-        #region Order
-        class When_OrderHasNoProfile_NoProfiles_PrintOrder
+        private static class ProfileSpecified
         {
-            static Order Order;
-            static IsChooseProfileNeededHelper Helper;
-            static IsChooseProfileNeededHelper.ChooseProfileDialogState Result;
-
-            Establish context = () =>
+            public interface IArgument
             {
-                Order = Create.Order(isProfileSpecified: false, profileCount: 0);
-                Helper = new IsChooseProfileNeededHelper(Create.OrderReadModel(Order), Create.LegalPersonRepository(Order));
-            };
+                bool IsProfileSpecified { get; }
+            }
 
-            Because of = () => Result = Helper.GetChooseProfileDialogState(Order.Id, PrintOrderType.PrintOrder);
+            public class True : IArgument
+            {
+                public bool IsProfileSpecified { get { return true; } }
+            }
 
-            It should_not_fail = () => Result.IsChooseProfileNeeded.Should().Be(true);
+            public class False : IArgument
+            {
+                public bool IsProfileSpecified { get { return false; } }
+            }
         }
 
-        class When_OrderHasNoProfile_SingleProfile_PrintOrder
+        private static class ProfileCount
         {
-            static Order Order;
-            static IsChooseProfileNeededHelper Helper;
-            static IsChooseProfileNeededHelper.ChooseProfileDialogState Result;
+            public interface IArgument
+            {
+                int ProfileCount { get; }
+            }
 
+            public class NoOne : IArgument
+            {
+                public int ProfileCount { get { return 0; } }
+            }
+
+            public class One : IArgument
+            {
+                public int ProfileCount { get { return 1; } }
+            }
+
+            public class MoreThanOne : IArgument
+            {
+                public int ProfileCount { get { return 2; } }
+            }
+        }
+
+        private static class PrintOrder
+        {
+            public interface IArgument
+            {
+                PrintOrderType PrintOrderType { get; }
+            }
+
+            public class True : IArgument
+            {
+                public PrintOrderType PrintOrderType { get { return PrintOrderType.PrintOrder; } }
+            }
+
+            public class False : IArgument
+            {
+                public PrintOrderType PrintOrderType { get { return PrintOrderType.PrintReferenceInformation; } }
+            }
+        }
+        
+        private abstract class Context
+        {
+            protected static Order Order;
+            protected static IsChooseProfileNeededHelper Helper;
+            protected static IsChooseProfileNeededHelper.ChooseProfileDialogState Result;
+        }
+
+        private abstract class Context<TProfileSpecified, TPrintOrderType, TProfileCount> : Context
+            where TProfileSpecified : ProfileSpecified.IArgument, new()
+            where TProfileCount : ProfileCount.IArgument, new()
+            where TPrintOrderType : PrintOrder.IArgument, new()
+        {
             Establish context = () =>
                 {
-                    Order = Create.Order(isProfileSpecified: false, profileCount: 1);
+                    Order = Create.Order(new TProfileSpecified().IsProfileSpecified, new TProfileCount().ProfileCount);
                     Helper = new IsChooseProfileNeededHelper(Create.OrderReadModel(Order), Create.LegalPersonRepository(Order));
                 };
 
-            Because of = () => Result = Helper.GetChooseProfileDialogState(Order.Id, PrintOrderType.PrintOrder);
-
-            It should_not_fail = () => Result.IsChooseProfileNeeded.Should().Be(false);
+            Because of = () => Result = Helper.GetChooseProfileDialogState(Order.Id, new TPrintOrderType().PrintOrderType);
         }
 
-        class When_OrderHasNoProfile_MultipleProfiles_PrintOrder
+        class When_OrderHasNoProfile_NoProfiles_PrintOrder : Context<ProfileSpecified.False, PrintOrder.True, ProfileCount.NoOne>
         {
-            static Order Order;
-            static IsChooseProfileNeededHelper Helper;
-            static IsChooseProfileNeededHelper.ChooseProfileDialogState Result;
-
-            Establish context = () =>
-            {
-                Order = Create.Order(isProfileSpecified: false, profileCount: 2);
-                Helper = new IsChooseProfileNeededHelper(Create.OrderReadModel(Order), Create.LegalPersonRepository(Order));
-            };
-
-            Because of = () => Result = Helper.GetChooseProfileDialogState(Order.Id, PrintOrderType.PrintOrder);
-
             It should_not_fail = () => Result.IsChooseProfileNeeded.Should().Be(true);
         }
 
-        class When_OrderHasProfile_NoProfile_PrintOrder
+        class When_OrderHasNoProfile_SingleProfile_PrintOrder : Context<ProfileSpecified.False, PrintOrder.True, ProfileCount.One>
         {
-            static Order Order;
-            static IsChooseProfileNeededHelper Helper;
-            static IsChooseProfileNeededHelper.ChooseProfileDialogState Result;
-
-            Establish context = () =>
-            {
-                Order = Create.Order(isProfileSpecified: true, profileCount: 0);
-                Helper = new IsChooseProfileNeededHelper(Create.OrderReadModel(Order), Create.LegalPersonRepository(Order));
-            };
-
-            Because of = () => Result = Helper.GetChooseProfileDialogState(Order.Id, PrintOrderType.PrintOrder);
-
-            It should_not_fail = () => Result.IsChooseProfileNeeded.Should().Be(true);
-        }
-
-        class When_OrderHasProfile_SingleProfile_PrintOrder
-        {
-            static Order Order;
-            static IsChooseProfileNeededHelper Helper;
-            static IsChooseProfileNeededHelper.ChooseProfileDialogState Result;
-
-            Establish context = () =>
-            {
-                Order = Create.Order(isProfileSpecified: true, profileCount: 1);
-                Helper = new IsChooseProfileNeededHelper(Create.OrderReadModel(Order), Create.LegalPersonRepository(Order));
-            };
-
-            Because of = () => Result = Helper.GetChooseProfileDialogState(Order.Id, PrintOrderType.PrintOrder);
-
             It should_not_fail = () => Result.IsChooseProfileNeeded.Should().Be(false);
         }
 
-        class When_OrderHasProfile_MultipleProfiles_PrintOrder
+        class When_OrderHasNoProfile_MultipleProfiles_PrintOrder : Context<ProfileSpecified.False, PrintOrder.True, ProfileCount.MoreThanOne>
         {
-            static Order Order;
-            static IsChooseProfileNeededHelper Helper;
-            static IsChooseProfileNeededHelper.ChooseProfileDialogState Result;
+            It should_not_fail = () => Result.IsChooseProfileNeeded.Should().Be(true);
+        }
 
-            Establish context = () =>
-                {
-                    Order = Create.Order(isProfileSpecified: true, profileCount: 2);
-                    Helper = new IsChooseProfileNeededHelper(Create.OrderReadModel(Order), Create.LegalPersonRepository(Order));
-                };
+        class When_OrderHasProfile_NoProfile_PrintOrder : Context<ProfileSpecified.True, PrintOrder.True, ProfileCount.NoOne>
+        {
+            It should_not_fail = () => Result.IsChooseProfileNeeded.Should().Be(true);
+        }
 
-            Because of = () => Result = Helper.GetChooseProfileDialogState(Order.Id, PrintOrderType.PrintOrder);
+        class When_OrderHasProfile_SingleProfile_PrintOrder : Context<ProfileSpecified.True, PrintOrder.True, ProfileCount.One>
+        {
+            It should_not_fail = () => Result.IsChooseProfileNeeded.Should().Be(false);
+        }
 
+        class When_OrderHasProfile_MultipleProfiles_PrintOrder : Context<ProfileSpecified.True, PrintOrder.True, ProfileCount.MoreThanOne>
+        {
             It should_not_fail = () => Result.IsChooseProfileNeeded.Should().Be(true);
         } 
-        #endregion
 
-        #region PrintReferenceInformation
-        class When_OrderHasNoProfile_NoProfiles_PrintNotOrder
+        class When_OrderHasNoProfile_NoProfiles_PrintNotOrder : Context<ProfileSpecified.False, PrintOrder.False, ProfileCount.NoOne>
         {
-            static Order Order;
-            static IsChooseProfileNeededHelper Helper;
-            static IsChooseProfileNeededHelper.ChooseProfileDialogState Result;
-
-            Establish context = () =>
-                {
-                    Order = Create.Order(isProfileSpecified: false, profileCount: 0);
-                    Helper = new IsChooseProfileNeededHelper(Create.OrderReadModel(Order), Create.LegalPersonRepository(Order));
-                };
-
-            Because of = () => Result = Helper.GetChooseProfileDialogState(Order.Id, PrintOrderType.PrintReferenceInformation);
-
             It should_not_fail = () => Result.IsChooseProfileNeeded.Should().Be(true);
         }
 
-        class When_OrderHasNoProfile_SingleProfile_PrintNotOrder
+        class When_OrderHasNoProfile_SingleProfile_PrintNotOrder : Context<ProfileSpecified.False, PrintOrder.False, ProfileCount.One>
         {
-            static Order Order;
-            static IsChooseProfileNeededHelper Helper;
-            static IsChooseProfileNeededHelper.ChooseProfileDialogState Result;
-
-            Establish context = () =>
-                {
-                    Order = Create.Order(isProfileSpecified: false, profileCount: 1);
-                    Helper = new IsChooseProfileNeededHelper(Create.OrderReadModel(Order), Create.LegalPersonRepository(Order));
-                };
-
-            Because of = () => Result = Helper.GetChooseProfileDialogState(Order.Id, PrintOrderType.PrintReferenceInformation);
-
             It should_not_fail = () => Result.IsChooseProfileNeeded.Should().Be(false);
         }
 
-        class When_OrderHasNoProfile_MultipleProfiles_PrintNotOrder
+        class When_OrderHasNoProfile_MultipleProfiles_PrintNotOrder : Context<ProfileSpecified.False, PrintOrder.False, ProfileCount.MoreThanOne>
         {
-            static Order Order;
-            static IsChooseProfileNeededHelper Helper;
-            static IsChooseProfileNeededHelper.ChooseProfileDialogState Result;
-
-            Establish context = () =>
-                {
-                    Order = Create.Order(isProfileSpecified: false, profileCount: 2);
-                    Helper = new IsChooseProfileNeededHelper(Create.OrderReadModel(Order), Create.LegalPersonRepository(Order));
-                };
-
-            Because of = () => Result = Helper.GetChooseProfileDialogState(Order.Id, PrintOrderType.PrintReferenceInformation);
-
             It should_not_fail = () => Result.IsChooseProfileNeeded.Should().Be(true);
         }
 
-        class When_OrderHasProfile_NoProfile_PrintNotOrder
+        class When_OrderHasProfile_NoProfile_PrintNotOrder : Context<ProfileSpecified.True, PrintOrder.False, ProfileCount.NoOne>
         {
-            static Order Order;
-            static IsChooseProfileNeededHelper Helper;
-            static IsChooseProfileNeededHelper.ChooseProfileDialogState Result;
-
-            Establish context = () =>
-            {
-                Order = Create.Order(isProfileSpecified: true, profileCount: 0);
-                Helper = new IsChooseProfileNeededHelper(Create.OrderReadModel(Order), Create.LegalPersonRepository(Order));
-            };
-
-            Because of = () => Result = Helper.GetChooseProfileDialogState(Order.Id, PrintOrderType.PrintReferenceInformation);
-
             It should_not_fail = () => Result.IsChooseProfileNeeded.Should().Be(true);
         }
 
-        class When_OrderHasProfile_SingleProfile_PrintNotOrder
+        class When_OrderHasProfile_SingleProfile_PrintNotOrder : Context<ProfileSpecified.True, PrintOrder.False, ProfileCount.One>
         {
-            static Order Order;
-            static IsChooseProfileNeededHelper Helper;
-            static IsChooseProfileNeededHelper.ChooseProfileDialogState Result;
-
-            Establish context = () =>
-            {
-                Order = Create.Order(isProfileSpecified: true, profileCount: 1);
-                Helper = new IsChooseProfileNeededHelper(Create.OrderReadModel(Order), Create.LegalPersonRepository(Order));
-            };
-
-            Because of = () => Result = Helper.GetChooseProfileDialogState(Order.Id, PrintOrderType.PrintReferenceInformation);
-
             It should_not_fail = () => Result.IsChooseProfileNeeded.Should().Be(false);
         }
 
-        class When_OrderHasProfile_MultipleProfiles_PrintNotOrder
+        class When_OrderHasProfile_MultipleProfiles_PrintNotOrder : Context<ProfileSpecified.True, PrintOrder.False, ProfileCount.MoreThanOne>
         {
-            static Order Order;
-            static IsChooseProfileNeededHelper Helper;
-            static IsChooseProfileNeededHelper.ChooseProfileDialogState Result;
-
-            Establish context = () =>
-            {
-                Order = Create.Order(isProfileSpecified: true, profileCount: 2);
-                Helper = new IsChooseProfileNeededHelper(Create.OrderReadModel(Order), Create.LegalPersonRepository(Order));
-            };
-
-            Because of = () => Result = Helper.GetChooseProfileDialogState(Order.Id, PrintOrderType.PrintReferenceInformation);
-
             It should_not_fail = () => Result.IsChooseProfileNeeded.Should().Be(false);
         } 
-        #endregion
     }
 }
