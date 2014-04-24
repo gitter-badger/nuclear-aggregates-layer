a369cbae231ffda2cba3c96424bd6cf497048c7b
Тукаев Александр Юрьевич <a.tukaev@2Gis.ru>
2014-04-24 09:23:40 +0000

    [NA] Composition/NSMReleasing RI
    git-tfs-id: [http://uk-tfs02:8080/tfs/DefaultCollection]$/ERM.Composition/Main/CompositionRoots;C19924

--- Source/Tests/2Gis.Erm.Tests.Integration.InProc/DI/TestIntegrationInProcAssembly.cs
+++ Source/Tests/2Gis.Erm.Tests.Integration.InProc/DI/TestIntegrationInProcAssembly.cs
@@ -4,8 +4,8 @@ using DoubleGis.Erm.Tests.Integration.InProc.Suite.Infrastructure;
 namespace DoubleGis.Erm.Tests.Integration.InProc.DI
 {
     public sealed class TestIntegrationInProcAssembly : IZoneAssembly<IntegrationTestsZone>,
-                                                        IZoneAnchor<IntegrationTestsZone>,
-                                                        IContainsType<IIntegrationTest>
+                                                          IZoneAnchor<IntegrationTestsZone>,
+                                                          IContainsType<IIntegrationTest>
     {
     }
 }
