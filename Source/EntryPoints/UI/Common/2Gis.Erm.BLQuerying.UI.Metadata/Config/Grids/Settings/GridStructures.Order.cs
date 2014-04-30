0d0200cd74a230cb5e8e9359f040ca3665f57364
Пашук Максим Владимирович <m.pashuk@2gis.ru>
2014-04-30 08:53:44 +0000

    MMR
    git-tfs-id: [http://uk-tfs02:8080/tfs/DefaultCollection]$/ERM.Research/Main/BLQuerying;C20054

--- Source/EntryPoints/UI/Common/2Gis.Erm.BLQuerying.UI.Metadata/Config/Grids/Settings/GridStructures.Order.cs
+++ Source/EntryPoints/UI/Common/2Gis.Erm.BLQuerying.UI.Metadata/Config/Grids/Settings/GridStructures.Order.cs
@@ -1,12 +1,12 @@
 ﻿using DoubleGis.Erm.BLCore.UI.Metadata.Config.Grids;
-using DoubleGis.Erm.Platform.Model.Entities;
+using DoubleGis.Erm.Platform.Model.Entities.Erm;
 
 namespace DoubleGis.Erm.BLQuerying.UI.Metadata.Config.Grids.Settings
 {
     public partial class GridStructures
     {
-        public static GridStructure Order = GridStructure.Config
-            .For(EntityName.Order)
+        public static GridMetadata Order = GridMetadata.Config
+            .For<Order>()
             .AttachDataLists(
                 DataLists.Configuration.DataLists.Orders,
                 DataLists.Configuration.DataLists.ActiveOrders);
