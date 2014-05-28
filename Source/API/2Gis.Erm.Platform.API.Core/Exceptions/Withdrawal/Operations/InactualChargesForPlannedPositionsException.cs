80757435ae01618983dd222ab595a74ff1906a4d
Масленников Иван Евгеньевич <i.maslennikov@2gis.ru>
2014-05-28 07:05:28 +0000

    Core.NSMW RI
    git-tfs-id: [http://uk-tfs02:8080/tfs/DefaultCollection]$/ERM.Core/Main/Platform;C20767

---
+++ Source/API/2Gis.Erm.Platform.API.Core/Exceptions/Withdrawal/Operations/InactualChargesForPlannedPositionsException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace DoubleGis.Erm.Platform.API.Core.Exceptions.Withdrawal.Operations
+{
+    public class InactualChargesForPlannedPositionsException : BusinessLogicException
+    {
+        public InactualChargesForPlannedPositionsException()
+        {
+        }
+
+        public InactualChargesForPlannedPositionsException(string message) : base(message)
+        {
+        }
+
+        public InactualChargesForPlannedPositionsException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected InactualChargesForPlannedPositionsException(SerializationInfo serializationInfo, StreamingContext streamingContext)
+            : base(serializationInfo, streamingContext)
+        {
+        }
+    }
+}
