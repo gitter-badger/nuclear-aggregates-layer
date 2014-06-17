3b18a2cce879fff264b7c3408f338ad12d7cede4
Тукаев Александр Юрьевич <a.tukaev@2Gis.ru>
2014-06-17 03:20:52 +0000

    [NA] Core/PositionsExport RI
    git-tfs-id: [http://uk-tfs02:8080/tfs/DefaultCollection]$/ERM.Core/Main/Platform;C21205

---
+++ Source/Model/2Gis.Erm.Platform.Model/Entities/Erm/Partial/ExportFlowNomenclatures_NomenclatureElement.cs
@@ -0,0 +1,8 @@
+﻿using DoubleGis.Erm.Platform.Model.Entities.Interfaces.Integration;
+
+namespace DoubleGis.Erm.Platform.Model.Entities.Erm
+{
+    public partial class ExportFlowNomenclatures_NomenclatureElement : IIntegrationProcessorState
+    {
+    }
+}
