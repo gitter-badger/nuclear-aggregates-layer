712c4e9b8d7b2f00b0397590cd46e59547c3d0af
Иванов Денис Игоревич <d.ivanov@2gis.ru>
2014-06-24 08:16:39 +0000

    [ERM-4259] Добавить проверку о возможности сборки города в текущей инсталляции - done
    git-tfs-id: [http://uk-tfs02:8080/tfs/DefaultCollection]$/ERM.Core/Main/Platform;C21549

---
+++ Source/Model/2Gis.Erm.Platform.Model.Metadata/Globalization/BusinessModelCountryMapping.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DoubleGis.Erm.Platform.Model.Metadata.Globalization
+{
+    public static class BusinessModelCountryMapping
+    {
+        // BusinessModel <-> [ISOCode, ..., ISOCode]
+        private static readonly Lazy<IDictionary<BusinessModel, IEnumerable<int>>> Mapping =
+            new Lazy<IDictionary<BusinessModel, IEnumerable<int>>>(() =>
+                                                                   new Dictionary<BusinessModel, IEnumerable<int>>
+                                                                       {
+                                                                           { BusinessModel.Russia, new[] { 643 } },
+                                                                           { BusinessModel.Cyprus, new[] { 196 } },
+                                                                           { BusinessModel.Czech, new[] { 203 } },
+                                                                           { BusinessModel.Chile, new[] { 152 } },
+                                                                           { BusinessModel.Ukraine, new[] { 804 } },
+                                                                           { BusinessModel.Emirates, new[] { 784 } }
+                                                                       });
+
+        public static IEnumerable<int> GetCountryCodes(BusinessModel businessModel)
+        {
+            IEnumerable<int> countryCodes;
+            return Mapping.Value.TryGetValue(businessModel, out countryCodes) ? countryCodes : Enumerable.Empty<int>();
+        }
+    }
+}
