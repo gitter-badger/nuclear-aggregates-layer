fe430467cfd0e40aeaeb3dfb7f7bc114e0195599
Баранихин Юрий Владимирович <y.baranihin@2Gis.ru>
2014-06-25 08:48:00 +0000

    $/ERM.BL/Dev/Features/ERM-4230-NewEntity -> $/ERM.BL/Main RI
    git-tfs-id: [http://uk-tfs02:8080/tfs/DefaultCollection]$/ERM.BL/Main/BL;C21609

---
+++ Source/ApplicationServices/2Gis.Erm.BL.Aggregates/DomainEntityObtainers/DenialReasonObtainer.cs
@@ -0,0 +1,33 @@
+﻿using DoubleGis.Erm.BLCore.API.Operations.Generic.Modify.DomainEntityObtainers;
+using DoubleGis.Erm.Platform.DAL;
+using DoubleGis.Erm.Platform.DAL.Specifications;
+using DoubleGis.Erm.Platform.Model.Entities.DTOs;
+using DoubleGis.Erm.Platform.Model.Entities.Erm;
+using DoubleGis.Erm.Platform.Model.Entities.Interfaces;
+
+namespace DoubleGis.Erm.BL.Aggregates.DomainEntityObtainers
+{
+    public sealed class DenialReasonObtainer : ISimplifiedModelEntityObtainer<DenialReason>
+    {
+        private readonly IFinder _finder;
+
+        public DenialReasonObtainer(IFinder finder)
+        {
+            _finder = finder;
+        }
+
+        public DenialReason ObtainSimplifiedModelEntity(IDomainEntityDto domainEntityDto)
+        {
+            var dto = (DenialReasonDomainEntityDto)domainEntityDto;
+            var entity = _finder.FindOne(Specs.Find.ById<DenialReason>(dto.Id))
+                         ?? new DenialReason { Id = dto.Id, IsActive = true };
+
+            entity.Name = dto.Name;
+            entity.Description = dto.Description;
+            entity.ProofLink = dto.ProofLink;
+            entity.Type = (int)dto.Type;
+            entity.Timestamp = dto.Timestamp;
+            return entity;
+        }
+    }
+}
