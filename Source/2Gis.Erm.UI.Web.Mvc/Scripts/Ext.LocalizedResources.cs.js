737e9821310ecb4b959603bef91dcdf5d6d116ac
Захаров Филипп Александрович <f.zaharov@2Gis.ru>
2014-07-17 07:31:47 +0000

    ERM-4516
    git-tfs-id: [http://uk-tfs02:8080/tfs/DefaultCollection]$/ERM.Composition/Prerelease/CompositionRoots;C22399

--- Source/2Gis.Erm.UI.Web.Mvc/Scripts/Ext.LocalizedResources.cs.js
+++ Source/2Gis.Erm.UI.Web.Mvc/Scripts/Ext.LocalizedResources.cs.js
@@ -1,4 +1,4 @@
-﻿// File updated : 06.06.2014 17:52:28
+﻿// File updated : 17.07.2014 13:10:13
 // Processed resource entry host types:
 // DoubleGis.Erm.BL.Resources.Client.Properties.Resources
 // DoubleGis.Erm.Platform.Resources.Client.ResPlatformUI
