7981c1705b4424f2725deb529825bb4cd4595c91
Чурбанов Алексей Валерьевич <a.churbanov@2Gis.ru>
2014-08-11 05:54:19 +0000

    [NA] Тестовые среды перезжают на короткие имена доменов
    git-tfs-id: [http://uk-tfs02:8080/tfs/DefaultCollection]$/Erm.Core/Prerelease/BLCore;C23142

--- Source/ApplicationServices/2Gis.Erm.BLCore.MsCRM.Plugins/Customizations/Hack/_static/_common/scripts/Global.js
+++ Source/ApplicationServices/2Gis.Erm.BLCore.MsCRM.Plugins/Customizations/Hack/_static/_common/scripts/Global.js
@@ -919,15 +919,15 @@ window.CrmUrlToErmUrlTemplateMapping =
 
     // test environments
     { CrmUrl: "http://uk-erm-edu01/DoubleGis", ErmUrlTemplate: "//web-app.edu.erm.2gis.ru/Crm/CreateOrUpdate/{0}?crmId={1}", ErmUrl: "//web-app.edu.erm.2gis.ru/" },
-    { CrmUrl: "http://uk-erm-load03/DoubleGis", ErmUrlTemplate: "//web-app.load.erm.russia/Crm/CreateOrUpdate/{0}?crmId={1}", ErmUrl: "//web-app.load.erm.russia/" },
-    { CrmUrl: "http://uk-erm-load04/DoubleGis", ErmUrlTemplate: "//web-app.load.erm.russia/Crm/CreateOrUpdate/{0}?crmId={1}", ErmUrl: "//web-app.load.erm.russia/" },
-    { CrmUrl: "http://uk-test-int02/DoubleGis", ErmUrlTemplate: "//web-app.int.erm.russia/Crm/CreateOrUpdate/{0}?crmId={1}", ErmUrl: "//web-app.int.erm.russia/" },
-    { CrmUrl: "http://uk-erm-test01/DoubleGis01", ErmUrlTemplate: "//web-app01.test.erm.russia/Crm/CreateOrUpdate/{0}?crmId={1}", ErmUrl: "//web-app01.test.erm.russia/" },
-    { CrmUrl: "http://uk-erm-test02/DoubleGis02", ErmUrlTemplate: "//web-app02.test.erm.russia/Crm/CreateOrUpdate/{0}?crmId={1}", ErmUrl: "//web-app02.test.erm.russia/" },
-    { CrmUrl: "http://uk-erm-test03/DoubleGis03", ErmUrlTemplate: "//web-app03.test.erm.russia/Crm/CreateOrUpdate/{0}?crmId={1}", ErmUrl: "//web-app03.test.erm.russia/" },
-    { CrmUrl: "http://uk-erm-test04/DoubleGis04", ErmUrlTemplate: "//web-app04.test.erm.russia/Crm/CreateOrUpdate/{0}?crmId={1}", ErmUrl: "//web-app04.test.erm.russia/" },
-    { CrmUrl: "http://uk-erm-test05/DoubleGis05", ErmUrlTemplate: "//web-app05.test.erm.russia/Crm/CreateOrUpdate/{0}?crmId={1}", ErmUrl: "//web-app05.test.erm.russia/" },
-    { CrmUrl: "http://uk-erm-test06/DoubleGis06", ErmUrlTemplate: "//web-app06.test.erm.russia/Crm/CreateOrUpdate/{0}?crmId={1}", ErmUrl: "//web-app06.test.erm.russia/" }
+    { CrmUrl: "http://uk-erm-load03/DoubleGis", ErmUrlTemplate: "//web-app.load.erm.2gis.ru/Crm/CreateOrUpdate/{0}?crmId={1}", ErmUrl: "//web-app.load.erm.2gis.ru/" },
+    { CrmUrl: "http://uk-erm-load04/DoubleGis", ErmUrlTemplate: "//web-app.load.erm.2gis.ru/Crm/CreateOrUpdate/{0}?crmId={1}", ErmUrl: "//web-app.load.erm.2gis.ru/" },
+    { CrmUrl: "http://uk-test-int02/DoubleGis", ErmUrlTemplate: "//web-app.int.erm.2gis.ru/Crm/CreateOrUpdate/{0}?crmId={1}", ErmUrl: "//web-app.int.erm.2gis.ru/" },
+    { CrmUrl: "http://uk-erm-test01/DoubleGis01", ErmUrlTemplate: "//web-app01.test.erm.2gis.ru/Crm/CreateOrUpdate/{0}?crmId={1}", ErmUrl: "//web-app01.test.erm.2gis.ru/" },
+    { CrmUrl: "http://uk-erm-test02/DoubleGis02", ErmUrlTemplate: "//web-app02.test.erm.2gis.ru/Crm/CreateOrUpdate/{0}?crmId={1}", ErmUrl: "//web-app02.test.erm.2gis.ru/" },
+    { CrmUrl: "http://uk-erm-test03/DoubleGis03", ErmUrlTemplate: "//web-app03.test.erm.2gis.ru/Crm/CreateOrUpdate/{0}?crmId={1}", ErmUrl: "//web-app03.test.erm.2gis.ru/" },
+    { CrmUrl: "http://uk-erm-test04/DoubleGis04", ErmUrlTemplate: "//web-app04.test.erm.2gis.ru/Crm/CreateOrUpdate/{0}?crmId={1}", ErmUrl: "//web-app04.test.erm.2gis.ru/" },
+    { CrmUrl: "http://uk-erm-test05/DoubleGis05", ErmUrlTemplate: "//web-app05.test.erm.2gis.ru/Crm/CreateOrUpdate/{0}?crmId={1}", ErmUrl: "//web-app05.test.erm.2gis.ru/" },
+    { CrmUrl: "http://uk-erm-test06/DoubleGis06", ErmUrlTemplate: "//web-app06.test.erm.2gis.ru/Crm/CreateOrUpdate/{0}?crmId={1}", ErmUrl: "//web-app06.test.erm.2gis.ru/" }
 ];
 
 window.CrmEntityMapping =
