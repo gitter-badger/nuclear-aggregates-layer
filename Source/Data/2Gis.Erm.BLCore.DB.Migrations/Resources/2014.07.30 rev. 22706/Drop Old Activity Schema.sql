7d7a6cf53741f631ce0d1c2c182376fd5105f1b1
Сергей Помадин <s.pomadin@2Gis.ru>
2014-08-13 03:41:39 +0000

    [ERM-4646] fixed delete order
    git-tfs-id: [http://uk-tfs02:8080/tfs/DefaultCollection]$/ERM.MW/Dev/Features/ActivityMigration/BLCore;C23260

--- Source/Data/2Gis.Erm.BLCore.DB.Migrations/Resources/2014.07.30 rev. 22706/Drop Old Activity Schema.sql
+++ Source/Data/2Gis.Erm.BLCore.DB.Migrations/Resources/2014.07.30 rev. 22706/Drop Old Activity Schema.sql
@@ -1,2 +1,2 @@
-﻿IF object_id('[Activity].[ActivityInstances]') IS NOT NULL DROP TABLE [Activity].ActivityInstances;
-IF object_id('[Activity].[ActivityPropertyInstances]') IS NOT NULL DROP TABLE [Activity].ActivityPropertyInstances;
+﻿IF object_id('[Activity].[ActivityPropertyInstances]') IS NOT NULL DROP TABLE [Activity].ActivityPropertyInstances;
+IF object_id('[Activity].[ActivityInstances]') IS NOT NULL DROP TABLE [Activity].ActivityInstances;
