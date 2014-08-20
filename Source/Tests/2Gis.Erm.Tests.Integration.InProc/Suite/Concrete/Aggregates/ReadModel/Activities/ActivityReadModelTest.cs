de1d0bfe52a15fbc759a638020402b5e0989a432
Сергей Помадин <s.pomadin@2Gis.ru>
2014-08-20 13:05:23 +0000

    [ERM-4646] follow up
    git-tfs-id: [http://uk-tfs02:8080/tfs/DefaultCollection]$/ERM.MW/Dev/Features/ActivityMigration/CompositionRoots;C23473

--- Source/Tests/2Gis.Erm.Tests.Integration.InProc/Suite/Concrete/Aggregates/ReadModel/Activities/ActivityReadModelTest.cs
+++ Source/Tests/2Gis.Erm.Tests.Integration.InProc/Suite/Concrete/Aggregates/ReadModel/Activities/ActivityReadModelTest.cs
@@ -2,9 +2,9 @@
 
 using DoubleGis.Erm.BLCore.API.Aggregates.Activities.ReadModel;
 using DoubleGis.Erm.Platform.DAL;
+using DoubleGis.Erm.Platform.DAL.Specifications;
 using DoubleGis.Erm.Platform.Model.Entities;
 using DoubleGis.Erm.Platform.Model.Entities.Activity;
-using DoubleGis.Erm.Platform.Model.Entities.Erm;
 using DoubleGis.Erm.Tests.Integration.InProc.Suite.Infrastructure;
 
 namespace DoubleGis.Erm.Tests.Integration.InProc.Suite.Concrete.Aggregates.ReadModel.Activities
@@ -22,32 +22,32 @@ namespace DoubleGis.Erm.Tests.Integration.InProc.Suite.Concrete.Aggregates.ReadM
 
         public ITestResult Execute()
         {
-			var appropriateAppointment = _finder.Find<AppointmentBase>(x => true).FirstOrDefault();
-			var appropriatePhonecall = _finder.Find<PhonecallBase>(x => true).FirstOrDefault();
-			var appropriateTask = _finder.Find<TaskBase>(x => true).FirstOrDefault();
-			var activityWithClient = _finder.Find<AppointmentBase>(x => x.AppointmentReferences.Any(
-				r => r.Reference == (int)ReferenceType.RegardingObject && r.ReferencedType == (int)EntityName.Client)).FirstOrDefault();
+	        var appropriateAppointment = _finder.FindMany(Specs.Find.Any<Appointment>()).FirstOrDefault();
+	        var appropriatePhonecall = _finder.FindMany(Specs.Find.Any<Phonecall>()).FirstOrDefault();
+	        var appropriateTask = _finder.FindMany(Specs.Find.Any<Task>()).FirstOrDefault();
+	        var reference = _finder.FindMany(Specs.Find.Custom<RegardingObject<Appointment>>(x => x.TargetEntityName == EntityName.Client)).FirstOrDefault();
+	        var activityWithClient = reference != null ? _finder.FindMany(Specs.Find.ById<Appointment>(reference.SourceEntityId)) : null;
 
-            if (appropriateAppointment == null || appropriatePhonecall == null || appropriateTask == null || activityWithClient == null)
-            {
-                return OrdinaryTestResult.As.NotExecuted;
-            }
+	        if (appropriateAppointment == null || appropriatePhonecall == null || appropriateTask == null || activityWithClient == null)
+	        {
+		        return OrdinaryTestResult.As.NotExecuted;
+	        }
 
-            var appointment = _activityReadModel.GetAppointment(appropriateAppointment.Id);
-            var task = _activityReadModel.GetTask(appropriateTask.Id);
-            var phonecall = _activityReadModel.GetPhonecall(appropriatePhonecall.Id);
+	        var appointment = _activityReadModel.GetAppointment(appropriateAppointment.Id);
+	        var task = _activityReadModel.GetTask(appropriateTask.Id);
+	        var phonecall = _activityReadModel.GetPhonecall(appropriatePhonecall.Id);
 
-            // ReSharper disable once PossibleInvalidOperationException
-			_activityReadModel.CheckIfRelatedActivitiesExists(activityWithClient.AppointmentReferences.First(r => r.ReferencedType == (int)EntityName.Client).ReferencedObjectId);
+	        // ReSharper disable once PossibleInvalidOperationException
+	        _activityReadModel.CheckIfRelatedActivitiesExists(reference.TargetEntityId);
 
-            return new object[]
-                {
-                    appointment,
-                    task,
-                    phonecall
-                }.Any(x => x == null)
-                       ? OrdinaryTestResult.As.Failed
-                       : OrdinaryTestResult.As.Succeeded;
+	        return new object[]
+		        {
+			        appointment,
+			        task,
+			        phonecall
+		        }.Any(x => x == null)
+		               ? OrdinaryTestResult.As.Failed
+		               : OrdinaryTestResult.As.Succeeded;
         }
     }
 }
