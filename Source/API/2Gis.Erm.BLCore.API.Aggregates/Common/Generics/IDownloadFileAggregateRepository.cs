8688550e5d0f4ca9327baa248f05e705537a9a63
Тукаев Александр Юрьевич <a.tukaev@2Gis.ru>
2014-08-20 09:33:53 +0000

    [NA] расставил Obsolete на generic контракты агрегирующих репозиториев
    git-tfs-id: [http://uk-tfs02:8080/tfs/DefaultCollection]$/ERM.Core/Main/BLCore;C23458

--- Source/API/2Gis.Erm.BLCore.API.Aggregates/Common/Generics/IDownloadFileAggregateRepository.cs
+++ Source/API/2Gis.Erm.BLCore.API.Aggregates/Common/Generics/IDownloadFileAggregateRepository.cs
@@ -1,4 +1,6 @@
-﻿using DoubleGis.Erm.BLCore.API.Operations.Concrete.Old.Common;
+﻿using System;
+
+using DoubleGis.Erm.BLCore.API.Operations.Concrete.Old.Common;
 using DoubleGis.Erm.BLCore.API.Operations.Generic.File;
 using DoubleGis.Erm.Platform.Model.Aggregates;
 using DoubleGis.Erm.Platform.Model.Entities.Interfaces;
@@ -6,6 +8,7 @@ using DoubleGis.Erm.Platform.Model.Identities.Operations.Identity.Generic;
 
 namespace DoubleGis.Erm.BLCore.API.Aggregates.Common.Generics
 {
+    [Obsolete("Use non-generic interface marked with IAggregateSpecificOperation")]
     public interface IDownloadFileAggregateRepository<TEntity> : IUnknownAggregateSpecificOperation<DownloadIdentity>
         where TEntity : class, IEntity, IEntityKey
     {
