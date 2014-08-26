0c6f50e3a58f3bc1319bde08871d9042a10a7ced
Речкалов Андрей Михайлович <a.rechkalov@2Gis.ru>
2014-08-26 03:55:32 +0000

    [NA] ERM.MW\ActivityMigration -> Erm.BL\ActivityMigration RI
    git-tfs-id: [http://uk-tfs02:8080/tfs/DefaultCollection]$/ERM.BL/Dev/Features/ActivityMigration/BLFlex;C23615

--- Source/EntryPoints/UI/Web/2Gis.Erm.BLFlex.UI.Web.Mvc.Global/Services/Cards/MultiCultureAppointmentViewModelCustomizationService.cs
+++ Source/EntryPoints/UI/Web/2Gis.Erm.BLFlex.UI.Web.Mvc.Global/Services/Cards/MultiCultureAppointmentViewModelCustomizationService.cs
@@ -2,7 +2,7 @@ using System.Web.Mvc;
 
 using DoubleGis.Erm.BLCore.UI.Web.Mvc.Services.Cards;
 using DoubleGis.Erm.BLCore.UI.Web.Mvc.ViewModels;
-using DoubleGis.Erm.Platform.Model.Entities.Erm;
+using DoubleGis.Erm.Platform.Model.Entities.Activity;
 using DoubleGis.Erm.Platform.Model.Metadata.Globalization;
 
 namespace DoubleGis.Erm.BLFlex.UI.Web.Mvc.Global.Services.Cards
