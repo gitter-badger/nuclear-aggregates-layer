9d9d0b797826955732dfb8398ff02fa6be474d67
Иванов Денис Игоревич <d.ivanov@2gis.ru>
2014-08-28 08:18:56 +0000

    [NA] DistributedCache => Main RI
    git-tfs-id: [http://uk-tfs02:8080/tfs/DefaultCollection]$/ERM.Core/Main/Platform;C23742

---
+++ Source/EntryPoints/Common/2Gis.Erm.Platform.AppFabric/DI/AppFabricZone.cs
@@ -0,0 +1,8 @@
+﻿using DoubleGis.Erm.Platform.Model.Zones;
+
+namespace DoubleGis.Erm.Platform.AppFabric.DI
+{
+    public class AppFabricZone : IZone
+    {
+    }
+}
---
+++ Source/EntryPoints/Common/2Gis.Erm.Platform.AppFabric/DI/PlatformAppFabricAssembly.cs
@@ -0,0 +1,11 @@
+﻿using DoubleGis.Erm.Platform.Common.Caching;
+using DoubleGis.Erm.Platform.Model.Zones;
+
+namespace DoubleGis.Erm.Platform.AppFabric.DI
+{
+    public class PlatformAppFabricAssembly : IZoneAssembly<AppFabricZone>,
+                                             IZoneAnchor<AppFabricZone>,
+                                             IContainsType<ICacheAdapter>
+    {
+    }
+}
