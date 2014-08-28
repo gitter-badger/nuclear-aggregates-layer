23e35355912b450a5c2a06e4c71fea3254cc9422
Речкалов Андрей Михайлович <a.rechkalov@2Gis.ru>
2014-08-28 10:46:24 +0000

    [ERM-4712], операция смены профиля
    git-tfs-id: [http://uk-tfs02:8080/tfs/DefaultCollection]$/ERM.BL/Dev/Features/ERM-4712-Profile/Platform;C23764

---
+++ Source/Model/2Gis.Erm.Platform.Model/Identities/Operations/Identity/Specific/Order/ChangeOrderProfilesIdentity.cs
@@ -0,0 +1,21 @@
+﻿namespace DoubleGis.Erm.Platform.Model.Identities.Operations.Identity.Specific.Order
+{
+    public sealed class ChangeOrderProfilesIdentity : OperationIdentityBase<ChangeOrderProfilesIdentity>, INonCoupledOperationIdentity
+    {
+        public override int Id
+        {
+            get
+            {
+                return OperationIdentityIds.ChangeOrderProfilesIdentity;
+            }
+        }
+
+        public override string Description
+        {
+            get
+            {
+                return "Смена профилей в заказе";
+            }
+        }
+    }
+}
