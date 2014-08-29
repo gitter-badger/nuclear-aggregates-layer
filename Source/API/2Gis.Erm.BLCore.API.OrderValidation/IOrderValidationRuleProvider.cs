b40c678e9ea46e8efa10ebb2f0e7261e481431e9
Масленников Иван Евгеньевич <i.maslennikov@2gis.ru>
2014-08-29 05:44:51 +0000

    продолжение
    git-tfs-id: [http://uk-tfs02:8080/tfs/DefaultCollection]$/Erm.Core/Dev/Features/OVR/BLCore;C23778

---
+++ Source/API/2Gis.Erm.BLCore.API.OrderValidation/IOrderValidationRuleProvider.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace DoubleGis.Erm.BLCore.API.OrderValidation
+{
+    public interface IOrderValidationRuleProvider
+    {
+        IEnumerable<OrderValidationRuleContainer> GetAppropriateRules(ValidationType validationType);
+    }
+
+    public sealed class OrderValidationRuleContainer
+    {
+        public OrderValidationRuleContainer(OrderValidationRuleGroup group, IEnumerable<IOrderValidationRule> rules)
+        {
+            Group = group;
+            Rules = rules;
+        }
+
+        public OrderValidationRuleGroup Group { get; private set; }
+        public IEnumerable<IOrderValidationRule> Rules { get; private set; }
+    }
+}
---
+++ Source/ApplicationServices/2Gis.Erm.BLCore.OrderValidation/OrderValidationRuleProvider.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using DoubleGis.Erm.BLCore.API.OrderValidation;
+
+namespace DoubleGis.Erm.BLCore.OrderValidation
+{
+    public sealed class OrderValidationRuleProvider : IOrderValidationRuleProvider
+    {
+        private readonly IOrderValidationRule[] _allValidationRules;
+
+        private readonly IReadOnlyCollection<OrderValidationRuleGroup> _orderedValidationRuleGroupsSequence = new[]
+            {
+                OrderValidationRuleGroup.Generic,
+                OrderValidationRuleGroup.AdvertisementMaterialsValidation,
+                OrderValidationRuleGroup.ADPositionsValidation,
+                OrderValidationRuleGroup.AdvertisementAmountValidation
+            };
+
+        public OrderValidationRuleProvider(
+            // ReSharper disable ParameterTypeCanBeEnumerable.Local
+            IOrderValidationRule[] allValidationRules
+            // ReSharper restore ParameterTypeCanBeEnumerable.Local
+            )
+        {
+            _allValidationRules = allValidationRules;
+        }
+
+        public IEnumerable<OrderValidationRuleContainer> GetAppropriateRules(ValidationType validationType)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
