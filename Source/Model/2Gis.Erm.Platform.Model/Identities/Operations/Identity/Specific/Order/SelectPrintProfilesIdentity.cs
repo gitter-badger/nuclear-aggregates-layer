2498d0fc7bb3d2a361c7b5ac3eefd49be707137d
Речкалов Андрей Михайлович <a.rechkalov@2Gis.ru>
2014-09-01 07:59:41 +0000

    [ERM-4832], удалил использование профиля, передаваемого с клиента
    git-tfs-id: [http://uk-tfs02:8080/tfs/DefaultCollection]$/ERM.BL/Dev/Features/ERM-4712-Profile/Platform;C23813

---
+++ Source/Model/2Gis.Erm.Platform.Model/Identities/Operations/Identity/Specific/Order/SelectPrintProfilesIdentity.cs
@@ -0,0 +1,21 @@
+﻿namespace DoubleGis.Erm.Platform.Model.Identities.Operations.Identity.Specific.Order
+{
+    public sealed class SelectPrintProfilesIdentity : OperationIdentityBase<SelectPrintProfilesIdentity>, INonCoupledOperationIdentity
+    {
+        public override int Id
+        {
+            get
+            {
+                return OperationIdentityIds.SelectPrintProfilesIdentity;
+            }
+        }
+
+        public override string Description
+        {
+            get
+            {
+                return "Выбор профиля при печати";
+            }
+        }
+    }
+}
