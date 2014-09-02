0a46465dd2606e05a250b082570718099d73370a
Тукаев Александр Юрьевич <a.tukaev@2Gis.ru>
2014-09-02 06:52:51 +0000

    [NA] Core/WarehouseIntegration RI
    git-tfs-id: [http://uk-tfs02:8080/tfs/DefaultCollection]$/ERM.Core/Main/Platform;C23875

---
+++ Source/Model/2Gis.Erm.Platform.Model/Identities/Operations/Identity/Specific/Firm/CreateBlankFirmsIdentity.cs
@@ -0,0 +1,15 @@
+﻿namespace DoubleGis.Erm.Platform.Model.Identities.Operations.Identity.Specific.Firm
+{
+    public class CreateBlankFirmsIdentity : OperationIdentityBase<CreateBlankFirmsIdentity>, INonCoupledOperationIdentity
+    {
+        public override int Id
+        {
+            get { return OperationIdentityIds.CreateBlankFirmsIdentity; }
+        }
+
+        public override string Description
+        {
+            get { return "Операция создания пустых фирм"; }
+        }
+    }
+}
