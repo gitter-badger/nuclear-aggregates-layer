a3e1fdfac0bcef9500c84a809c25cf393e0a6ac4
Баранихин Юрий Владимирович <y.baranihin@2Gis.ru>
2014-09-03 06:08:24 +0000

    [ERM-4805] CR
    git-tfs-id: [http://uk-tfs02:8080/tfs/DefaultCollection]$/ERM.BL/Dev/Features/ERM-4712-Profile/BL;C23924

--- Source/EntryPoints/UI/Web/2Gis.Erm.BL.UI.Web.Mvc/Models/ChangeOrderProfilesViewModel.cs
+++ Source/EntryPoints/UI/Web/2Gis.Erm.BL.UI.Web.Mvc/Models/ChangeOrderProfilesViewModel.cs
@@ -4,6 +4,8 @@ using DoubleGis.Erm.Platform.UI.Web.Mvc.ViewModels;
 
 namespace DoubleGis.Erm.BL.UI.Web.Mvc.Models
 {
+    // FIXME {a.rechkalov, 03.09.2014}: Очень похожая штука есть: ChooseProfileViewModel 
+    // Предлагаю их объеденить. Но нужно не забыть про надписи в окошке (сейчас там слово заказы фигурирует)
     public sealed class ChangeOrderProfilesViewModel : ViewModel
     {
         [RequiredLocalized]
