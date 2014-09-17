24e13b9d6b59015b650334d0a2e69661662ae738
Сергей Помадин <s.pomadin@2Gis.ru>
2014-09-17 12:35:42 +0000

    [ERM-4848] updated deal stage changing
    git-tfs-id: [http://uk-tfs02:8080/tfs/DefaultCollection]$/ERM.MW/Dev/Features/ActivityMigration/BLFlex;C24388

--- Source/ApplicationServices/2Gis.Erm.BLFlex.Operations.Global/MultiCulture/Concrete/Old/Clients/MultiCultureCheckClientActivitiesHandler.cs
+++ Source/ApplicationServices/2Gis.Erm.BLFlex.Operations.Global/MultiCulture/Concrete/Old/Clients/MultiCultureCheckClientActivitiesHandler.cs
@@ -1,9 +1,11 @@
 ﻿using DoubleGis.Erm.BLCore.API.Aggregates.Activities.ReadModel;
+using DoubleGis.Erm.BLCore.API.Operations.Generic.Read;
 using DoubleGis.Erm.BLCore.API.Operations.Remote.Disqualify;
 using DoubleGis.Erm.BLCore.Common.Infrastructure.Handlers;
 using DoubleGis.Erm.BLCore.Resources.Server.Properties;
 using DoubleGis.Erm.Platform.API.Core.Exceptions;
 using DoubleGis.Erm.Platform.API.Core.Operations.RequestResponse;
+using DoubleGis.Erm.Platform.Model.Entities;
 using DoubleGis.Erm.Platform.Model.Metadata.Globalization;
 
 namespace DoubleGis.Erm.BLFlex.Operations.Global.MultiCulture.Concrete.Old.Clients
@@ -11,21 +13,11 @@ namespace DoubleGis.Erm.BLFlex.Operations.Global.MultiCulture.Concrete.Old.Clien
     public class MultiCultureCheckClientActivitiesHandler : RequestHandler<CheckClientActivitiesRequest, EmptyResponse>, IChileAdapted, ICyprusAdapted,
                                                             ICzechAdapted, IUkraineAdapted, IEmiratesAdapted
     {
-        private readonly IAppointmentReadModel _appointmentReadModel;
-        private readonly ILetterReadModel _letterReadModel;
-        private readonly IPhonecallReadModel _phonecallReadModel;
-        private readonly ITaskReadModel _taskReadModel;
+        private readonly IActivityReadService _activityReadService;
 
-        public MultiCultureCheckClientActivitiesHandler(
-            IAppointmentReadModel appointmentReadModel,
-            ILetterReadModel letterReadModel,
-            IPhonecallReadModel phonecallReadModel,
-            ITaskReadModel taskReadModel)
+        public MultiCultureCheckClientActivitiesHandler(IActivityReadService activityReadService)
         {
-            _appointmentReadModel = appointmentReadModel;
-            _letterReadModel = letterReadModel;
-            _phonecallReadModel = phonecallReadModel;
-            _taskReadModel = taskReadModel;
+            _activityReadService = activityReadService;
         }
 
         protected override EmptyResponse Handle(CheckClientActivitiesRequest request)
@@ -33,11 +25,7 @@ namespace DoubleGis.Erm.BLFlex.Operations.Global.MultiCulture.Concrete.Old.Clien
             // Проверяем открытые связанные объекты:
             // Проверяем наличие открытых Действий (Звонок, Встреча, Задача и пр.), связанных с данным Клиентом и его фирмами, 
             // если есть открытые Действия, выдается сообщение "Необходимо закрыть все активные действия с данным Клиентом и его фирмами".
-            var hasRelatedOpenedActivities =
-                _appointmentReadModel.CheckIfRelatedActivitiesExists(request.ClientId)
-                || _letterReadModel.CheckIfRelatedActivitiesExists(request.ClientId)
-                || _phonecallReadModel.CheckIfRelatedActivitiesExists(request.ClientId)
-                || _taskReadModel.CheckIfRelatedActivitiesExists(request.ClientId);
+            var hasRelatedOpenedActivities = _activityReadService.CheckIfRelatedActivitiesExists(EntityName.Client, request.ClientId);
 
             if (hasRelatedOpenedActivities)
             {
