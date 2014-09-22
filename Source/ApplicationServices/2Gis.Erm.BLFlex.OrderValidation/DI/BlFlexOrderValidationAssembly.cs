e1ccf84dcd0e7e0509ef09f438ba36d58fd360f2
Масленников Иван Евгеньевич <i.maslennikov@2gis.ru>
2014-09-22 06:22:54 +0000

    продолжение
    git-tfs-id: [http://uk-tfs02:8080/tfs/DefaultCollection]$/Erm.Core/Dev/Features/OVR/BLFlex;C24539

---
+++ Source/ApplicationServices/2Gis.Erm.BLFlex.OrderValidation/DI/BlFlexOrderValidationAssembly.cs
@@ -0,0 +1,12 @@
+﻿using DoubleGis.Erm.BLCore.API.OrderValidation.DI;
+using DoubleGis.Erm.Platform.Model.Metadata.Common.Provider.Sources;
+using DoubleGis.Erm.Platform.Model.Zones;
+
+namespace DoubleGis.Erm.BLFlex.OrderValidation.DI
+{
+    public sealed class BlFlexOrderValidationAssembly : IZoneAssembly<OrderValidationZone>,
+                                                        IZoneAnchor<OrderValidationZone>,
+                                                        IContainsType<IMetadataSource>
+    {
+    }
+}
