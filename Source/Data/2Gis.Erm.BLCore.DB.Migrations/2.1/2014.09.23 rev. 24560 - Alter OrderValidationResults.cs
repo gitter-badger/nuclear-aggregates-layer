03e1893798e88248f97d2c5544619838379a6d42
Масленников Иван Евгеньевич <i.maslennikov@2gis.ru>
2014-09-23 04:58:51 +0000

    продолжение
    git-tfs-id: [http://uk-tfs02:8080/tfs/DefaultCollection]$/Erm.Core/Dev/Features/OVR/BLCore;C24571

---
+++ Source/Data/2Gis.Erm.BLCore.DB.Migrations/2.1/2014.09.23 rev. 24560 - Alter OrderValidationResults.cs
@@ -0,0 +1,64 @@
+﻿using DoubleGis.Erm.BLCore.DB.Migrations.Shared;
+using DoubleGis.Erm.Platform.Migration.Base;
+using DoubleGis.Erm.Platform.Migration.Core;
+
+using Microsoft.SqlServer.Management.Smo;
+
+namespace DoubleGis.Erm.BLCore.DB.Migrations._2._1
+{
+    [Migration(24560, "Обновление таблицы OrderValidationResults, переход на использование версий заказов, отказ от foreignkey на заказы и т.п.", "i.maslennikov")]
+    public sealed class Migration24560 : TransactedMigration
+    {
+        protected override void ApplyOverride(IMigrationContext context)
+        {
+            DropOldSchemaTable(context);
+            CreateWithNewSchema(context);
+        }
+
+        private void DropOldSchemaTable(IMigrationContext context)
+        {
+            var table = context.Database.Tables[ErmTableNames.OrderValidationResults.Name, ErmTableNames.OrderValidationResults.Schema];
+            if (table == null)
+            {
+                return;
+            }
+
+            table.Drop();
+        }
+
+        private void CreateWithNewSchema(IMigrationContext context)
+        {
+            var table = new Table(context.Database, ErmTableNames.OrderValidationResults.Name, ErmTableNames.OrderValidationResults.Schema);
+
+            const string OrderIdColumnName = "OrderId";
+            const string ValidatorIdColumnName = "ValidatorId";
+            const string ValidVersionColumnName = "ValidVersion";
+
+            table.CreateField(OrderIdColumnName, DataType.BigInt, false);
+            table.CreateField(ValidatorIdColumnName, DataType.Int, false);
+            table.CreateField(ValidVersionColumnName, DataType.Binary(8), false);
+
+            table.Create();
+
+            string primaryKeyIndexName = string.Join("_",
+                                                     "PK",
+                                                     ErmTableNames.OrderValidationResults.Name,
+                                                     OrderIdColumnName,
+                                                     ValidatorIdColumnName,
+                                                     ValidVersionColumnName);
+
+            var primaryKey = new Index(table, primaryKeyIndexName);
+
+            var keyColumn = new IndexedColumn(primaryKey, OrderIdColumnName);
+            primaryKey.IndexedColumns.Add(keyColumn);
+            keyColumn = new IndexedColumn(primaryKey, ValidatorIdColumnName);
+            primaryKey.IndexedColumns.Add(keyColumn);
+            keyColumn = new IndexedColumn(primaryKey, ValidVersionColumnName);
+            primaryKey.IndexedColumns.Add(keyColumn);
+
+            primaryKey.IsClustered = false;
+            primaryKey.IndexKeyType = IndexKeyType.DriPrimaryKey;
+            primaryKey.Create();
+        }
+    }
+}
