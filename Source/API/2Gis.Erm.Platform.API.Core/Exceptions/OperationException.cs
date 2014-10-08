558d271f71a7c5b19b59047b80ee6d0d7f016bfb
Тукаев Александр Юрьевич <a.tukaev@2Gis.ru>
2014-10-08 04:35:51 +0000

    [NA], Erm.BL\Prerelease -> Erm.Core\Prerelease RI (pull request from BL)
    git-tfs-id: [http://uk-tfs02:8080/tfs/DefaultCollection]$/Erm.Core/Prerelease/Platform;C25112

---
+++ Source/API/2Gis.Erm.Platform.API.Core/Exceptions/OperationException.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+using DoubleGis.Erm.Platform.Model.Entities.Interfaces;
+using DoubleGis.Erm.Platform.Model.Identities.Operations.Identity;
+
+namespace DoubleGis.Erm.Platform.API.Core.Exceptions
+{
+    [Serializable]
+    public class OperationException<TEntity, TOperation> : BusinessLogicException
+        where TEntity : IEntityKey
+        where TOperation : IOperationIdentity
+    {
+        public OperationException()
+        {
+        }
+
+        public OperationException(string message)
+            : base(message)
+        {
+        }
+
+        public OperationException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+
+        protected OperationException(SerializationInfo serializationInfo, StreamingContext streamingContext)
+            : base(serializationInfo, streamingContext)
+        {
+        }
+    }
+}
