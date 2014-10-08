6a68887be64d738fae37f28edf5f4342c8972fe7
Пашук Максим Владимирович <m.pashuk@2gis.ru>
2014-10-08 11:27:29 +0000

    ERM-5059
    git-tfs-id: [http://uk-tfs02:8080/tfs/DefaultCollection]$/ERM.Composition/Prerelease/CompositionRoots;C25131

--- Source/2Gis.Erm.UI.Web.Mvc/Scripts/DataList/FirmDeal.DataList.js
+++ Source/2Gis.Erm.UI.Web.Mvc/Scripts/DataList/FirmDeal.DataList.js
@@ -1,10 +1,15 @@
 ﻿window.InitPage = function() {
     Ext.apply(this, {
         AppendFirms: function() {
+
+            var urlComponents = location.pathname.split('/');
+            var parentEntityId = urlComponents[5];
+
             var options = {
                 UrlParameters: {
                     HonourDataListFields: true,
-                    NameLocaleResourceId: 'DListActiveFirmsToAppend'
+                    NameLocaleResourceId: 'DListActiveFirmsToAppend',
+                    extendedInfo: "appendToDealId=" + parentEntityId
                 }
             }
 
