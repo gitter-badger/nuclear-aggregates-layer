371f6847a7edbcd22a4b4565f5ac13b4bb39d549
Тукаев Александр Юрьевич <a.tukaev@2Gis.ru>
2014-10-15 06:47:24 +0000

    [NA] Erm.BL\ERM-4827-LegalPersonDeactivation -> Erm.Core\Main RI (pull request from BL) + CR
    git-tfs-id: [http://uk-tfs02:8080/tfs/DefaultCollection]$/ERM.Core/Main/BLCore;C25290

---
+++ Source/API/2Gis.Erm.BLCore.API.Aggregates/LegalPersons/Operations/IDeleteLegalPersonAggregateService.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+using DoubleGis.Erm.Platform.Model.Aggregates;
+using DoubleGis.Erm.Platform.Model.Entities.Erm;
+using DoubleGis.Erm.Platform.Model.Identities.Operations.Identity.Generic;
+
+namespace DoubleGis.Erm.BLCore.API.Aggregates.LegalPersons.Operations
+{
+    public interface IDeleteLegalPersonAggregateService : IAggregateSpecificOperation<LegalPerson, DeleteIdentity>
+    {
+        void Delete(LegalPerson legalPerson, IEnumerable<LegalPersonProfile> profiles);
+    }
+}
