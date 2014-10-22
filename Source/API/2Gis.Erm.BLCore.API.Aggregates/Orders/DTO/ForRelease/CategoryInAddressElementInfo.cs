6d2916b0639f96ebac326f4610051d1a17cd4d38
Иванов Денис Игоревич <d.ivanov@2gis.ru>
2014-10-22 09:14:18 +0000

    [ERM-5096] ERM-5096-ExportIntegration => Main Core FI
    git-tfs-id: [http://uk-tfs02:8080/tfs/DefaultCollection]$/ERM.Core/Main/BLCore;C25466

--- Source/API/2Gis.Erm.BLCore.API.Aggregates/Orders/DTO/ForRelease/CategoryInAddressElementInfo.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace DoubleGis.Erm.BLCore.API.Aggregates.Orders.DTO.ForRelease
-{
-    public sealed class CategoryInAddressElementInfo
-    {
-        public long CategoryId { get; set; }
-        public long FirmAddressId { get; set; }
-    }
-}
