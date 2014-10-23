8fc5ffd9202f53ecdaed1d8b8b7db18c9fcebdc8
Речкалов Андрей Михайлович <a.rechkalov@2Gis.ru>
2014-10-23 09:29:20 +0000

    [ERM-5158], поддержка выбора времни в контроле даты
    git-tfs-id: [http://uk-tfs02:8080/tfs/DefaultCollection]$/ERM.BL/Dev/Features/ERM-2842-DateTime/Platform;C25522

--- Source/EntryPoints/UI/Web/2Gis.Erm.Platform.UI.Web.Mvc/Utils/CalendarSettings.cs
+++ Source/EntryPoints/UI/Web/2Gis.Erm.Platform.UI.Web.Mvc/Utils/CalendarSettings.cs
@@ -18,9 +18,24 @@ namespace DoubleGis.Erm.Platform.UI.Web.Mvc.Utils
 
         public StoreMode Store { get; set; }
         public DisplayMode Display { get; set; }
+        public TimeSettings Time { get; set; }
 
         public bool ReadOnly { get; set; }
         public DateTime? MinDate { get; set; }
         public DateTime? MaxDate { get; set; }
+
+        public class TimeSettings
+        {
+            public TimeSettings()
+            {
+                Start = TimeSpan.FromHours(0);
+                End = TimeSpan.FromDays(1).Add(TimeSpan.FromSeconds(-1));
+                Step = TimeSpan.FromMinutes(30);
+            }
+
+            public TimeSpan Start { get; set; }
+            public TimeSpan End { get; set; }
+            public TimeSpan Step { get; set; }
+        }
     }
 }
