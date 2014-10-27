fe9a1744f4191d81107d45f88a6716f8700b3f4e
Тукаев Александр Юрьевич <a.tukaev@2Gis.ru>
2014-10-27 05:13:32 +0000

    [NA] Core/TSC RI
    git-tfs-id: [http://uk-tfs02:8080/tfs/DefaultCollection]$/ERM.Core/Main/Platform;C25543

--- Source/API/2Gis.Erm.Platform.API.Core/Settings/ConnectionStrings/ConnectionStringsSettingsAspect.cs
+++ Source/API/2Gis.Erm.Platform.API.Core/Settings/ConnectionStrings/ConnectionStringsSettingsAspect.cs
@@ -9,40 +9,45 @@ namespace DoubleGis.Erm.Platform.API.Core.Settings.ConnectionStrings
 {
     public sealed class ConnectionStringsSettingsAspect : ISettingsAspect, IConnectionStringSettings
     {
-        private readonly IReadOnlyDictionary<ConnectionStringName, string> _connectionStringsMap;
+        private readonly IReadOnlyDictionary<ConnectionStringName, ConnectionStringSettings> _connectionStringsMap;
 
         public ConnectionStringsSettingsAspect()
         {
             var specifiedConnectionStringsMap = ConfigurationManager.ConnectionStrings.Cast<ConnectionStringSettings>().ToDictionary(connection => connection.Name);
 
-            var availableConnectionStringsMap = new Dictionary<ConnectionStringName, string>();
+            var availableConnectionStringsMap = new Dictionary<ConnectionStringName, ConnectionStringSettings>();
             foreach (var connectionStringAlias in Enum.GetValues(typeof(ConnectionStringName)).Cast<ConnectionStringName>().Where(x => x != ConnectionStringName.None))
             {
                 var connectionStringName = connectionStringAlias.ToDefaultConnectionStringName();
                 ConnectionStringSettings connection;
                 if (specifiedConnectionStringsMap.TryGetValue(connectionStringName, out connection))
                 {
-                    availableConnectionStringsMap[connectionStringAlias] = connection.ConnectionString;
+                    availableConnectionStringsMap[connectionStringAlias] = connection;
                 }
             }
 
             _connectionStringsMap = availableConnectionStringsMap;
         }
 
-        public IReadOnlyDictionary<ConnectionStringName, string> AllConnections
+        public IReadOnlyDictionary<ConnectionStringName, ConnectionStringSettings> AllConnections
         {
             get { return _connectionStringsMap; }
         }
 
         public string GetConnectionString(ConnectionStringName connectionStringNameAlias)
         {
-            string connectionString;
-            if (!_connectionStringsMap.TryGetValue(connectionStringNameAlias, out connectionString))
+            return GetConnectionStringSettings(connectionStringNameAlias).ConnectionString;
+        }
+
+        public ConnectionStringSettings GetConnectionStringSettings(ConnectionStringName connectionStringNameAlias)
+        {
+            ConnectionStringSettings connectionStringSettings;
+            if (!_connectionStringsMap.TryGetValue(connectionStringNameAlias, out connectionStringSettings))
             {
                 throw new ConfigurationErrorsException(string.Format("Can't find connection string for alias '{0}'", connectionStringNameAlias));
             }
 
-            return connectionString;
+            return connectionStringSettings;
         }
 
         public string ResolveConnectionStringName(ConnectionStringName connectionStringNameAlias)
