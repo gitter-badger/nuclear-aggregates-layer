4e076d144adc82a539a9836c55334ce64c34d821
Баранихин Юрий Владимирович <y.baranihin@2Gis.ru>
2014-10-31 10:37:40 +0000

    [ERM-5159] рефакторинг перед перемещением
    git-tfs-id: [http://uk-tfs02:8080/tfs/DefaultCollection]$/ERM.BL/Dev/Features/ERM-5159-MoveLimits2/CompositionRoots;C25773

---
+++ Source/Tests/2Gis.Erm.Tests.Integration.InProc/Suite/Concrete/API/Operations/Concrete/Limits/RecalculateLimitOperationServiceTest.cs
@@ -0,0 +1,26 @@
+﻿using DoubleGis.Erm.BLCore.API.Operations.Concrete.Limits;
+using DoubleGis.Erm.Platform.Model.Entities.Erm;
+using DoubleGis.Erm.Tests.Integration.InProc.Suite.Base;
+using DoubleGis.Erm.Tests.Integration.InProc.Suite.Concrete.Common;
+using DoubleGis.Erm.Tests.Integration.InProc.Suite.Infrastructure;
+
+namespace DoubleGis.Erm.Tests.Integration.InProc.Suite.Concrete.API.Operations.Concrete.Limits
+{
+    public class RecalculateLimitOperationServiceTest : UseModelEntityTestBase<Limit>
+    {
+        private readonly IRecalculateLimitOperationService _recalculateLimitOperationService;
+
+        public RecalculateLimitOperationServiceTest(IAppropriateEntityProvider<Limit> appropriateEntityProvider, IRecalculateLimitOperationService recalculateLimitOperationService)
+            : base(appropriateEntityProvider)
+        {
+            _recalculateLimitOperationService = recalculateLimitOperationService;
+        }
+
+        protected override OrdinaryTestResult ExecuteWithModel(Limit modelEntity)
+        {
+            _recalculateLimitOperationService.Recalculate(modelEntity.Id);
+
+            return OrdinaryTestResult.As.Succeeded;
+        }
+    }
+}
