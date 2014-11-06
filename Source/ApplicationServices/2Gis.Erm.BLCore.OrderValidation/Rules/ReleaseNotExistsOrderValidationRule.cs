6bd205537349334d44b64211841a0c1afc269dd0
Масленников Иван Евгеньевич <i.maslennikov@2gis.ru>
2014-11-06 09:49:11 +0000

    Release*ValidationRule переведен на строгий single режим работы
    git-tfs-id: [http://uk-tfs02:8080/tfs/DefaultCollection]$/Erm.Core/Dev/Features/OVR/BLCore;C25870

--- Source/ApplicationServices/2Gis.Erm.BLCore.OrderValidation/Rules/ReleaseNotExistsOrderValidationRule.cs
+++ Source/ApplicationServices/2Gis.Erm.BLCore.OrderValidation/Rules/ReleaseNotExistsOrderValidationRule.cs
@@ -1,73 +1,37 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 
-using DoubleGis.Erm.BLCore.API.Aggregates.Releases.ReadModel;
 using DoubleGis.Erm.BLCore.API.OrderValidation;
 using DoubleGis.Erm.BLCore.Common.Infrastructure.Handlers;
 using DoubleGis.Erm.BLCore.OrderValidation.Rules.Contexts;
-using DoubleGis.Erm.BLCore.Resources.Server.Properties;
 using DoubleGis.Erm.Platform.Model.Entities.Enums;
-
-using MessageType = DoubleGis.Erm.BLCore.API.OrderValidation.MessageType;
 
 namespace DoubleGis.Erm.BLCore.OrderValidation.Rules
 {
-    public sealed class ReleaseNotExistsOrderValidationRule : OrderValidationRuleBase<HybridParamsValidationRuleContext>
+    public sealed class ReleaseNotExistsOrderValidationRule : OrderValidationRuleBase<SingleValidationRuleContext>
     {
         private readonly ISubRequestProcessor _subRequestProcessor;
-        private readonly IReleaseReadModel _releaseReadModel;
 
-        public ReleaseNotExistsOrderValidationRule(ISubRequestProcessor subRequestProcessor, IReleaseReadModel releaseReadModel)
+        public ReleaseNotExistsOrderValidationRule(ISubRequestProcessor subRequestProcessor)
         {
             _subRequestProcessor = subRequestProcessor;
-            _releaseReadModel = releaseReadModel;
         }
 
-        protected override IEnumerable<OrderValidationMessage> Validate(HybridParamsValidationRuleContext ruleContext)
+        protected override IEnumerable<OrderValidationMessage> Validate(SingleValidationRuleContext ruleContext)
         {
-            if (!ruleContext.ValidationParams.IsMassValidation)
-            {
-                var response = 
+            var response = 
                     (CheckOrderReleasePeriodResponse)_subRequestProcessor.HandleSubRequest(
                         new CheckOrderReleasePeriodRequest
                             {
-                                    OrderId = ruleContext.ValidationParams.Single.OrderId,
+                                    OrderId = ruleContext.ValidationParams.OrderId,
                                 InProgressOnly = false,
                             },
                         null);
-                    if (!response.Success)
-                    {
-                        if (ruleContext.ValidationParams.Single.CurrentOrderState != OrderState.Approved)
-                        {
-                            return new[] { response.Message };                         
-                    }
-                }                
-            }
-            else
-            {
-                // we just need to check whether there exists a release for the speicified period/OrganizationUnit
-                const short SuccessReleaseStatus = (short)ReleaseStatus.Success;
 
-                var lastFinalRelease = _releaseReadModel.GetLastFinalRelease(ruleContext.ValidationParams.Mass.OrganizationUnitId, ruleContext.ValidationParams.Mass.Period);
-                if (lastFinalRelease != null && lastFinalRelease.Status == SuccessReleaseStatus)
-                {
-                    var organizationUnitName = _releaseReadModel.GetOrganizationUnitName(ruleContext.ValidationParams.Mass.OrganizationUnitId);
-                    return new[]
-                    {
-                        new OrderValidationMessage
-                        {
-                            Type = MessageType.Error,
-                                           MessageText =
-                                               string.Format(BLResources.OrdersCheckOrderHasReleaseInfo,
-                                                             ruleContext.ValidationParams.Mass.Period.Start,
-                                                             ruleContext.ValidationParams.Mass.Period.End,
-                                                             organizationUnitName),
-                            OrderId = 0,
-                            OrderNumber = string.Empty
-                        }
-                    };
-                }
-            }
+            if (!response.Success && ruleContext.ValidationParams.CurrentOrderState != OrderState.Approved)
+            {
+                return new[] { response.Message };  
+            } 
 
             return Enumerable.Empty<OrderValidationMessage>();
         }
