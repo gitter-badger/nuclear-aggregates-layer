5ed88e6a8f0b28100374266af5de6f7b01561660
Баранихин Юрий Владимирович <y.baranihin@2Gis.ru>
2014-11-07 07:58:41 +0000

    [ERM-5221] переносим конфинурирование существующих кастомизаций на инфраструктуру метамоделирования
    git-tfs-id: [http://uk-tfs02:8080/tfs/DefaultCollection]$/ERM.Composition/Dev/Features/ERM-5221-ViewModelCustomizationServices/BLFlex;C25887

---
+++ Source/EntryPoints/UI/Web/2Gis.Erm.BLFlex.UI.Web.Mvc.Global/Services/Cards/Russia/ViewModelCustomizationsMetadaSource.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using DoubleGis.Erm.BLCore.UI.Web.Mvc.Services.Cards;
+using DoubleGis.Erm.Platform.Model.Entities.Erm;
+using DoubleGis.Erm.Platform.Model.Metadata.Common.Elements;
+using DoubleGis.Erm.Platform.Model.Metadata.Common.Provider.Sources;
+using DoubleGis.Erm.Platform.Model.Metadata.Globalization;
+
+namespace DoubleGis.Erm.BLFlex.UI.Web.Mvc.Global.Services.Cards.Russia
+{
+    public class ViewModelCustomizationsMetadaSource : MetadataSourceBase<ViewModelCustomizationsIdentity>, IRussiaAdapted
+    {
+        private readonly IReadOnlyDictionary<Uri, IMetadataElement> _metadata;
+
+        public ViewModelCustomizationsMetadaSource()
+        {
+            _metadata = InitializeMetadataContainer();
+        }
+
+        public override IReadOnlyDictionary<Uri, IMetadataElement> Metadata
+        {
+            get { return _metadata; }
+        }
+
+        private static IReadOnlyDictionary<Uri, IMetadataElement> InitializeMetadataContainer()
+        {
+            IReadOnlyCollection<ViewModelCustomizationsMetada> metadataContainer =
+                new ViewModelCustomizationsMetada[]
+                    {
+                        ViewModelCustomizationsMetada.Config
+                                                     .For<Client>()
+                                                     .Use<EditIsAdvertisingAgencyViewModelCustomization>(),
+                    };
+
+            return metadataContainer.ToDictionary(x => x.Identity.Id, x => (IMetadataElement)x);
+        }
+    }
+}
--- Source/EntryPoints/UI/Web/2Gis.Erm.BLFlex.UI.Web.Mvc.Global/Services/Cards/ViewModelCustomizationProvider.cs
+++ Source/EntryPoints/UI/Web/2Gis.Erm.BLFlex.UI.Web.Mvc.Global/Services/Cards/ViewModelCustomizationProvider.cs
@@ -3,53 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 
 using DoubleGis.Erm.BLCore.UI.Web.Mvc.Services.Cards;
-using DoubleGis.Erm.BLFlex.UI.Web.Mvc.Global.Services.Cards.Russia;
-using DoubleGis.Erm.Platform.API.Core.Settings.Globalization;
-using DoubleGis.Erm.Platform.Model;
 using DoubleGis.Erm.Platform.Model.Entities;
+using DoubleGis.Erm.Platform.Model.Metadata.Common.Elements.Identities;
+using DoubleGis.Erm.Platform.Model.Metadata.Common.Provider;
 
 namespace DoubleGis.Erm.BLFlex.UI.Web.Mvc.Global.Services.Cards
 {
-    // TODO {d.ivanov, 02.09.2014}: Реализовать на подсистеме метаданных, см IMetadataProvider, IMetadataElement, IMetadataProcessor
     public class ViewModelCustomizationProvider : IViewModelCustomizationProvider
     {
-        private static readonly Dictionary<EntityName, IEnumerable<Type>> CustomizationsByEntities =
-            new Dictionary<EntityName, IEnumerable<Type>>
-                {
-                    { EntityName.Client, new[] {typeof(WarnLinkToAdvAgencyExistsVmCustomization) } }
-                };
+        private readonly IMetadataProvider _metadataProvider;
 
-        private static readonly Dictionary<Tuple<EntityName, BusinessModel>, IEnumerable<Type>> BusinessModelSpecificCustomizations =
-            new Dictionary<Tuple<EntityName, BusinessModel>, IEnumerable<Type>>
-                {
-                    {
-                        Tuple.Create(EntityName.Client, BusinessModel.Russia),
-                        new[] { typeof(EditIsAdvertisingAgencyViewModelCustomization) }
-                    }
-                };
-
-        private readonly BusinessModel _currentBusinessModel;
-
-        public ViewModelCustomizationProvider(IBusinessModelSettings businessModelSettings)
+        public ViewModelCustomizationProvider(IMetadataProvider metadataProvider)
         {
-            _currentBusinessModel = businessModelSettings.BusinessModel;
+            _metadataProvider = metadataProvider;
         }
 
         public IEnumerable<Type> GetCustomizations(EntityName entityName)
         {
-            IEnumerable<Type> genericCustomizations;
-            if (!CustomizationsByEntities.TryGetValue(entityName, out genericCustomizations))
+            ViewModelCustomizationsMetada customizationsMetadata;
+
+            if (!_metadataProvider.TryGetMetadata(IdBuilder.For<ViewModelCustomizationsIdentity>(entityName.ToString()), out customizationsMetadata))
             {
-                genericCustomizations = Enumerable.Empty<Type>();
+                return Enumerable.Empty<Type>();
             }
 
-            IEnumerable<Type> businessModelSpecificCustomizations;
-            if (!BusinessModelSpecificCustomizations.TryGetValue(Tuple.Create(entityName, _currentBusinessModel), out businessModelSpecificCustomizations))
-            {
-                businessModelSpecificCustomizations = Enumerable.Empty<Type>();
-            }
-
-            return genericCustomizations.Concat(businessModelSpecificCustomizations);
+            return customizationsMetadata.Features.OfType<IViewModelCustomizationFeature>().Select(x => x.CustomizationType).ToArray();
         }
     }
 }
