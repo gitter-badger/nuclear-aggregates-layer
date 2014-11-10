eb7981f31e1491e373ba99be92c70a5a817d2271
Речкалов Андрей Михайлович <a.rechkalov@2Gis.ru>
2014-11-10 10:27:24 +0000

    [ERM-5192], общая валидация выделана как отдельный IOperation
    git-tfs-id: [http://uk-tfs02:8080/tfs/DefaultCollection]$/ERM.BL/Dev/Features/ERM-4712-Profile/Platform;C25914

---
+++ Source/Model/2Gis.Erm.Platform.Model/Identities/Operations/Identity/Specific/Order/PrintValidationIdentity.cs
@@ -0,0 +1,21 @@
+﻿namespace DoubleGis.Erm.Platform.Model.Identities.Operations.Identity.Specific.Order
+{
+    public sealed class PrintValidationIdentity : OperationIdentityBase<PrintValidationIdentity>, INonCoupledOperationIdentity
+    {
+        public override int Id
+        {
+            get
+            {
+                return OperationIdentityIds.PrintValidationIdentity;
+            }
+        }
+
+        public override string Description
+        {
+            get
+            {
+                return "Валидация полей заказа перед печатью";
+            }
+        }
+    }
+}
