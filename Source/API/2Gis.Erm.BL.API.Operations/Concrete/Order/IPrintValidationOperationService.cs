4e48d5de6fcc12f04c8fe2bda6d20aa34b1dd40d
Речкалов Андрей Михайлович <a.rechkalov@2Gis.ru>
2014-11-10 10:27:24 +0000

    [ERM-5192], общая валидация выделана как отдельный IOperation
    git-tfs-id: [http://uk-tfs02:8080/tfs/DefaultCollection]$/ERM.BL/Dev/Features/ERM-4712-Profile/BL;C25914

---
+++ Source/API/2Gis.Erm.BL.API.Operations/Concrete/Order/IPrintValidationOperationService.cs
@@ -0,0 +1,12 @@
+﻿
+using DoubleGis.Erm.Platform.API.Core.Operations;
+using DoubleGis.Erm.Platform.Model.Identities.Operations.Identity.Specific.Order;
+
+namespace DoubleGis.Erm.BL.API.Operations.Concrete.Order
+{
+    public interface IPrintValidationOperationService : IOperation<PrintValidationIdentity>
+    {
+        void ValidateOrder(long orderId);
+        void ValidateBill(long billId);
+    }
+}
---
+++ Source/ApplicationServices/2Gis.Erm.BL.Operations/Concrete/Order/PrintValidationOperationService.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Linq;
+
+using DoubleGis.Erm.BL.API.Operations.Concrete.Order;
+using DoubleGis.Erm.BLCore.API.Aggregates.Orders.DTO;
+using DoubleGis.Erm.BLCore.API.Aggregates.Orders.ReadModel;
+using DoubleGis.Erm.BLCore.Resources.Server.Properties;
+using DoubleGis.Erm.Core.Exceptions;
+using DoubleGis.Erm.Platform.API.Core.Exceptions;
+using DoubleGis.Erm.Platform.DAL;
+using DoubleGis.Erm.Platform.DAL.Specifications;
+using DoubleGis.Erm.Platform.Model.Entities.Interfaces;
+
+
+namespace DoubleGis.Erm.BL.Operations.Concrete.Order
+{
+    public sealed class PrintValidationOperationService : IPrintValidationOperationService
+    {
+        private readonly IFinder _finder;
+
+        public PrintValidationOperationService(IFinder finder)
+        {
+            _finder = finder;
+        }
+
+        public void ValidateOrder(long orderId)
+        {
+            Validate(Specs.Find.ById<Platform.Model.Entities.Erm.Order>(orderId), OrderSpecs.Orders.Select.OrderPrintValidationDto());
+        }
+
+        public void ValidateBill(long billId)
+        {
+            Validate(Specs.Find.ById<Platform.Model.Entities.Erm.Bill>(billId), OrderSpecs.Bills.Select.OrderPrintValidationDto());
+        }
+
+        public void Validate<T>(IFindSpecification<T> filter, ISelectSpecification<T, OrderPrintValidationDto> projection) 
+            where T : class, IEntity
+        {
+            var dtos = _finder.Find(projection, filter).Take(2).ToArray();
+
+            if (dtos.Length > 1)
+            {
+                // ReSharper disable once LocalizableElement
+                throw new ArgumentException("Неоднозначный фильтр", "filter");
+            }
+
+            if (dtos.Length < 1)
+            {
+                throw new EntityNotFoundException(typeof(T));
+            }
+
+            var dto = dtos.Single();
+
+            if (dto.LegalPersonId == null)
+            {
+                throw new RequiredFieldIsEmptyException(string.Format(BLResources.OrderFieldNotSpecified, MetadataResources.LegalPerson));
+            }
+
+            if (dto.LegalPersonProfileId == null)
+            {
+                throw new RequiredFieldIsEmptyException(string.Format(BLResources.OrderFieldNotSpecified, MetadataResources.LegalPersonProfile));
+            }
+
+            if (dto.BranchOfficeOrganizationUnitId == null)
+            {
+                throw new RequiredFieldIsEmptyException(string.Format(BLResources.OrderFieldNotSpecified, MetadataResources.BranchOfficeOrganizationUnit));
+            }
+        }
+    }
+}
