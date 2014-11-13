e97a281bf9901b2ab21a2b9826525810b0e8764a
Речкалов Андрей Михайлович <a.rechkalov@2Gis.ru>
2014-11-13 06:42:38 +0000

    [ERM-5249], fix, 8: валюта в ПФ
    git-tfs-id: [http://uk-tfs02:8080/tfs/DefaultCollection]$/ERM.BL/Prerelease/BLFlex;C25983

--- Source/ApplicationServices/2Gis.Erm.BLFlex.Operations.Global/Shared/Formatter/TengeFormatter.cs
+++ Source/ApplicationServices/2Gis.Erm.BLFlex.Operations.Global/Shared/Formatter/TengeFormatter.cs
@@ -1,3 +1,5 @@
+using System.Globalization;
+
 using DoubleGis.Erm.Platform.Common.PrintFormEngine;
 
 namespace DoubleGis.Erm.BLFlex.Operations.Global.Shared.Formatter
@@ -5,8 +7,19 @@ namespace DoubleGis.Erm.BLFlex.Operations.Global.Shared.Formatter
     internal sealed class TengeFormatter : CurrencyFormatter, IFormatter
     {
         public TengeFormatter()
-            : base("kk-KZ")
+            : base(CustomizeCurrency())
         {
+        }
+
+        private static NumberFormatInfo CustomizeCurrency()
+        {
+            var baseCulture = CultureInfo.GetCultureInfo("kk-KZ");
+            var modifiedFormat = (NumberFormatInfo)baseCulture.NumberFormat.Clone();
+            modifiedFormat.CurrencyPositivePattern = 3;
+            modifiedFormat.CurrencyDecimalSeparator = ",";
+            modifiedFormat.CurrencySymbol = "עד.";
+            
+            return modifiedFormat;
         }
     }
 }
