ef962ef4dce5ea784415f028dd9a820e652adcb2
Баранихин Юрий Владимирович <y.baranihin@2Gis.ru>
2014-11-19 08:27:28 +0000

    [ERM-5221] Re: Cr
    git-tfs-id: [http://uk-tfs02:8080/tfs/DefaultCollection]$/ERM.Composition/Dev/Features/ERM-5221-ViewModelCustomizationServices/BL;C26179

--- Source/EntryPoints/UI/Web/2Gis.Erm.BL.UI.Web.Mvc/Services/Cards/Advertisements/AdvertisementAccessCustomization.cs
+++ Source/EntryPoints/UI/Web/2Gis.Erm.BL.UI.Web.Mvc/Services/Cards/Advertisements/AdvertisementAccessCustomization.cs
@@ -1,17 +1,44 @@
 using System.Web.Mvc;
 
 using DoubleGis.Erm.BL.UI.Web.Mvc.Models;
+using DoubleGis.Erm.BLCore.API.Aggregates.Firms.ReadModel;
 using DoubleGis.Erm.BLCore.UI.Web.Mvc.Services.Cards;
+using DoubleGis.Erm.Platform.API.Security;
+using DoubleGis.Erm.Platform.API.Security.EntityAccess;
+using DoubleGis.Erm.Platform.API.Security.UserContext;
+using DoubleGis.Erm.Platform.Model.Entities;
 
 namespace DoubleGis.Erm.BL.UI.Web.Mvc.Services.Cards.Advertisements
 {
     public sealed class AdvertisementAccessCustomization : IViewModelCustomization<AdvertisementViewModel>
     {
+        private readonly IUserContext _userContext;
+        private readonly ISecurityServiceEntityAccess _securityServiceEntityAccess;
+        private readonly IFirmReadModel _firmReadModel;
+
+        public AdvertisementAccessCustomization(IUserContext userContext, ISecurityServiceEntityAccess securityServiceEntityAccess, IFirmReadModel firmReadModel)
+        {
+            _userContext = userContext;
+            _securityServiceEntityAccess = securityServiceEntityAccess;
+            _firmReadModel = firmReadModel;
+        }
+
         public void Customize(AdvertisementViewModel viewModel, ModelStateDictionary modelState)
         {
-            // FIXME {all, 18.11.2014}: техдолг: поле UserDoesntHaveRightsToEditFirm не требуется ни в dto ни во ViewModel.
-            //                          можно вычислять права тут и по результатам выставлять ReadOnly
-            viewModel.ViewConfig.ReadOnly |= viewModel.UserDoesntHaveRightsToEditFirm;
+            if (!viewModel.Firm.Key.HasValue)
+            {
+                return;
+            }
+
+            var firmId = viewModel.Firm.Key.Value;
+            var firmOwnerCode = _firmReadModel.GetFirmOwnerCodeUnsecure(firmId);
+
+            viewModel.ViewConfig.ReadOnly |= !_securityServiceEntityAccess.HasEntityAccess(EntityAccessTypes.Update,
+                                                                                           EntityName.Firm,
+                                                                                           _userContext.Identity.Code,
+                                                                                           firmId,
+                                                                                           firmOwnerCode,
+                                                                                           null);
         }
     }
 }
