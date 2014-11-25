abf292d78d1e6bbd43bffe9fc045d77f56260c9a
Баранихин Юрий <y.baranihin@2gis.ru>
2014-11-25 12:38:15 +0600

    Продолжение

--- BL/Source/EntryPoints/UI/Web/2Gis.Erm.BL.UI.Web.Metadata/Cards/CardMetadataExtensions.cs
+++ BL/Source/EntryPoints/UI/Web/2Gis.Erm.BL.UI.Web.Metadata/Cards/CardMetadataExtensions.cs
@@ -4,8 +4,11 @@ using System.Linq;
 using DoubleGis.Erm.BLCore.API.Common.Metadata.Old.Dto;
 using DoubleGis.Erm.BLCore.UI.Metadata.Config.Cards;
 using DoubleGis.Erm.Platform.Model.Metadata.Common.Elements;
+using DoubleGis.Erm.Platform.UI.Metadata.Config.Common.Card.Features;
+using DoubleGis.Erm.Platform.UI.Metadata.Config.Common.Features.RelatedItems;
 using DoubleGis.Erm.Platform.UI.Metadata.UiElements;
 using DoubleGis.Erm.Platform.UI.Metadata.UiElements.ControlTypes;
+using DoubleGis.Erm.Platform.UI.Metadata.UiElements.Features;
 
 namespace DoubleGis.Erm.BL.UI.Web.Metadata.Cards
 {
@@ -64,6 +67,43 @@ namespace DoubleGis.Erm.BL.UI.Web.Metadata.Cards
             if (toolbarElement.ImageDescriptor != null)
             {
                 result.Icon = toolbarElement.ImageDescriptor.ToString();
+            }
+
+            if (toolbarElement.TitleDescriptor != null)
+            {
+                result.NameLocaleResourceId = toolbarElement.TitleDescriptor.ResourceKeyToString();
+                result.LocalizedName = toolbarElement.TitleDescriptor.GetValue(culture);
+            }
+
+            result.LockOnNew = toolbarElement.Uses<LockOnNewFeature>();
+            result.LockOnInactive = toolbarElement.Uses<LockOnInactiveFeature>();
+
+            int SecurityPrivelegeFlag = 0;
+            foreach (var feature in toolbarElement.Features<SecuredByFunctionalPrivelegeFeature>())
+            {
+                SecurityPrivelegeFlag |= (int)feature.Privilege;
+            }
+
+            foreach (var feature in toolbarElement.Features<SecuredByEntityPrivelegeFeature>())
+            {
+                SecurityPrivelegeFlag |= (int)feature.Privilege;
+            }
+
+            return result;
+        }
+
+        public static CardRelatedItemsGroupStructure ToCardRelatedItemsGroupStructure(this RelatedItemsFeature relatedItemsElement, CultureInfo culture)
+        {
+            var result = new CardRelatedItemsGroupStructure();
+            if (relatedItemsElement.NameDescriptor != null)
+            {
+                result.Name = relatedItemsElement.NameDescriptor.ToString();
+            }
+
+            if (relatedItemsElement.TitleDescriptor != null)
+            {
+                result.NameLocaleResourceId = relatedItemsElement.TitleDescriptor.ResourceKeyToString();
+                result.LocalizedName = relatedItemsElement.TitleDescriptor.GetValue(culture);
             }
 
             return result;
