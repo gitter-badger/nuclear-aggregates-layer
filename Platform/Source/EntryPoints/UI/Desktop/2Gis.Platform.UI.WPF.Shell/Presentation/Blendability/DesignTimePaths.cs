338fcf08f1737a79c713b5d71ad414d48c536dfb
Тукаев Александр Юрьевич <a.tukaev@2Gis.ru>
2014-12-03 16:13:48 +0600

    [ERM-5380]

--- Platform/Source/EntryPoints/UI/Desktop/2Gis.Platform.UI.WPF.Shell/Presentation/Blendability/DesignTimePaths.cs
+++ Platform/Source/EntryPoints/UI/Desktop/2Gis.Platform.UI.WPF.Shell/Presentation/Blendability/DesignTimePaths.cs
@@ -1,1 +1,1 @@
-namespace DoubleGis.Platform.UI.WPF.Shell.Presentation.Blendability { static public class DesignTimePaths { public static readonly string LocalPath = @"D:\git\NuClear\erm-reserve\CompositionRoots\Source\2Gis.Erm.UI.Desktop.WPF\bin\Debug";} } 
+namespace DoubleGis.Platform.UI.WPF.Shell.Presentation.Blendability { static public class DesignTimePaths { public static readonly string LocalPath = @"C:\git\erm-primary\CompositionRoots\Source\2Gis.Erm.UI.Desktop.WPF\bin\Debug";} } 
