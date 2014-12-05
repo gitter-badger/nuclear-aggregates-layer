3fce8c0565bac030cf7c78f7a1cd6231e1ae2924
Sergey Pomadin <s.pomadin@2gis.ru>
2014-12-05 19:16:05 +0600

    [NA] fixed auto completing for activity titles

--- CompositionRoots/Source/2Gis.Erm.UI.Web.Mvc/Scripts/Activity/Ext.DoubleGis.UI.ActivityBase.js
+++ CompositionRoots/Source/2Gis.Erm.UI.Web.Mvc/Scripts/Activity/Ext.DoubleGis.UI.ActivityBase.js
@@ -34,16 +34,16 @@ Ext.DoubleGis.UI.ActivityBase = Ext.extend(Ext.DoubleGis.UI.Card, {
     },
     autocompleteHeader: function () {
         var prefix = this.getTitlePrefix();
-        if (!prefix) return;
+        var suffix = this.getTitleSuffix();
 
         var headerElement = Ext.get("Title");
         var header = headerElement.getValue();
 
         // Автозаполнение срабатывает если поле "Заголовок" - пустое или ранее было автоматически заполнено (т.е. после автозаполнения не редактировалось пользователем).
-        var shouldAutoCompleteHeader = !header || header.trim() == this.autoHeader.build().trim();
+        var shouldAutoCompleteHeader = prefix && (!header || header.trim() == this.autoHeader.build().trim());
+        this.autoHeader.prefix = prefix;
+        this.autoHeader.suffix = suffix;
         if (shouldAutoCompleteHeader) {
-            this.autoHeader.prefix = prefix;
-            this.autoHeader.suffix = this.getTitleSuffix();
             headerElement.setValue(this.autoHeader.build());
         }
     },
@@ -99,8 +99,6 @@ Ext.DoubleGis.UI.ActivityBase = Ext.extend(Ext.DoubleGis.UI.Card, {
     Build : function() {
         Ext.DoubleGis.UI.ActivityBase.superclass.Build.call(this);
 
-        this.autoHeader.prefix = this.getTitlePrefix();
-        this.autoHeader.suffix = this.getTitleSuffix();
         Ext.getCmp("Client").on("change", this.autocompleteHeader, this);
 
         if (this.contactField && this.contactComp) {
