1c37f4061e0701e730e6615f277fdb6e88421ea4
a.rechkalov <a.rechkalov@2gis.ru>
2014-12-10 19:05:39 +0600

    [ERM-5247] выполнил todo годовой давности: убрал генерацию номера счёта в AggregateService

--- BLCore/Source/ApplicationServices/2Gis.Erm.BLCore.Aggregates/Orders/Operations/Bills/CreateBillsAggregateService.cs
+++ BLCore/Source/ApplicationServices/2Gis.Erm.BLCore.Aggregates/Orders/Operations/Bills/CreateBillsAggregateService.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 using DoubleGis.Erm.BLCore.API.Aggregates.Orders.Operations.Bills;
 using DoubleGis.Erm.BLCore.API.Aggregates.Orders.Operations.Crosscutting;
@@ -17,30 +19,44 @@ namespace DoubleGis.Erm.BLCore.Aggregates.Orders.Operations.Bills
         private readonly IOperationScopeFactory _scopeFactory;
         private readonly IRepository<Bill> _billGenericRepository; 
         private readonly IValidateBillsService _validateBillsService;
+        private readonly IEvaluateBillNumberService _evaluateBillNumberService;
 
         public CreateBillsAggregateService(
             IOperationScopeFactory scopeFactory, 
             IRepository<Bill> billGenericRepository, 
             IIdentityProvider identityProvider,
-            IValidateBillsService validateBillsService)
+            IValidateBillsService validateBillsService, 
+            IEvaluateBillNumberService evaluateBillNumberService)
         {
             _scopeFactory = scopeFactory;
             _billGenericRepository = billGenericRepository;
             _identityProvider = identityProvider;
             _validateBillsService = validateBillsService;
+            _evaluateBillNumberService = evaluateBillNumberService;
         }
 
         public void Create(Order order, IEnumerable<Bill> bills)
         {
+            var billsArray = bills as Bill[] ?? bills.ToArray();
+
+            var numberEvaluation = billsArray.Length == 1
+                ? new Func<Bill, int, string>((bill, index) => _evaluateBillNumberService.Evaluate(bill.BillNumber, order.Number))
+                : new Func<Bill, int, string>((bill, index) => _evaluateBillNumberService.Evaluate(bill.BillNumber, order.Number, index + 1));
+
+            for (var i = 0; i < billsArray.Length; i++)
+            {
+                billsArray[i].BillNumber = numberEvaluation.Invoke(billsArray[i], i);
+            }
+
             string report;
-            if (!_validateBillsService.PreValidate(bills, out report) || !_validateBillsService.Validate(bills, order, out report))
+            if (!_validateBillsService.PreValidate(billsArray, out report) || !_validateBillsService.Validate(billsArray, order, out report))
             {
                 throw new NotificationException(report);
             }
 
             using (var scope = _scopeFactory.CreateSpecificFor<CreateIdentity, Bill>())
             {
-                foreach (var bill in bills)
+                foreach (var bill in billsArray)
                 {
                     _identityProvider.SetFor(bill);
                     _billGenericRepository.Add(bill);
--- BLCore/Source/ApplicationServices/2Gis.Erm.BLCore.Operations/Generic/Modify/CreateOrderBillsOperationService.cs
+++ BLCore/Source/ApplicationServices/2Gis.Erm.BLCore.Operations/Generic/Modify/CreateOrderBillsOperationService.cs
@@ -20,20 +20,17 @@ namespace DoubleGis.Erm.BLCore.Operations.Generic.Modify
         private readonly IBulkDeleteBillAggregateService _deleteAggregateService;
         private readonly ICreateBillsAggregateService _createAggregateService;
         private readonly IOrderReadModel _orderReadModel;
-        private readonly IEvaluateBillNumberService _evaluateBillNumberService;
         private readonly IBusinessModelSettings _businessModelSettings;
 
         public CreateOrderBillsOperationService(
             IBusinessModelSettings businessModelSettings,
             ICreateBillsAggregateService createAggregateService,
             IOrderReadModel orderReadModel,
-            IEvaluateBillNumberService evaluateBillNumberService,
             IBulkDeleteBillAggregateService deleteAggregateService,
             IOperationScopeFactory scopeFactory)
         {
             _createAggregateService = createAggregateService;
             _orderReadModel = orderReadModel;
-            _evaluateBillNumberService = evaluateBillNumberService;
             _deleteAggregateService = deleteAggregateService;
             _scopeFactory = scopeFactory;
             _businessModelSettings = businessModelSettings;
@@ -60,7 +57,7 @@ namespace DoubleGis.Erm.BLCore.Operations.Generic.Modify
             {
                 var createBillInfo = createBillInfos[i];
                 var bill = CreateBill(createBillInfo, orderInfo);
-
+                bill.BillNumber = createBillInfo.BillNumber;
                 bill.PayablePlan = Math.Round(createBillInfo.PayablePlan, _businessModelSettings.SignificantDigitsNumber, MidpointRounding.ToEven);
                 billsPayablePlanSum += bill.PayablePlan;
 
@@ -74,25 +71,11 @@ namespace DoubleGis.Erm.BLCore.Operations.Generic.Modify
             // correct PayablePlan for last bill
             var lastCreateBillInfo = createBillInfos[createBillInfos.Length - 1];
             var lastBill = CreateBill(lastCreateBillInfo, orderInfo);
-
+            lastBill.BillNumber = lastCreateBillInfo.BillNumber;
             lastBill.PayablePlan = orderInfo.PayablePlan - billsPayablePlanSum;
             lastBill.VatPlan = orderInfo.VatPlan - billsVatPlanSum;
 
             billsToCreate.Add(lastBill);
-
-            if (billsToCreate.Count == 1)
-            {
-                // FIXME {all, 29.01.2014}: при рефакторинге ApplicationService нужно перенести использование evaluateBillNumberService в запиливаемый CreateBillAggregateService
-                billsToCreate.Single().BillNumber = _evaluateBillNumberService.Evaluate(createBillInfos.Single().BillNumber, orderInfo.Number);
-            }
-            else
-            {
-                for (var i = 0; i < billsToCreate.Count; i++)
-                {
-                    // FIXME {all, 29.01.2014}: при рефакторинге ApplicationService нужно перенести использование evaluateBillNumberService в запиливаемый CreateBillAggregateService
-                    billsToCreate[i].BillNumber = _evaluateBillNumberService.Evaluate(createBillInfos[i].BillNumber, orderInfo.Number, i + 1);
-                }
-            }
 
             using (var scope = _scopeFactory.CreateSpecificFor<BulkCreateIdentity, Bill>())
             {
