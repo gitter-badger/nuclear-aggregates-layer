4daf4ca3bcac3e692773a8c3a579e438ed4a867d
a.rechkalov <a.rechkalov@2gis.ru>
2014-12-11 10:58:10 +0600

    [ERM-5247] выделил проверки как отдельные сущности

--- BLFlex/Source/ApplicationServices/2Gis.Erm.BLFlex.Aggregates.Global/Chile/Crosscutting/ChileValidateBillsService.cs
+++ BLFlex/Source/ApplicationServices/2Gis.Erm.BLFlex.Aggregates.Global/Chile/Crosscutting/ChileValidateBillsService.cs
@@ -16,78 +16,143 @@ namespace DoubleGis.Erm.BLFlex.Aggregates.Global.Chile.Crosscutting
 {
     public class ChileValidateBillsService : IValidateBillsService, IChileAdapted
     {
-        private static readonly Regex ChileBillNumberPattern = new Regex(@"^\d{7}$", RegexOptions.Compiled);
-        private readonly IFinder _finder;
+        private readonly IBillInvariant[] _invariants;
 
         public ChileValidateBillsService(IFinder finder)
         {
-            _finder = finder;
+            _invariants = new IBillInvariant[]
+                                 {
+                                     new ChileBillNumberFormatInvariant(), 
+                                     new BillSummInvariant(),
+                                     new BillDublicateNumbersInvariant(finder), 
+                                     new BillDatesInvariant(), 
+                                 };
         }
 
         public bool Validate(IEnumerable<Bill> bills, Order order, out string report)
         {
-            var billNumbers = bills.Select(x => x.BillNumber).ToArray();
-            var billIds = bills.Where(x => !x.IsNew()).Select(x => x.Id).ToArray();
-
-            var duplicateBillNumbers = billNumbers.GroupBy(x => x).Where(x => x.Count() > 1).Select(x => x.Key).ToArray();
-            if (duplicateBillNumbers.Any())
+            report = null;
+            foreach (var invariant in _invariants)
             {
-                report = string.Format(BLResources.DublicateBillNumbers, string.Join(", ", duplicateBillNumbers));
-                return false;
-            }
-
-            var wrongBillNumbers = billNumbers.Where(x => ChileBillNumberPattern.IsMatch(x)).ToArray();
-            if (wrongBillNumbers.Any())
-            {
-                report = string.Format(BLResources.WrongBillNumbers, string.Join(", ", wrongBillNumbers));
-                return false;
-            }
-
-            // simple validation
-            var createBillsPayablePlan = bills.OrderBy(x => x.PayablePlan).Sum(x => x.PayablePlan);
-            if (createBillsPayablePlan != order.PayablePlan)
-            {
-                report = BLCore.Resources.Server.Properties.BLResources.BillsPayableSumNotEqualsToOrderPayable;
-                return false;
-            }
-
-            // numbers validation
-            var existingNumbers = _finder.Find(Specs.Find.ActiveAndNotDeleted<Bill>() && BillSpecifications.Find.ByNumbers(billNumbers) && !Specs.Find.ByIds<Bill>(billIds))
-                                         .Select(x => x.BillNumber)
-                                         .ToArray();
-
-            if (existingNumbers.Any())
-            {
-                report = string.Format(BLResources.ExistingBillNumbers, string.Join(", ", existingNumbers));
-                return false;
-            }
-
-            // dates validation
-            foreach (var bill in bills)
-            {
-                if (bill.PaymentDatePlan > bill.BeginDistributionDate)
+                if (!invariant.Validate(bills, order, out report))
                 {
-                    report = string.Format(BLCore.Resources.Server.Properties.BLResources.PaymentDatePlanMustBeLessThanBeginDistributionDateForPayment, bill.BillNumber);
-                    return false;
-                }
-
-                if (bill.BeginDistributionDate > bill.EndDistributionDate)
-                {
-                    report = string.Format(BLCore.Resources.Server.Properties.BLResources.BeginDistributionDatePlanMustBeLessThanEndDistributionDateForPayment, bill.BillNumber);
-                    return false;
-                }
-
-                var endOfPaymentDatePlan = bill.PaymentDatePlan.GetEndOfTheDay();
-                var endOfCheckPeriod = bill.BeginDistributionDate.GetPrevMonthLastDate();
-                if (order.SignupDate > bill.PaymentDatePlan && endOfPaymentDatePlan <= endOfCheckPeriod)
-                {
-                    report = BLCore.Resources.Server.Properties.BLResources.BillPaymentDatePlanMustBeInCorrectPeriod;
                     return false;
                 }
             }
 
-            report = null; 
             return true;
+        }
+
+        public interface IBillInvariant
+        {
+            bool Validate(IEnumerable<Bill> bills, Order order, out string report);
+        }
+
+        public sealed class ChileBillNumberFormatInvariant : IBillInvariant
+        {
+            private static readonly Regex ChileBillNumberPattern = new Regex(@"^\d{7}$", RegexOptions.Compiled);
+
+            public bool Validate(IEnumerable<Bill> bills, Order order, out string report)
+            {
+                var billNumbers = bills.Select(x => x.BillNumber).ToArray();
+                var billIds = bills.Where(x => !x.IsNew()).Select(x => x.Id).ToArray();
+
+                var wrongBillNumbers = billNumbers.Where(x => ChileBillNumberPattern.IsMatch(x)).ToArray();
+                if (wrongBillNumbers.Any())
+                {
+                    report = string.Format(BLResources.WrongBillNumbers, string.Join(", ", wrongBillNumbers));
+                    return false;
+                }
+
+                report = null;
+                return true;
+            }
+        }
+
+        public sealed class BillSummInvariant : IBillInvariant
+        {
+            public bool Validate(IEnumerable<Bill> bills, Order order, out string report)
+            {
+                // simple validation
+                var createBillsPayablePlan = bills.OrderBy(x => x.PayablePlan).Sum(x => x.PayablePlan);
+                if (createBillsPayablePlan != order.PayablePlan)
+                {
+                    report = BLCore.Resources.Server.Properties.BLResources.BillsPayableSumNotEqualsToOrderPayable;
+                    return false;
+                }
+
+                report = null;
+                return true;
+            }
+        }
+
+        public sealed class BillDublicateNumbersInvariant : IBillInvariant
+        {
+            private readonly IFinder _finder;
+
+            public BillDublicateNumbersInvariant(IFinder finder)
+            {
+                _finder = finder;
+            }
+
+            public bool Validate(IEnumerable<Bill> bills, Order order, out string report)
+            {
+                // дубликаты в пакете
+                var billNumbers = bills.Select(x => x.BillNumber).ToArray();
+                var duplicateBillNumbers = billNumbers.GroupBy(x => x).Where(x => x.Count() > 1).Select(x => x.Key).ToArray();
+                if (duplicateBillNumbers.Any())
+                {
+                    report = string.Format(BLResources.DublicateBillNumbers, string.Join(", ", duplicateBillNumbers));
+                    return false;
+                }
+
+                // пересечения в базе с пакетом
+                var billIds = bills.Where(x => !x.IsNew()).Select(x => x.Id).ToArray();
+                var existingNumbers = _finder.Find(Specs.Find.ActiveAndNotDeleted<Bill>() && BillSpecifications.Find.ByNumbers(billNumbers) && !Specs.Find.ByIds<Bill>(billIds))
+                                             .Select(x => x.BillNumber)
+                                             .ToArray();
+
+                if (existingNumbers.Any())
+                {
+                    report = string.Format(BLResources.ExistingBillNumbers, string.Join(", ", existingNumbers));
+                    return false;
+                }
+
+                report = null;
+                return true;
+            }
+        }
+
+        public sealed class BillDatesInvariant : IBillInvariant
+        {
+            public bool Validate(IEnumerable<Bill> bills, Order order, out string report)
+            {
+                foreach (var bill in bills)
+                {
+                    if (bill.PaymentDatePlan > bill.BeginDistributionDate)
+                    {
+                        report = string.Format(BLCore.Resources.Server.Properties.BLResources.PaymentDatePlanMustBeLessThanBeginDistributionDateForPayment, bill.BillNumber);
+                        return false;
+                    }
+
+                    if (bill.BeginDistributionDate > bill.EndDistributionDate)
+                    {
+                        report = string.Format(BLCore.Resources.Server.Properties.BLResources.BeginDistributionDatePlanMustBeLessThanEndDistributionDateForPayment, bill.BillNumber);
+                        return false;
+                    }
+
+                    var endOfPaymentDatePlan = bill.PaymentDatePlan.GetEndOfTheDay();
+                    var endOfCheckPeriod = bill.BeginDistributionDate.GetPrevMonthLastDate();
+                    if (order.SignupDate > bill.PaymentDatePlan && endOfPaymentDatePlan <= endOfCheckPeriod)
+                    {
+                        report = BLCore.Resources.Server.Properties.BLResources.BillPaymentDatePlanMustBeInCorrectPeriod;
+                        return false;
+                    }
+                }
+
+                report = null;
+                return true;
+            }
         }
     }
 }
--- BLFlex/Source/ApplicationServices/2Gis.Erm.BLFlex.Aggregates.Global/Multiculture/Crosscutting/DefaultValidateBillsService.cs
+++ BLFlex/Source/ApplicationServices/2Gis.Erm.BLFlex.Aggregates.Global/Multiculture/Crosscutting/DefaultValidateBillsService.cs
@@ -12,49 +12,100 @@ namespace DoubleGis.Erm.BLFlex.Aggregates.Global.Multiculture.Crosscutting
 {
     public class DefaultValidateBillsService : IValidateBillsService, IRussiaAdapted, ICyprusAdapted, ICzechAdapted
     {
+        private readonly IBillInvariant[] _invariants;
+
+        public DefaultValidateBillsService()
+        {
+            _invariants = new IBillInvariant[]
+                                 {
+                                     new LockedOrderInvariant(), 
+                                     new BillSummInvariant(),
+                                     new BillDatesInvariant(), 
+                                 };
+        }
+
         public bool Validate(IEnumerable<Bill> bills, Order order, out string report)
         {
-            // order state validation
-            if (order.WorkflowStepId != OrderState.OnRegistration)
-            {
-                report = BLResources.CantEditBillsWhenOrderIsNotOnRegistration;
-                return false;
-            }
-
-            // simple validation
-            var createBillsPayablePlan = bills.OrderBy(x => x.PayablePlan).Sum(x => x.PayablePlan);
-            if (createBillsPayablePlan != order.PayablePlan)
-            {
-                report = BLResources.BillsPayableSumNotEqualsToOrderPayable;
-                return false;
-            }
-
-            // dates validation
-            foreach (var bill in bills)
-            {
-                if (bill.PaymentDatePlan > bill.BeginDistributionDate)
-                {
-                    report = string.Format(BLResources.PaymentDatePlanMustBeLessThanBeginDistributionDateForPayment, bill.BillNumber);
-                    return false;
-                }
-
-                if (bill.BeginDistributionDate > bill.EndDistributionDate)
-                {
-                    report = string.Format(BLResources.BeginDistributionDatePlanMustBeLessThanEndDistributionDateForPayment, bill.BillNumber);
-                    return false;
-                }
-
-                var endOfPaymentDatePlan = bill.PaymentDatePlan.GetEndOfTheDay();
-                var endOfCheckPeriod = bill.BeginDistributionDate.GetPrevMonthLastDate();
-                if (order.SignupDate > bill.PaymentDatePlan && endOfPaymentDatePlan <= endOfCheckPeriod)
-                {
-                    report = BLResources.BillPaymentDatePlanMustBeInCorrectPeriod;
-                    return false;
-                }
-            }
-
             report = null;
+            foreach (var invariant in _invariants)
+            {
+                if (!invariant.Validate(bills, order, out report))
+                {
+                    return false;
+                }
+            }
+
             return true;
+        }
+
+        public interface IBillInvariant
+        {
+            bool Validate(IEnumerable<Bill> bills, Order order, out string report);
+        }
+
+        public sealed class LockedOrderInvariant : IBillInvariant
+        {
+            public bool Validate(IEnumerable<Bill> bills, Order order, out string report)
+            {
+                // order state validation
+                if (order.WorkflowStepId != OrderState.OnRegistration)
+                {
+                    report = BLResources.CantEditBillsWhenOrderIsNotOnRegistration;
+                    return false;
+                }
+
+                report = null;
+                return true;
+            }
+        }
+
+        public sealed class BillSummInvariant : IBillInvariant
+        {
+            public bool Validate(IEnumerable<Bill> bills, Order order, out string report)
+            {
+                // simple validation
+                var createBillsPayablePlan = bills.OrderBy(x => x.PayablePlan).Sum(x => x.PayablePlan);
+                if (createBillsPayablePlan != order.PayablePlan)
+                {
+                    report = BLResources.BillsPayableSumNotEqualsToOrderPayable;
+                    return false;
+                }
+
+                report = null;
+                return true;
+            }
+        }
+
+        public sealed class BillDatesInvariant : IBillInvariant
+        {
+            public bool Validate(IEnumerable<Bill> bills, Order order, out string report)
+            {
+                foreach (var bill in bills)
+                {
+                    if (bill.PaymentDatePlan > bill.BeginDistributionDate)
+                    {
+                        report = string.Format(BLResources.PaymentDatePlanMustBeLessThanBeginDistributionDateForPayment, bill.BillNumber);
+                        return false;
+                    }
+
+                    if (bill.BeginDistributionDate > bill.EndDistributionDate)
+                    {
+                        report = string.Format(BLResources.BeginDistributionDatePlanMustBeLessThanEndDistributionDateForPayment, bill.BillNumber);
+                        return false;
+                    }
+
+                    var endOfPaymentDatePlan = bill.PaymentDatePlan.GetEndOfTheDay();
+                    var endOfCheckPeriod = bill.BeginDistributionDate.GetPrevMonthLastDate();
+                    if (order.SignupDate > bill.PaymentDatePlan && endOfPaymentDatePlan <= endOfCheckPeriod)
+                    {
+                        report = BLResources.BillPaymentDatePlanMustBeInCorrectPeriod;
+                        return false;
+                    }
+                }
+
+                report = null;
+                return true;
+            }
         }
     }
 }
