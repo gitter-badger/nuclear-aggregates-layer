cea426811686578f01d26f4ba98014e67effcda4
Alexander Tukaev <tukaef@gmail.com>
2014-12-24 22:06:24 +0600

    [ERM-5544] fix

--- BLCore/Source/ApplicationServices/2Gis.Erm.BLCore.Operations/Generic/ActionHistory/ActionsHistoryService.cs
+++ BLCore/Source/ApplicationServices/2Gis.Erm.BLCore.Operations/Generic/ActionHistory/ActionsHistoryService.cs
@@ -49,55 +49,55 @@ namespace DoubleGis.Erm.BLCore.Operations.Generic.ActionHistory
             var actionsInfo = _finder.Find<ActionsHistory>(x => x.EntityType == (int)entityName && x.EntityId == entityId)
                                      .OrderByDescending(x => x.Id)
                                      .Select(item => new
-                                                         {
-                                                             Item = new
-                                                                        {
-                                                                            item.Id,
-                                                                            ActionType = (ActionType)item.ActionType,
-                                                                            item.CreatedBy,
-                                                                            item.CreatedOn
-                                                                        },
-                                                             Details = item.ActionsHistoryDetails
-                                                                           .Where(detail => metadata.Properties.Contains(detail.PropertyName))
-                                                                           .Select(detail => new
-                                                                                                 {
-                                                                                                     detail.Id,
-                                                                                                     detail.ActionsHistoryId,
-                                                                                                     detail.PropertyName,
-                                                                                                     detail.OriginalValue,
-                                                                                                     detail.ModifiedValue
-                                                                                                 })
-                                                         })
+                                     {
+                                         Item = new
+                                         {
+                                             item.Id,
+                                             ActionType = (ActionType)item.ActionType,
+                                             item.CreatedBy,
+                                             item.CreatedOn
+                                         },
+                                         Details = item.ActionsHistoryDetails
+                                                       .Where(detail => metadata.Properties.Contains(detail.PropertyName))
+                                                       .Select(detail => new
+                                                       {
+                                                           detail.Id,
+                                                           detail.ActionsHistoryId,
+                                                           detail.PropertyName,
+                                                           detail.OriginalValue,
+                                                           detail.ModifiedValue
+                                                       })
+                                     })
                                      .ToArray();
 
             var actionHistoryData = actionsInfo
                 .Select(x => new ActionsHistoryDto.ActionsHistoryItemDto
-                                 {
-                                     Id = x.Item.Id,
-                                     ActionType = x.Item.ActionType.ToStringLocalized(EnumResources.ResourceManager, _userContext.Profile.UserLocaleInfo.UserCultureInfo),
-                                     CreatedBy = _userIdentifierService.GetUserInfo(x.Item.CreatedBy).DisplayName,
-                                     CreatedOn = x.Item.CreatedOn
-                                 })
+                {
+                    Id = x.Item.Id,
+                    ActionType = x.Item.ActionType.ToStringLocalized(EnumResources.ResourceManager, _userContext.Profile.UserLocaleInfo.UserCultureInfo),
+                    CreatedBy = _userIdentifierService.GetUserInfo(x.Item.CreatedBy).DisplayName,
+                    CreatedOn = x.Item.CreatedOn
+                })
                 .ToArray();
 
             var actionHistoryDetailsData = actionsInfo.SelectMany(x => x.Details,
                                                                   (x, y) => new ActionsHistoryDto.ActionsHistoryDetailDto
-                                                                                {
-                                                                                    Id = y.Id,
-                                                                                    ActionsHistoryId = y.ActionsHistoryId,
-                                                                                    PropertyName = MetadataResources.ResourceManager.GetString(y.PropertyName,
-                                                                                                                                               _userContext.Profile.UserLocaleInfo
-                                                                                                                                                           .UserCultureInfo),
-                                                                                    OriginalValue = ProcessValue(entityName, y.PropertyName, y.OriginalValue),
-                                                                                    ModifiedValue = ProcessValue(entityName, y.PropertyName, y.ModifiedValue)
-                                                                                })
+                                                                  {
+                                                                      Id = y.Id,
+                                                                      ActionsHistoryId = y.ActionsHistoryId,
+                                                                      PropertyName = MetadataResources.ResourceManager.GetString(y.PropertyName,
+                                                                                                                                 _userContext.Profile.UserLocaleInfo
+                                                                                                                                             .UserCultureInfo),
+                                                                      OriginalValue = ProcessValue(entityName, y.PropertyName, y.OriginalValue),
+                                                                      ModifiedValue = ProcessValue(entityName, y.PropertyName, y.ModifiedValue)
+                                                                  })
                                                       .ToArray();
 
             return new ActionsHistoryDto
-                       {
-                           ActionHistoryData = actionHistoryData,
-                           ActionHistoryDetailsData = actionHistoryDetailsData
-                       };
+            {
+                ActionHistoryData = actionHistoryData,
+                ActionHistoryDetailsData = actionHistoryDetailsData
+            };
         }
 
         private string ProcessValue(EntityName entityName, string propertyName, string value)
@@ -114,20 +114,20 @@ namespace DoubleGis.Erm.BLCore.Operations.Generic.ActionHistory
                 case "ModifiedBy":
                     return _userIdentifierService.GetUserInfo(long.Parse(value)).DisplayName;
                 case "WorkflowStepId":
-                    return EnumUtils.ParseEnum<OrderState>(value).ToStringLocalized(EnumResources.ResourceManager, _userContext.Profile.UserLocaleInfo.UserCultureInfo);
+                    return EnumUtils.ParseEnum<OrderState>(value).ToStringLocalized(EnumResources.ResourceManager, EnumResources.Culture);
                 case "DealStage":
-                    return EnumUtils.ParseEnum<DealStage>(value).ToStringLocalized(EnumResources.ResourceManager, _userContext.Profile.UserLocaleInfo.UserCultureInfo);
+                    return EnumUtils.ParseEnum<DealStage>(value).ToStringLocalized(EnumResources.ResourceManager, EnumResources.Culture);
                 case "Status":
                     if (entityName == EntityName.Limit)
                     {
                         LimitStatus status;
-                        return EnumUtils.TryParseEnum(value, out status) ? status.ToStringLocalized(EnumResources.ResourceManager, CultureInfo.CurrentCulture) : value;
+                        return EnumUtils.TryParseEnum(value, out status) ? status.ToStringLocalized(EnumResources.ResourceManager, EnumResources.Culture) : value;
                     }
 
                     if (entityName == EntityName.AdvertisementElementStatus)
                     {
                         AdvertisementElementStatusValue status;
-                        return EnumUtils.TryParseEnum(value, out status) ? status.ToStringLocalized(EnumResources.ResourceManager, CultureInfo.CurrentCulture) : value;
+                        return EnumUtils.TryParseEnum(value, out status) ? status.ToStringLocalized(EnumResources.ResourceManager, EnumResources.Culture) : value;
                     }
 
                     break;
