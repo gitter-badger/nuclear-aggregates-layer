5d2d6842b994e40070eb828a5ed3fcf66cea0a85
Alex Pashkin <a.pashkin@2gis.ru>
2015-01-13 11:50:05 +0600

    Added ForClientAndItsDescendants filter logic for Contacts and Deals

--- BLQuerying/Source/ApplicationServices/2Gis.Erm.BLQuerying.Operations.Listing/List/ListContactService.cs
+++ BLQuerying/Source/ApplicationServices/2Gis.Erm.BLQuerying.Operations.Listing/List/ListContactService.cs
@@ -31,6 +31,12 @@ namespace DoubleGis.Erm.BLQuerying.Operations.Listing.List
         {
             var query = _finder.FindAll<Contact>();
 
+            bool forClientAndItsDescendants;
+            if (querySettings.TryGetExtendedProperty("ForClientAndItsDescendants", out forClientAndItsDescendants) && querySettings.ParentEntityId.HasValue)
+            {
+                query = _filterHelper.ForClientAndItsDescendants(query, querySettings.ParentEntityId.Value);
+            }
+
             return query
             .Select(x => new ListContactDto
             {
