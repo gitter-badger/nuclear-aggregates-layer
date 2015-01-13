e8864241df0939380ea9d3496bbcaa7b4b4b555c
Баранихин Юрий <y.baranihin@2gis.ru>
2015-01-13 12:00:42 +0600

    Re: CR

--- BLCore/Source/EntryPoints/UI/Common/2Gis.Erm.BLCore.UI.Metadata/Config/Cards/CardMetadataBuilder.cs
+++ BLCore/Source/EntryPoints/UI/Common/2Gis.Erm.BLCore.UI.Metadata/Config/Cards/CardMetadataBuilder.cs
@@ -11,8 +11,6 @@ using DoubleGis.Erm.Platform.Model.Metadata.Common.Elements.Aspects.Features.Res
 using DoubleGis.Erm.Platform.Model.Metadata.Common.Elements.Aspects.Features.Resources.Titles;
 using DoubleGis.Erm.Platform.Model.Metadata.Entities.CommonFeatures;
 using DoubleGis.Erm.Platform.UI.Metadata.Config.Common.Card.Features.Parts;
-using DoubleGis.Erm.Platform.UI.Metadata.Config.Common.Features.Actions;
-using DoubleGis.Erm.Platform.UI.Metadata.Config.Common.Features.RelatedItems;
 using DoubleGis.Erm.Platform.UI.Metadata.Config.Common.ViewModel;
 using DoubleGis.Erm.Platform.UI.Metadata.UIElements.Features;
 
@@ -21,40 +19,18 @@ namespace DoubleGis.Erm.BLCore.UI.Metadata.Config.Cards
     public sealed class CardMetadataBuilder<TEntity> : ViewModelMetadataBuilder<CardMetadataBuilder<TEntity>, CardMetadata>
         where TEntity : IEntity, IEntityKey
     {
-        private readonly TitleFeatureAspect<CardMetadataBuilder<TEntity>, CardMetadata> _title;
         private readonly ImageFeatureAspect<CardMetadataBuilder<TEntity>, CardMetadata> _icon;
-        private readonly RelatedItemsFeatureAspect<CardMetadataBuilder<TEntity>, CardMetadata> _relatedItems;
-        private readonly ActionsFeatureAspect<CardMetadataBuilder<TEntity>, CardMetadata> _actions;
-
         private readonly EntityName _entityName;
 
         public CardMetadataBuilder()
         {
-            _title = new TitleFeatureAspect<CardMetadataBuilder<TEntity>, CardMetadata>(this);
             _icon = new ImageFeatureAspect<CardMetadataBuilder<TEntity>, CardMetadata>(this);
-            _relatedItems = new RelatedItemsFeatureAspect<CardMetadataBuilder<TEntity>, CardMetadata>(this);
-            _actions = new ActionsFeatureAspect<CardMetadataBuilder<TEntity>, CardMetadata>(this);
             _entityName = typeof(TEntity).AsEntityName();
-        }
-
-        public TitleFeatureAspect<CardMetadataBuilder<TEntity>, CardMetadata> Title
-        {
-            get { return _title; }
         }
 
         public ImageFeatureAspect<CardMetadataBuilder<TEntity>, CardMetadata> Icon
         {
             get { return _icon; }
-        }
-
-        public new RelatedItemsFeatureAspect<CardMetadataBuilder<TEntity>, CardMetadata> RelatedItems
-        {
-            get { return _relatedItems; }
-        }
-
-        public new ActionsFeatureAspect<CardMetadataBuilder<TEntity>, CardMetadata> Actions
-        {
-            get { return _actions; }
         }
 
         public CardMetadataBuilder<TEntity> EntityLocalization<TKey>(Expression<Func<TKey>> resourceKeyExpression)
