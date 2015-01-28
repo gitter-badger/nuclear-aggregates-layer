d11a84d670083b8d1eb59e956b163e3df0dcd8c9
Баранихин Юрий <y.baranihin@2gis.ru>
2015-01-28 12:35:04 +0600

    Re:CR

--- BLCore/Source/EntryPoints/UI/Common/2Gis.Erm.BLCore.UI.Metadata/Aspects/Entities/Aggregations/INewableAndPublishablePriceAspects.cs
+++ BLCore/Source/EntryPoints/UI/Common/2Gis.Erm.BLCore.UI.Metadata/Aspects/Entities/Aggregations/INewableAndPublishablePriceAspects.cs
@@ -3,7 +3,7 @@ using DoubleGis.Erm.Platform.Model.Aspects.Entities;
 
 namespace DoubleGis.Erm.BLCore.UI.Metadata.Aspects.Entities.Aggregations
 {
-    public interface INewableAndPublishablePriceAspects : INewableAspect, IPublishablePriceAspect
+    public interface INewableAndPublishablePriceAspects : IAspectsAggregation, INewableAspect, IPublishablePriceAspect
     {
     }
 }
---
+++ Platform/Source/Model/2Gis.Erm.Platform.Model/Aspects/IAspectsAggregation.cs
@@ -0,0 +1,9 @@
+﻿namespace DoubleGis.Erm.Platform.Model.Aspects
+{
+    /// <summary>
+    /// Маркерный интерфейс агрегата аспетов. Такие штуки сейчас вызывают вопросы и, вероятно, от них нужно будет избавиться.
+    /// </summary>
+    public interface IAspectsAggregation
+    {
+    }
+}
