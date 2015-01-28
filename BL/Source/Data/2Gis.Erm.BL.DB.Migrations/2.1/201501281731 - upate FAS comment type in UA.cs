3def4f1cc39153b65f15ae2be213e6ee86d31e1a
a.rechkalov <a.rechkalov@2gis.ru>
2015-01-28 18:02:24 +0600

    перенёс прежние комментарии в свободную форму ("новый комментарий")

---
+++ BL/Source/Data/2Gis.Erm.BL.DB.Migrations/2.1/201501281731 - upate FAS comment type in UA.cs
@@ -0,0 +1,17 @@
+﻿using DoubleGis.Erm.Platform.Migration.Base;
+using DoubleGis.Erm.Platform.Migration.Core;
+
+namespace DoubleGis.Erm.BL.DB.Migrations
+{
+    [Migration(201501281731, "Миграция существующих украинских ФАС на тип 'Новый комментарий'", "a.rechkalov")]
+    public class Migration201501281731 : TransactedMigration
+    {
+        // Кроме как в украинских ЭРМ не должно быть украинских комментариях - поэтому не заморачиваемся
+        private const string UpdateStatement = "update Billing.AdvertisementElements set FasCommentType = 6 where FasCommentType in (401, 402, 403)";
+
+        protected override void ApplyOverride(IMigrationContext context)
+        {
+            context.Connection.ExecuteNonQuery(UpdateStatement);
+        }
+    }
+}
