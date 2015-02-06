deac270d7ca859c02dac96436b1301e9cbeca3c1
Alex Pashkin <a.pashkin@2gis.ru>
2015-02-06 12:36:02 +0600

    Write a Migration

---
+++ BLCore/Source/Data/2Gis.Erm.BLCore.DB.Migrations/2.1/201502061213 - Remove FirstCall for Appointmens.cs
@@ -0,0 +1,16 @@
+﻿using DoubleGis.Erm.Platform.Migration.Base;
+using DoubleGis.Erm.Platform.Migration.Core;
+
+namespace DoubleGis.Erm.BLCore.DB.Migrations._2._1
+{
+    [Migration(201502061213, "ERM-5657. Убрать цель в действии типа Встреча", "a.pashkin")]
+    public class Migration201502061213 : TransactedMigration
+    {
+        private const string UpdateQuery = "UPDATE [Activity].[AppointmentBase] SET Purpose = 3 WHERE Purpose = 1";
+
+        protected override void ApplyOverride(IMigrationContext context)
+        {
+            context.Database.ExecuteNonQuery(UpdateQuery);
+        }
+    }
+}
