d158d4a8930752bea028c6c0d9d464bf4be8ab61
Alex Pashkin <a.pashkin@2gis.ru>
2015-02-12 16:40:53 +0600

    Add CancelActivity to CRM

--- BL/Source/Data/2Gis.Erm.BL.DB.Migrations/2.1/201502121619 - Add cancel activity to  CRM.cs
+++ BL/Source/Data/2Gis.Erm.BL.DB.Migrations/2.1/201502121619 - Add cancel activity to  CRM.cs
@@ -5,12 +5,12 @@ using DoubleGis.Erm.Platform.Migration.CRM;
 
 namespace DoubleGis.Erm.BL.DB.Migrations._2._1
 {
-    [Migration(201502051735, "Массовое назначение для действий в CRM", "a.pashkin")]
-    public class Migration201502051735 : CrmImportAndPublishCustomizationMigration
+    [Migration(201502121619, "ERM-5694. Массовая отмена для действий в CRM", "a.pashkin")]
+    public class Migration201502121619 : CrmImportAndPublishCustomizationMigration
     {
         protected override XElement GetCustomizationXml(ICrmMigrationContext context)
         {
-            return XElement.Parse(Properties.Resources.customizations_201502051735);
+            return XElement.Parse(Properties.Resources.customizations_201502121619);
         }
     }
 }
