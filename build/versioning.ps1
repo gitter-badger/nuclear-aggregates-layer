4dd59d152b690baa8db2a39f1843bbd54f278c3b
m.pashuk <m.pashuk@2gis.ru>
2015-02-13 19:44:40 +0600

    root build folder

---
+++ build/versioning.ps1
@@ -0,0 +1,19 @@
+﻿Set-StrictMode -Version Latest
+$ErrorActionPreference = 'Stop'
+#------------------------------
+
+Import-Module "$BuildToolsRoot\modules\versioning.psm1" -DisableNameChecking
+
+Task Set-BuildNumber {
+	$version = Get-Version
+	
+	if (Test-Path 'Env:\TEAMCITY_VERSION') {
+		Write-Host "##teamcity[buildNumber '$($version.SemanticVersion)']"
+	}
+}
+
+Task Update-AssemblyInfo {
+	$globalDir = Join-Path $global:Context.Dir.Solution '..\..'
+	$assemblyInfos = Get-ChildItem $globalDir -Filter 'AssemblyInfo.Version.cs' -Recurse
+	Update-AssemblyInfo $assemblyInfos
+}
