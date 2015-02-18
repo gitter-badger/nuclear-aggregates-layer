cbccc3f1e91b05fb9d4cdd749149410396bdb8aa
m.pashuk <m.pashuk@2gis.ru>
2015-02-18 12:13:01 +0600

    Revert "длинные имена"
    This reverts commit 4df0a93bb6ee1dc54e27a7ef03f4c21b072fec7c.

--- build/metadata.servicebus.psm1
+++ build/metadata.servicebus.psm1
@@ -6,13 +6,13 @@ $ErrorActionPreference = 'Stop'
 $SharedAccessKeys = @{
 	'Production.Russia' = 'e97SXT9BYyBHpANc+ZjiOjp97EHqcaFii/BK77Ntijo='
 	'Load.Russia' = 'daOQn6EYUWbJPlfV1592+xCIQRTumxZzzEw2c1i1u0M='
-	'Test.Russia.Crm.01' = 'VMjlhLcjp4K3+Ur6c2c8vEbEsRZVbFXHuiv9MRW7Y34='
-	'Test.Russia.Crm.02' = 'UQcwkyrf7YrfjVVmq0Y3J7fIQIpfc4UuUHcORMeg+vA='
-	'Test.Russia.Crm.03' = 'TmElHfY7fGenC9thyWL+/P/lDrcthzzKSoF2lB8LSBs='
-	'Test.Russia.Crm.04' = 'qXjGMK2DyIySJtVL0SjQM3RcNYnN365gHtmpkb9IWKs='
-	'Test.Russia.Crm.05' =	'fJrj72O4D4JJ4dE06xDCp1eFFRDkYbqbHL1aIYAEelE='
-	'Test.Russia.Crm.06' =	'JB2JhAMShwaCCbhOxohqnNy/HpxSQUT2SWpDeUSgOM4='
-	'Test.Russia.08' =	'P+wgbmmPDO+3G3OQi/cT3e0Ym6TgMpV4BpN8zkNW9WI='
+	'Test.01' = 'VMjlhLcjp4K3+Ur6c2c8vEbEsRZVbFXHuiv9MRW7Y34='
+	'Test.02' = 'UQcwkyrf7YrfjVVmq0Y3J7fIQIpfc4UuUHcORMeg+vA='
+	'Test.03' = 'TmElHfY7fGenC9thyWL+/P/lDrcthzzKSoF2lB8LSBs='
+	'Test.04' = 'qXjGMK2DyIySJtVL0SjQM3RcNYnN365gHtmpkb9IWKs='
+	'Test.05' =	'fJrj72O4D4JJ4dE06xDCp1eFFRDkYbqbHL1aIYAEelE='
+	'Test.06' =	'JB2JhAMShwaCCbhOxohqnNy/HpxSQUT2SWpDeUSgOM4='
+	'Test.08' =	'P+wgbmmPDO+3G3OQi/cT3e0Ym6TgMpV4BpN8zkNW9WI='
 }
 
 function Get-SharedAccessKeyMetadata($EnvName){
