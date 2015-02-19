aa06fa0c9c9f4d00e70db43d0133092a7bc21bbe
Alex Pashkin <a.pashkin@2gis.ru>
2015-02-19 17:23:47 +0600

    Revert changes from ActivityReadService

--- BLCore/Source/API/2Gis.Erm.BLCore.API.Operations/Generic/Read/IActivityReadService.cs
+++ BLCore/Source/API/2Gis.Erm.BLCore.API.Operations/Generic/Read/IActivityReadService.cs
@@ -2,7 +2,6 @@
 
 using DoubleGis.Erm.Platform.API.Core.Operations;
 using DoubleGis.Erm.Platform.Model.Entities;
-using DoubleGis.Erm.Platform.Model.Entities.Activity;
 using DoubleGis.Erm.Platform.Model.Entities.Interfaces;
 using DoubleGis.Erm.Platform.Model.Identities.Operations.Identity.Specific.Activity;
 
@@ -17,8 +16,5 @@ namespace DoubleGis.Erm.BLCore.API.Operations.Generic.Read
         
         IEnumerable<IEntity> LookupActivitiesRegarding(EntityName entityName, long clientId);
 
-        void CheckIfAnyEntityReferencesContainsReserve(IEnumerable<EntityReference> references);
-
-        void CheckIfEntityReferencesContainsReserve(EntityReference reference);
     }
 }
