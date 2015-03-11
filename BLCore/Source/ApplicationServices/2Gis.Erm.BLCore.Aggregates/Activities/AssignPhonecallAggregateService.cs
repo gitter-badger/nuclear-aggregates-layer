f9ec240fe9df667df5d43ce14463c4a8ea44f718
Alex Pashkin <a.pashkin@2gis.ru>
2015-03-11 13:25:45 +0600

    [ERM-5857] Logging change owner

--- BLCore/Source/ApplicationServices/2Gis.Erm.BLCore.Aggregates/Activities/AssignPhonecallAggregateService.cs
+++ BLCore/Source/ApplicationServices/2Gis.Erm.BLCore.Aggregates/Activities/AssignPhonecallAggregateService.cs
@@ -2,6 +2,7 @@ using System;
 
 using DoubleGis.Erm.BLCore.API.Aggregates.Activities;
 using DoubleGis.Erm.BLCore.Resources.Server.Properties;
+using DoubleGis.Erm.Platform.API.Core.ActionLogging;
 using DoubleGis.Erm.Platform.API.Core.Exceptions;
 using DoubleGis.Erm.Platform.API.Core.Operations.Logging;
 using DoubleGis.Erm.Platform.DAL;
@@ -13,13 +14,18 @@ namespace DoubleGis.Erm.BLCore.Aggregates.Activities
     public sealed class AssignPhonecallAggregateService : IAssignPhonecallAggregateService
     {
         private readonly IOperationScopeFactory _operationScopeFactory;
+
+        private readonly IActionLogger _actionLogger;
+
         private readonly IRepository<Phonecall> _repository;
 
         public AssignPhonecallAggregateService(
             IOperationScopeFactory operationScopeFactory,
+            IActionLogger actionLogger,
             IRepository<Phonecall> repository)
         {
             _operationScopeFactory = operationScopeFactory;
+            _actionLogger = actionLogger;
             _repository = repository;
         }
 
@@ -37,10 +43,13 @@ namespace DoubleGis.Erm.BLCore.Aggregates.Activities
 
             using (var operationScope = _operationScopeFactory.CreateSpecificFor<AssignIdentity, Phonecall>())
             {
+                var originalOwnerCode = phonecall.OwnerCode;
                 phonecall.OwnerCode = ownerCode;
 
                 _repository.Update(phonecall);
                 _repository.Save();
+
+                _actionLogger.LogChanges(phonecall, x => x.OwnerCode, originalOwnerCode, ownerCode);
 
                 operationScope.Updated<Phonecall>(phonecall.Id);
                 operationScope.Complete();
--- BLCore/Source/ApplicationServices/2Gis.Erm.BLCore.Aggregates/Activities/AssignTaskAggregateService.cs
+++ BLCore/Source/ApplicationServices/2Gis.Erm.BLCore.Aggregates/Activities/AssignTaskAggregateService.cs
@@ -2,6 +2,7 @@ using System;
 
 using DoubleGis.Erm.BLCore.API.Aggregates.Activities;
 using DoubleGis.Erm.BLCore.Resources.Server.Properties;
+using DoubleGis.Erm.Platform.API.Core.ActionLogging;
 using DoubleGis.Erm.Platform.API.Core.Exceptions;
 using DoubleGis.Erm.Platform.API.Core.Operations.Logging;
 using DoubleGis.Erm.Platform.DAL;
@@ -14,13 +15,18 @@ namespace DoubleGis.Erm.BLCore.Aggregates.Activities
     public sealed class AssignTaskAggregateService : IAssignTaskAggregateService
     {
         private readonly IOperationScopeFactory _operationScopeFactory;
+
+        private readonly IActionLogger _actionLogger;
+
         private readonly IRepository<Task> _repository;
 
         public AssignTaskAggregateService(
             IOperationScopeFactory operationScopeFactory,
+            IActionLogger actionLogger,
             IRepository<Task> repository)
         {
             _operationScopeFactory = operationScopeFactory;
+            _actionLogger = actionLogger;
             _repository = repository;
         }
 
@@ -38,10 +44,13 @@ namespace DoubleGis.Erm.BLCore.Aggregates.Activities
 
             using (var operationScope = _operationScopeFactory.CreateSpecificFor<AssignIdentity, Task>())
             {
+                var originalOwnerCode = task.OwnerCode;
                 task.OwnerCode = ownerCode;
 
                 _repository.Update(task);
                 _repository.Save();
+
+                _actionLogger.LogChanges(task, x => x.OwnerCode, originalOwnerCode, ownerCode);
 
                 operationScope.Updated<Task>(task.Id);
                 operationScope.Complete();
