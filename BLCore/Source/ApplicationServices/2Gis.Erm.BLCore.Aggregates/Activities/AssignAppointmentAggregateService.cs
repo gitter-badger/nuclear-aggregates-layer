90e8966567fd6955627a416dc28cb2c20d2e703a
Alex Pashkin <a.pashkin@2gis.ru>
2015-03-12 13:40:41 +0600

    [ERM-5857] Remove logging from assign activity aggregates

--- BLCore/Source/ApplicationServices/2Gis.Erm.BLCore.Aggregates/Activities/AssignAppointmentAggregateService.cs
+++ BLCore/Source/ApplicationServices/2Gis.Erm.BLCore.Aggregates/Activities/AssignAppointmentAggregateService.cs
@@ -2,7 +2,6 @@ using System;
 
 using DoubleGis.Erm.BLCore.API.Aggregates.Activities;
 using DoubleGis.Erm.BLCore.Resources.Server.Properties;
-using DoubleGis.Erm.Platform.API.Core.ActionLogging;
 using DoubleGis.Erm.Platform.API.Core.Exceptions;
 using DoubleGis.Erm.Platform.API.Core.Operations.Logging;
 using DoubleGis.Erm.Platform.DAL;
@@ -14,18 +13,13 @@ namespace DoubleGis.Erm.BLCore.Aggregates.Activities
     public sealed class AssignAppointmentAggregateService : IAssignAppointmentAggregateService
     {
         private readonly IOperationScopeFactory _operationScopeFactory;
-
-        private readonly IActionLogger _actionLogger;
-
         private readonly IRepository<Appointment> _repository;
 
         public AssignAppointmentAggregateService(
             IOperationScopeFactory operationScopeFactory,
-            IActionLogger actionLogger,
             IRepository<Appointment> repository)
         {
             _operationScopeFactory = operationScopeFactory;
-            _actionLogger = actionLogger;
             _repository = repository;
         }
 
@@ -43,13 +37,10 @@ namespace DoubleGis.Erm.BLCore.Aggregates.Activities
 
             using (var operationScope = _operationScopeFactory.CreateSpecificFor<AssignIdentity, Appointment>())
             {
-                var originalOwnderCode = appointment.OwnerCode;
                 appointment.OwnerCode = ownerCode;
 
                 _repository.Update(appointment);
                 _repository.Save();
-
-                _actionLogger.LogChanges(appointment, x => x.OwnerCode, originalOwnderCode, ownerCode);
 
                 operationScope.Updated<Appointment>(appointment.Id);
                 operationScope.Complete();
--- BLCore/Source/ApplicationServices/2Gis.Erm.BLCore.Aggregates/Activities/AssignLetterAggregateService.cs
+++ BLCore/Source/ApplicationServices/2Gis.Erm.BLCore.Aggregates/Activities/AssignLetterAggregateService.cs
@@ -2,7 +2,6 @@ using System;
 
 using DoubleGis.Erm.BLCore.API.Aggregates.Activities;
 using DoubleGis.Erm.BLCore.Resources.Server.Properties;
-using DoubleGis.Erm.Platform.API.Core.ActionLogging;
 using DoubleGis.Erm.Platform.API.Core.Exceptions;
 using DoubleGis.Erm.Platform.API.Core.Operations.Logging;
 using DoubleGis.Erm.Platform.DAL;
@@ -14,18 +13,13 @@ namespace DoubleGis.Erm.BLCore.Aggregates.Activities
     public sealed class AssignLetterAggregateService : IAssignLetterAggregateService
     {
         private readonly IOperationScopeFactory _operationScopeFactory;
-
-        private readonly IActionLogger _actionLogger;
-
         private readonly IRepository<Letter> _repository;
 
         public AssignLetterAggregateService(
             IOperationScopeFactory operationScopeFactory,
-            IActionLogger actionLogger,
             IRepository<Letter> repository)
         {
             _operationScopeFactory = operationScopeFactory;
-            _actionLogger = actionLogger;
             _repository = repository;
         }
 
@@ -43,13 +37,10 @@ namespace DoubleGis.Erm.BLCore.Aggregates.Activities
 
             using (var operationScope = _operationScopeFactory.CreateSpecificFor<AssignIdentity, Letter>())
             {
-                var originalOwnerCode = letter.OwnerCode;
                 letter.OwnerCode = ownerCode;
 
                 _repository.Update(letter);
                 _repository.Save();
-
-                _actionLogger.LogChanges(letter, x => x.OwnerCode, originalOwnerCode, ownerCode);
 
                 operationScope.Updated<Letter>(letter.Id);
                 operationScope.Complete();
