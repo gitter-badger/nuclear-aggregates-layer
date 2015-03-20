8a8b94cd110bfee3bb8c2759073b14f1e045b7f7
Yury Baranikhin <BaranikhinYury@gmail.com>
2015-03-20 12:45:26 +0600

    [ERM-6050] добавил регистрацию операции CalculateOrderPositionPricePerUnit

--- BLCore/Source/ApplicationServices/2Gis.Erm.BLCore.Operations/Concrete/OrderPositions/CalculateOrderPositionPricePerUnitOperationService.cs
+++ BLCore/Source/ApplicationServices/2Gis.Erm.BLCore.Operations/Concrete/OrderPositions/CalculateOrderPositionPricePerUnitOperationService.cs
@@ -2,7 +2,9 @@
 using DoubleGis.Erm.BLCore.API.Operations.Concrete.OrderPositions;
 using DoubleGis.Erm.BLCore.API.Operations.Concrete.OrderPositions.Dto;
 using DoubleGis.Erm.BLCore.API.Operations.Special.CostCalculation;
+using DoubleGis.Erm.Platform.API.Core.Operations.Logging;
 using DoubleGis.Erm.Platform.Model.Entities.Enums;
+using DoubleGis.Erm.Platform.Model.Identities.Operations.Identity.Specific.OrderPosition;
 
 namespace DoubleGis.Erm.BLCore.Operations.Concrete.OrderPositions
 {
@@ -10,31 +12,38 @@ namespace DoubleGis.Erm.BLCore.Operations.Concrete.OrderPositions
     {
         private readonly IOrderReadModel _orderReadModel;
         private readonly ICostCalculator _costCalculator;
+        private readonly IOperationScopeFactory _operationScopeFactory;
 
-        public CalculateOrderPositionPricePerUnitOperationService(IOrderReadModel orderReadModel, ICostCalculator costCalculator)
+        public CalculateOrderPositionPricePerUnitOperationService(IOrderReadModel orderReadModel, ICostCalculator costCalculator, IOperationScopeFactory operationScopeFactory)
         {
             _orderReadModel = orderReadModel;
             _costCalculator = costCalculator;
+            _operationScopeFactory = operationScopeFactory;
         }
 
         public OrderPositionPricePerUnitDto CalculatePricePerUnit(long orderId, decimal categoryRate, decimal pricePositionCost)
         {
-            var orderType = _orderReadModel.GetOrderType(orderId);
-
-            if (orderType == OrderType.SelfAds || orderType == OrderType.SocialAds)
+            using (var scope = _operationScopeFactory.CreateNonCoupled<CalculateOrderPositionPricePerUnitIdentity>())
             {
-                return new OrderPositionPricePerUnitDto { PricePerUnit = 0m, PricePerUnitWithVat = 0m };
+                var orderType = _orderReadModel.GetOrderType(orderId);
+
+                if (orderType == OrderType.SelfAds || orderType == OrderType.SocialAds)
+                {
+                    scope.Complete();
+                    return new OrderPositionPricePerUnitDto { PricePerUnit = 0m, PricePerUnitWithVat = 0m };
+                }
+
+                bool showVat;
+                var vatRate = _orderReadModel.GetVatRate(orderId, out showVat);
+                var calcResult = _costCalculator.CalculatePricePerUnit(pricePositionCost, categoryRate, vatRate, showVat);
+
+                scope.Complete();
+                return new OrderPositionPricePerUnitDto
+                {
+                    PricePerUnit = calcResult.PricePerUnit,
+                    PricePerUnitWithVat = calcResult.PricePerUnitWithVat
+                };
             }
-
-            bool showVat;
-            var vatRate = _orderReadModel.GetVatRate(orderId, out showVat);
-            var calcResult = _costCalculator.CalculatePricePerUnit(pricePositionCost, categoryRate, vatRate, showVat);
-
-            return new OrderPositionPricePerUnitDto
-                       {
-                           PricePerUnit = calcResult.PricePerUnit,
-                           PricePerUnitWithVat = calcResult.PricePerUnitWithVat
-                       };
         }
     }
 }
