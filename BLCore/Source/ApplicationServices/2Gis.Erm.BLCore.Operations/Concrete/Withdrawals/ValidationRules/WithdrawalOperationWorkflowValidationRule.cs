56dec822987fc6c9e3f1eb2e732464e01c747d4f
Yury Baranikhin <BaranikhinYury@gmail.com>
2015-03-20 14:40:59 +0600

    [ERM-6130] fix - уточнение текста об ошибке

--- BLCore/Source/ApplicationServices/2Gis.Erm.BLCore.Operations/Concrete/Withdrawals/ValidationRules/WithdrawalOperationWorkflowValidationRule.cs
+++ BLCore/Source/ApplicationServices/2Gis.Erm.BLCore.Operations/Concrete/Withdrawals/ValidationRules/WithdrawalOperationWorkflowValidationRule.cs
@@ -27,7 +27,7 @@ namespace DoubleGis.Erm.BLCore.Operations.Concrete.Withdrawals.ValidationRules
                 return true;
             }
 
-            messages = new[] { string.Format("There is a withdrawal with a forbidden status. The status is '{0}'", lastWithdrawal.Status) };
+            messages = new[] { string.Format("There is a previous withdrawal with a forbidden status. The status is '{0}'", lastWithdrawal.Status) };
 
             return false;
         }
