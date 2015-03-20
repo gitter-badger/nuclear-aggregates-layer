ca30f089fcd1b15b468e51bbde220f7d6640952d
a.rechkalov <a.rechkalov@2gis.ru>
2015-03-20 14:41:41 +0600

    [ERM-6066] Новые ПФ

---
+++ BLFlex/Source/ApplicationServices/2Gis.Erm.BLFlex.Operations.Global/Russia/Concrete/Old/Orders/PrintForms/PrintBindingChangeAgreementHandler.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+using DoubleGis.Erm.BLCore.API.Operations.Concrete.Old.Common;
+using DoubleGis.Erm.BLCore.API.Operations.Concrete.Old.Orders.PrintForms;
+using DoubleGis.Erm.BLCore.API.Operations.Concrete.Orders;
+using DoubleGis.Erm.BLCore.Common.Infrastructure.Handlers;
+using DoubleGis.Erm.Platform.API.Core.Exceptions;
+using DoubleGis.Erm.Platform.Common.PrintFormEngine;
+using DoubleGis.Erm.Platform.DAL;
+using DoubleGis.Erm.Platform.DAL.Specifications;
+using DoubleGis.Erm.Platform.Model.Entities.Enums;
+using DoubleGis.Erm.Platform.Model.Entities.Erm;
+using DoubleGis.Erm.Platform.Model.Metadata.Globalization;
+
+using BLCoreResources = DoubleGis.Erm.BLCore.Resources.Server.Properties.BLResources;
+
+namespace DoubleGis.Erm.BLFlex.Operations.Global.Russia.Concrete.Old.Orders.PrintForms
+{
+    public sealed class PrintBindingChangeAgreementHandler : RequestHandler<PrintBindingChangeAgreementRequest, StreamResponse>, IRussiaAdapted
+    {
+        private readonly ISubRequestProcessor _requestProcessor;
+        private readonly IFinder _finder;
+
+        public PrintBindingChangeAgreementHandler(ISubRequestProcessor requestProcessor, IFinder finder)
+        {
+            _requestProcessor = requestProcessor;
+            _finder = finder;
+        }
+
+        protected override StreamResponse Handle(PrintBindingChangeAgreementRequest request)
+        {
+            var order = _finder.FindOne(Specs.Find.ById<Order>(request.OrderId));
+
+            if (order == null)
+            {
+                throw new EntityNotFoundException(typeof(Order), request.OrderId);
+            }
+
+            if (order.BranchOfficeOrganizationUnitId == null)
+            {
+                throw new NotificationException(BLCoreResources.OrderHasNoBranchOfficeOrganizationUnit);
+            }
+
+            if (order.LegalPersonProfileId == null)
+            {
+                throw new LegalPersonProfileMustBeSpecifiedException();
+            }
+
+            var currency = _finder.FindOne(Specs.Find.ById<Currency>(order.CurrencyId));
+            var bargain = _finder.FindOne(Specs.Find.ById<Bargain>(order.BargainId));
+            var legalPerson = _finder.FindOne(Specs.Find.ById<LegalPerson>(order.LegalPersonId));
+            var legalPersonProfile = _finder.FindOne(Specs.Find.ById<LegalPersonProfile>(order.LegalPersonProfileId));
+            var branchOfficeOrganizationUnit = _finder.FindOne(Specs.Find.ById<BranchOfficeOrganizationUnit>(order.BranchOfficeOrganizationUnitId));
+            var branchOffice = _finder.FindOne(Specs.Find.ById<BranchOffice>(branchOfficeOrganizationUnit.BranchOfficeId));
+            var firm = _finder.FindOne(Specs.Find.ById<Firm>(order.FirmId));
+
+            var documentData = PrintHelper.AgreementBody(order, legalPerson, legalPersonProfile, branchOfficeOrganizationUnit, firm);
+            var bargainData = PrintHelper.RelatedBrgain(bargain);
+            var requisites = PrintHelper.Requisites(legalPerson, legalPersonProfile, branchOffice, branchOfficeOrganizationUnit);
+
+            var printDocumentRequest = new PrintDocumentRequest
+            {
+                CurrencyIsoCode = currency.ISOCode,
+                FileName = order.Number,
+                BranchOfficeOrganizationUnitId = order.BranchOfficeOrganizationUnitId.Value,
+                TemplateCode = TemplateCode.BindingChangeAgreement,
+                PrintData = PrintData.Concat(documentData, requisites, bargainData)
+            };
+
+            var response = (StreamResponse)_requestProcessor.HandleSubRequest(printDocumentRequest, Context);
+            return response;
+        }
+    }
+}
---
+++ BLFlex/Source/ApplicationServices/2Gis.Erm.BLFlex.Operations.Global/Russia/Concrete/Old/Orders/PrintForms/PrintHelper.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+using DoubleGis.Erm.Platform.Common.PrintFormEngine;
+using DoubleGis.Erm.Platform.Model.Entities.Enums;
+using DoubleGis.Erm.Platform.Model.Entities.Erm;
+
+namespace DoubleGis.Erm.BLFlex.Operations.Global.Russia.Concrete.Old.Orders.PrintForms
+{
+    public static class PrintHelper
+    {
+        public static PrintData Requisites(LegalPerson legalPerson, LegalPersonProfile legalPersonProfile, BranchOffice branchOffice, BranchOfficeOrganizationUnit branchOfficeOrganizationUnit)
+        {
+            return new PrintData
+                       {
+                           { "UseLegalPerson", legalPerson.LegalPersonTypeEnum == LegalPersonType.LegalPerson },
+                           { "UseBusinessman", legalPerson.LegalPersonTypeEnum == LegalPersonType.Businessman },
+                           { "UseNaturalPerson", legalPerson.LegalPersonTypeEnum == LegalPersonType.NaturalPerson },
+
+                           { "BranchOffice.Name", branchOffice.Inn },
+                           { "BranchOffice.LegalAddress", branchOffice.LegalAddress },
+                           { "BranchOfficeOrganizationUnit.ShortLegalName", branchOfficeOrganizationUnit.ShortLegalName },
+                           { "BranchOfficeOrganizationUnit.Kpp", branchOfficeOrganizationUnit.Kpp },
+                           { "BranchOfficeOrganizationUnit.ActualAddress", branchOfficeOrganizationUnit.ActualAddress },
+                           { "BranchOfficeOrganizationUnit.PaymentEssentialElements", branchOfficeOrganizationUnit.PaymentEssentialElements },
+                           { "BranchOfficeOrganizationUnit.Email", branchOfficeOrganizationUnit.Email },
+                           { "BranchOfficeOrganizationUnit.PositionInNominative", branchOfficeOrganizationUnit.PositionInNominative },
+                           { "BranchOfficeOrganizationUnit.ChiefNameInNominative", branchOfficeOrganizationUnit.ChiefNameInNominative },
+
+                           { "LegalPerson.Inn", legalPerson.Inn },
+                           { "LegalPerson.Kpp", legalPerson.Kpp },
+                           { "LegalPerson.LegalAddress", legalPerson.LegalAddress },
+                           { "LegalPerson.LegalName", legalPerson.LegalName },
+                           { "LegalPerson.PassportSeries", legalPerson.PassportSeries },
+                           { "LegalPerson.PassportNumber", legalPerson.PassportNumber },
+                           { "LegalPerson.PassportIssuedBy", legalPerson.PassportIssuedBy },
+                           { "LegalPerson.RegistrationAddress", legalPerson.RegistrationAddress },
+                           { "LegalPerson.ShortName", legalPerson.ShortName },
+                           { "LegalPersonProfile.PaymentEssentialElements", legalPersonProfile.PaymentEssentialElements },
+                           { "LegalPersonProfile.PositionInNominative", legalPersonProfile.PositionInNominative },
+                           { "LegalPersonProfile.ChiefNameInNominative", legalPersonProfile.ChiefNameInNominative },
+                       };
+        }
+
+        public static PrintData RelatedBrgain(Bargain bargain)
+        {
+            return bargain == null
+                       ? new PrintData
+                             {
+                                 { "UseBargain", false },
+                             }
+                       : new PrintData
+                             {
+                                 { "UseBargain", true },
+                                 { "Bargain.Number", bargain.Number },
+                                 { "Bargain.CreatedOn", bargain.CreatedOn }
+                             };
+        }
+
+        public static PrintData AgreementBody(Order order, LegalPerson legalPerson, LegalPersonProfile legalPersonProfile, BranchOfficeOrganizationUnit branchOfficeOrganizationUnit, Firm firm)
+        {
+            return new PrintData
+                       {
+                           { "Now", DateTime.Today },
+                           { "Order.Number", order.Number },
+                           { "Order.CreatedOn", order.CreatedOn },
+                           { "LegalPerson.LegalName", legalPerson.LegalName },
+                           { "OrganizationUnitName", "ToDo" }, // FIXME {a.rechkalov, 19.03.2015}: закончить после ERM-6067
+
+                           { "LegalPersonProfile.PositionInGenitive", legalPersonProfile.PositionInGenitive },
+                           { "LegalPersonProfile.ChiefNameInGenitive", legalPersonProfile.ChiefNameInGenitive },
+                           { "LegalPersonProfile.OperatesOnTheBasisInGenitive", legalPersonProfile.OperatesOnTheBasisInGenitive },
+
+                           { "BranchOfficeOrganizationUnit.ShortLegalName", branchOfficeOrganizationUnit.ShortLegalName },
+                           { "BranchOfficeOrganizationUnit.PositionInGenitive", branchOfficeOrganizationUnit.PositionInGenitive },
+                           { "BranchOfficeOrganizationUnit.ChiefNameInGenitive", branchOfficeOrganizationUnit.ChiefNameInGenitive },
+                           { "BranchOfficeOrganizationUnit.OperatesOnTheBasisInGenitive", branchOfficeOrganizationUnit.OperatesOnTheBasisInGenitive },
+
+                           { "Firm.Name", firm.Name },
+                       };
+        }
+    }
+}
