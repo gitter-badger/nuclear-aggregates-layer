fedeec97c27af99c908802227eb1a5c866316057
a.rechkalov <a.rechkalov@2gis.ru>
2015-03-23 14:52:45 +0600

    [ERM-6066] Исключил лишние коды шаблонов

---
+++ BLFlex/Source/EntryPoints/UI/Web/2Gis.Erm.BLFlex.UI.Web.Mvc.Global/Services/Enumeration/Russia/RussiaTemplateCodeEnumCustomization.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using DoubleGis.Erm.BLCore.UI.Web.Mvc.Services.Enumeration;
+using DoubleGis.Erm.Platform.Model.Entities.Enums;
+using DoubleGis.Erm.Platform.Model.Metadata.Globalization;
+
+namespace DoubleGis.Erm.BLFlex.UI.Web.Mvc.Global.Services.Enumeration.Russia
+{
+    public class RussiaTemplateCodeEnumCustomization : EnumCustomizationBase<TemplateCode>, IRussiaAdapted
+    {
+        private static readonly IEnumerable<TemplateCode> IgnoredAdditionalAgreements =
+            new[]
+                {
+                    TemplateCode.AdditionalAgreementBusinessman,
+                    TemplateCode.AdditionalAgreementNaturalPerson,
+                };
+
+        protected override IEnumerable<TemplateCode> GetRequiredEnumValues()
+        {
+            return Enum.GetValues(typeof(TemplateCode))
+                       .Cast<TemplateCode>()
+                       .Except(IgnoredAdditionalAgreements);
+        }
+    }
+}
