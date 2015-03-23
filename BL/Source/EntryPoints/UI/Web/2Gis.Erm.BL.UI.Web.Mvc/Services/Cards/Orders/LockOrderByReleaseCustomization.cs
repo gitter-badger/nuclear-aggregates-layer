b7c1165109165051e53b3480670fa23a7ae976d1
Yury Baranikhin <BaranikhinYury@gmail.com>
2015-03-23 17:37:48 +0600

    [ERM-6147] fix

--- BL/Source/EntryPoints/UI/Web/2Gis.Erm.BL.UI.Web.Mvc/Services/Cards/Orders/LockOrderByReleaseCustomization.cs
+++ BL/Source/EntryPoints/UI/Web/2Gis.Erm.BL.UI.Web.Mvc/Services/Cards/Orders/LockOrderByReleaseCustomization.cs
@@ -7,6 +7,7 @@ using DoubleGis.Erm.BLCore.UI.Metadata.Aspects.Entities;
 using DoubleGis.Erm.BLCore.UI.Web.Mvc.Services.Cards;
 using DoubleGis.Erm.BLCore.UI.Web.Mvc.ViewModels;
 using DoubleGis.Erm.Platform.API.Core;
+using DoubleGis.Erm.Platform.Common.Utils;
 using DoubleGis.Erm.Platform.Model.Aspects.Entities;
 using DoubleGis.Erm.Platform.Model.Entities.Enums;
 using DoubleGis.Erm.Platform.Model.Entities.Erm;
@@ -36,7 +37,7 @@ namespace DoubleGis.Erm.BL.UI.Web.Mvc.Services.Cards.Orders
             if (orderWorkflowAspect.WorkflowStepId == OrderState.Approved || orderWorkflowAspect.WorkflowStepId == OrderState.OnTermination)
             {
                 var isReleaseInProgress = _releaseReadModel.HasFinalReleaseInProgress(orderDirectionAspect.DestinationOrganizationUnitKey.Value,
-                                                                                      new TimePeriod(orderDatesAspect.BeginDistributionDate, orderDatesAspect.EndDistributionDateFact));
+                                                                                      new TimePeriod(orderDatesAspect.BeginDistributionDate, orderDatesAspect.EndDistributionDateFact.GetEndPeriodOfThisMonth()));
                 if (isReleaseInProgress)
                 {
                     viewModel.LockOrderToolbar();
