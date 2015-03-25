6e46014d7911d6a878a2f1011824b447c551ee57
Alex Pashkin <a.pashkin@2gis.ru>
2015-03-25 11:39:09 +0600

    [ERM-6103] a little bit of code

---
+++ BLCore/Source/API/2Gis.Erm.BLCore.API.Operations.Special/Remote/Dial/IDialApplicationService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+using DoubleGis.Erm.Platform.API.Core;
+
+namespace DoubleGis.Erm.BLCore.API.Operations.Special.Remote.Dial
+{
+    [ServiceContract(SessionMode = SessionMode.Allowed, Namespace = ServiceNamespaces.FinancialOperations.FinancialOperations201310)]
+    public interface IDialApplicationService
+    {
+    }
+}
---
+++ BLCore/Source/EntryPoints/ServiceBackend/2Gis.Erm.BLCore.WCF.Operations.Special/Dial/DialApplicationService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoubleGis.Erm.BLCore.WCF.Operations.Special.Dial
+{
+    [ServiceBehavior(InstanceContextMode = InstanceContextMode.PerCall, ConcurrencyMode = ConcurrencyMode.Single)]
+    public class DialApplicationService
+    {
+    }
+}
