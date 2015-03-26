9456bcaec29866dd6326155727b174d22e805b0e
Denis Ivanov <d.ivanov@2gis.ru>
2015-03-26 18:21:01 +0600

    security assembly infos

---
+++ NuClear/Security.API/Properties/AssemblyInfo.Common.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Reflection;
+using System.Resources;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// attributes for all assemblies
+[assembly: AssemblyCompany("2GIS")]
+[assembly: AssemblyProduct("2GIS ERM")]
+[assembly: CLSCompliant(false)]
+[assembly: ComVisible(false)]
+
+#if !SILVERLIGHT
+
+[assembly: SuppressIldasm]
+[assembly: NeutralResourcesLanguage("en", UltimateResourceFallbackLocation.Satellite)]
+
+#endif
+
+#if DEBUG
+
+[assembly: AssemblyConfiguration("Debug")]
+[assembly: AssemblyDescription("This is Debug version of assembly")]
+
+#else
+
+[assembly: AssemblyConfiguration("Release")]
+[assembly: AssemblyDescription("This is Release version of assembly")]
+
+#endif
---
+++ NuClear/Security/Properties/AssemblyInfo.Version.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using System.Resources;
+
+[assembly: AssemblyVersion("2.0.0")]
+[assembly: SatelliteContractVersion("2.0.0")]
+
+[assembly: AssemblyFileVersion("0.0.0")]
+[assembly: AssemblyInformationalVersion("0.0.0")]
+
+internal static class ThisAssembly
+{
+    internal const string SemanticVersion = "0.0.0";
+    internal const string Build = "0";
+}
