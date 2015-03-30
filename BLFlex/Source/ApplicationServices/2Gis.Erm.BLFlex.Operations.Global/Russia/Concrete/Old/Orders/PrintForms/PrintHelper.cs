a824aa8cc3aa22fba7f5d0c0d0679b758517539c
a.rechkalov <a.rechkalov@2gis.ru>
2015-03-30 11:26:07 +0600

    [ERM-6066] Заменил дату на начало следующего месяца

--- BLFlex/Source/ApplicationServices/2Gis.Erm.BLFlex.Operations.Global/Russia/Concrete/Old/Orders/PrintForms/PrintHelper.cs
+++ BLFlex/Source/ApplicationServices/2Gis.Erm.BLFlex.Operations.Global/Russia/Concrete/Old/Orders/PrintForms/PrintHelper.cs
@@ -98,7 +98,7 @@ namespace DoubleGis.Erm.BLFlex.Operations.Global.Russia.Concrete.Old.Orders.Prin
         {
             return new PrintData
                        {
-                           { "DateToday", DateTime.Today },
+                           { "NextMonth", DateTime.Today.GetNextMonthFirstDate() },
                            { "Firm.Name", firm.Name },
                        };
         }
