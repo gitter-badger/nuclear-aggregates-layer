e7eb69a77a4f25caabb3348ccf45af3d6d48de69
a.rechkalov <a.rechkalov@2gis.ru>
2015-04-09 16:37:16 +0600

    [ERM-6177] Исправил часовые зоны в MS Dynamics

---
+++ BLCore/Source/Data/2Gis.Erm.BLCore.DB.Migrations/2.1/201504090414 - Fix MS Dynamics TimeZones.cs
@@ -0,0 +1,21 @@
+﻿using DoubleGis.Erm.BLCore.DB.Migrations.Properties;
+using DoubleGis.Erm.Platform.Migration.Base;
+using DoubleGis.Erm.Platform.Migration.Core;
+
+namespace DoubleGis.Erm.BLCore.DB.Migrations._2._1
+{
+    [Migration(201504090414, "Исправление часовых поясов MS Dynamics", "a.rechkalov")]
+    public class Migration201504090414 : TransactedMigration, INonDefaultDatabaseMigration
+    {
+        public ErmConnectionStringKey ConnectionStringKey
+        {
+            get { return ErmConnectionStringKey.CrmDatabase; }
+        }
+
+        protected override void ApplyOverride(IMigrationContext context)
+        {
+            var script = Resources.script_201504090414;
+            context.Database.ExecuteNonQuery(script);
+        }
+    }
+}
