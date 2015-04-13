05afd10dfd24e5c09a763da9c571ba3de7acdeda
a.rechkalov <a.rechkalov@2gis.ru>
2015-04-13 11:54:51 +0600

    [CR] Идемпотентность миграции

--- BLCore/Source/Data/2Gis.Erm.BLCore.DB.Migrations/2.1/201504090414 - Fix MS Dynamics TimeZones.cs
+++ BLCore/Source/Data/2Gis.Erm.BLCore.DB.Migrations/2.1/201504090414 - Fix MS Dynamics TimeZones.cs
@@ -7,6 +7,9 @@ namespace DoubleGis.Erm.BLCore.DB.Migrations._2._1
     [Migration(201504090414, "Исправление часовых поясов MS Dynamics", "a.rechkalov")]
     public class Migration201504090414 : TransactedMigration, INonDefaultDatabaseMigration
     {
+        private const string MarkerTimeZone = "Kaliningrad Standard Time";
+        private const string MarkerQueryTemplate = "select count (*) from [dbo].[TimeZoneDefinitionBase] where StandardName = '{0}'";
+
         public ErmConnectionStringKey ConnectionStringKey
         {
             get { return ErmConnectionStringKey.CrmDatabase; }
@@ -14,8 +17,14 @@ namespace DoubleGis.Erm.BLCore.DB.Migrations._2._1
 
         protected override void ApplyOverride(IMigrationContext context)
         {
+            var count = (int)context.Connection.ExecuteScalar(string.Format(MarkerQueryTemplate, MarkerTimeZone));
+            if (count > 0)
+            {
+                return;
+            }
+            
             var script = Resources.script_201504090414;
-            context.Database.ExecuteNonQuery(script);
+            context.Connection.ExecuteNonQuery(script);
         }
     }
 }
