8c8877290b03f92c6429244178c0ac90c7798f66
flibustier7seas <flibustier7seas@gmail.com>
2015-04-27 13:19:07 +0600

    Удалил тег <ol> из списка разрешенных

--- CompositionRoots/Source/2Gis.Erm.UI.Web.Mvc/Scripts/Ext.DoubleGis.UI.AdvertisementElement.js
+++ CompositionRoots/Source/2Gis.Erm.UI.Web.Mvc/Scripts/Ext.DoubleGis.UI.AdvertisementElement.js
@@ -180,7 +180,7 @@
                 theme_advanced_toolbar_align: "left",
                 theme_advanced_statusbar_location: "bottom",
                 theme_advanced_resizing: false,
-                valid_elements: "p,br,strong/b,em/i,ul,ol,li",
+                valid_elements: "p,br,strong/b,em/i,ul,li",
 
                 // очищаем формат при вставке, иначе tinymce намертво повисает
                 paste_remove_styles: true,
