ee3ccca4bc43bc59457b3466059572fea2eb686b
Alexander Tukaev <tukaef@gmail.com>
2015-04-30 11:29:50 +0600

    [ERM-6444] fix

--- Platform/Source/2Gis.Erm.Platform.Core/Identities/NullIdentityProvider.cs
+++ Platform/Source/2Gis.Erm.Platform.Core/Identities/NullIdentityProvider.cs
@@ -1,16 +1,21 @@
-﻿using DoubleGis.Erm.Platform.API.Core.Identities;
+﻿using System;
+using System.Collections.Generic;
+
+using DoubleGis.Erm.Platform.API.Core.Identities;
+using DoubleGis.Erm.Platform.Model.Entities.Interfaces;
 
 namespace DoubleGis.Erm.Platform.Core.Identities
 {
-    public class NullIdentityProvider : IdentityProviderBase
+    public class NullIdentityProvider : IIdentityProvider
     {
-        public NullIdentityProvider(IIdentityRequestChecker checker) : base(checker)
+        public void SetFor<TEntity>(params TEntity[] entities) where TEntity : class, IEntityKey
         {
+            throw new NotSupportedException("NullIdentityProvider cannot provide identities");
         }
 
-        protected override long[] New(int count)
+        public void SetFor<TEntity>(IReadOnlyCollection<TEntity> entities) where TEntity : class, IEntityKey
         {
-            return new long[count];
+            throw new NotSupportedException("NullIdentityProvider cannot provide identities");
         }
     }
 }
