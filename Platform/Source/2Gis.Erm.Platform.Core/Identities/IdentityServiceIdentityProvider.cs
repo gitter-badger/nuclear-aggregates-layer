32b5080789b1a9e026a77112484d1208bf8c706f
Alexander Tukaev <tukaef@gmail.com>
2015-04-30 11:42:53 +0600

    [NA] убрал ограничение на кол-во запрашиваемых id

--- Platform/Source/2Gis.Erm.Platform.Core/Identities/IdentityServiceIdentityProvider.cs
+++ Platform/Source/2Gis.Erm.Platform.Core/Identities/IdentityServiceIdentityProvider.cs
@@ -1,14 +1,10 @@
-﻿using System;
-
-using DoubleGis.Erm.Platform.API.Core.Identities;
+﻿using DoubleGis.Erm.Platform.API.Core.Identities;
 using NuClear.IdentityService.Client.Interaction;
 
 namespace DoubleGis.Erm.Platform.Core.Identities
 {
     public sealed class IdentityServiceIdentityProvider : IdentityProviderBase
     {
-        // Максимальное количество идентификаторов, которое можно запросить у сервиса генерации идентификаторов
-        private const int MaxRequestedCount = 32767;
         private readonly IIdentityServiceClient _identityServiceClient;
 
         public IdentityServiceIdentityProvider(IIdentityServiceClient identityServiceClient, IIdentityRequestChecker identityRequestChecker)
@@ -19,11 +15,6 @@ namespace DoubleGis.Erm.Platform.Core.Identities
 
         protected override long[] New(int count)
         {
-            if (count > MaxRequestedCount)
-            {
-                throw new ArgumentException(string.Format("Cannot generate more than {0} ids at once", MaxRequestedCount), "count");
-            }
-
             return _identityServiceClient.GetIdentities(count);
         }
     }
