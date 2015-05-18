074a9631f60e3335bcd7a84b9b7ebbb1f7ed7e75
Yury Baranikhin <BaranikhinYury@gmail.com>
2015-05-18 16:08:14 +0600

    [ERM-6579] fix

--- Platform/Source/2Gis.Erm.Platform.Common/Serialization/EntityTypeToStringConverter.cs
+++ Platform/Source/2Gis.Erm.Platform.Common/Serialization/EntityTypeToStringConverter.cs
@@ -17,35 +17,18 @@ namespace DoubleGis.Erm.Platform.Common.Serialization
             }
             else
             {
-                writer.WriteValue(value != null ? value.ToString() : "null");
+                throw new JsonWriterException("A value of IEntityType was expected");
             }
         }
 
         public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
         {
-            IEntityType value;
-            if (reader.Value is int && EntityType.Instance.TryParse((int)reader.Value, out value))
-            {
-                return value;
-            }
-
-            var stringValue = reader.Value as string;
-            if (string.IsNullOrEmpty(stringValue))
-            {
-                return null;
-            }
-            
-            if (EntityType.Instance.TryParse(stringValue, out value))
-            {
-                return value;
-            }
-
-            throw new JsonReaderException("Cannot convert value " + reader.Value + "to IEntityType");
+            throw new NotSupportedException();
         }
 
         public override bool CanConvert(Type objectType)
         {
-            return typeof(IEntityType).IsAssignableFrom(objectType) || objectType == typeof(int) || objectType == typeof(string);
+            return typeof(IEntityType).IsAssignableFrom(objectType);
         }
     }
 }
