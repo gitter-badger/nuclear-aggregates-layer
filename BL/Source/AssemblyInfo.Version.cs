98461d93a7b8efe4e8c5cb3dbeaf929d5dbad0a8
Erm-buildagent <Erm-buildagent@2gis.ru>
2015-05-25 19:00:04 +0600

    [NA] Обновлён номер версии

--- BL/Source/AssemblyInfo.Version.cs
+++ BL/Source/AssemblyInfo.Version.cs
@@ -4,11 +4,11 @@ using System.Resources;
 [assembly: AssemblyVersion("2.0.0")]
 [assembly: SatelliteContractVersion("2.0.0")]
 
-[assembly: AssemblyFileVersion("2.91.28")]
-[assembly: AssemblyInformationalVersion("2.91.0-2.92.0-340577-28")]
+[assembly: AssemblyFileVersion("2.92.30")]
+[assembly: AssemblyInformationalVersion("2.92.0-2.93.0-66074f-30")]
 
 internal static class ThisAssembly
 {
-    internal const string SemanticVersion = "2.91.0-2.92.0-340577-28";
-    internal const string Build = "28";
+    internal const string SemanticVersion = "2.92.0-2.93.0-66074f-30";
+    internal const string Build = "30";
 }
